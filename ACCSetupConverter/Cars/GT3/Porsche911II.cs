c7ecd898e1372fd6eace737f8f7c91cdc1cb7f7a
RiddleTime <reinierklarenberg@gmail.com>
2022-03-22 15:19:10 +0100

    Initial commit

---
+++ ACCSetupConverter/Cars/GT3/Porsche911II.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACCSetupConverter.Cars.GT3
+{
+    public class Porsche911II : ICarSetupConversion
+    {
+        public string CarName => "Porsche 911.2 GT3";
+        public CarClasses CarClass => CarClasses.GT3;
+
+        public ITyresSetup TyresSetup => new TyreSetup();
+        private class TyreSetup : ITyresSetup
+        {
+            public double Toe(Wheel wheel, List<int> rawValue)
+            {
+                return Math.Round(0.01 * rawValue[(int)wheel] - 0.4, 2);
+            }
+
+            public double Camber(Wheel wheel, List<int> rawValue)
+            {
+                Position position;
+                switch (wheel)
+                {
+                    case Wheel.FrontLeft:
+                        position = Position.Front;
+                        break;
+                    case Wheel.FrontRight:
+                        position = Position.Front;
+                        break;
+
+                    default:
+                        position = Position.Rear;
+                        break;
+                }
+
+                switch (position)
+                {
+                    case Position.Front: return Math.Round(-6.5 + 0.1 * rawValue[(int)wheel], 2);
+                    case Position.Rear: return Math.Round(-5 + 0.1 * rawValue[(int)wheel], 2);
+                    default: return -1;
+                }
+            }
+            public double Caster(int rawValue)
+            {
+                return Math.Round(4.4 + 0.2 * rawValue, 3);
+            }
+        }
+
+        public IMechanicalSetup MechanicalSetup => new MechSetup();
+        private class MechSetup : IMechanicalSetup
+        {
+            public int AntiRollBarFront(int rawValue)
+            {
+                return rawValue;
+            }
+
+            public int AntiRollBarRear(int rawValue)
+            {
+                return rawValue;
+            }
+
+            public double BrakeBias(int rawValue)
+            {
+                return Math.Round(43 + 0.2 * rawValue, 2);
+            }
+
+            public int BrakePower(int rawValue)
+            {
+                return 80 + rawValue;
+            }
+
+            public int BumpstopRange(List<int> rawValue, Wheel wheel)
+            {
+                return rawValue[(int)wheel];
+            }
+
+            public int BumpstopRate(List<int> rawValue, Wheel wheel)
+            {
+                return 300 + 100 * rawValue[(int)wheel];
+            }
+
+            public int PreloadDifferential(int rawValue)
+            {
+                return 20 + rawValue * 10;
+            }
+
+            public double SteeringRatio(int rawValue)
+            {
+                return Math.Round(11d + rawValue, 2);
+            }
+
+            public int WheelRate(List<int> rawValue, Wheel wheel)
+            {
+                int[] front = new int[] { 100500, 110000, 114000, 119000, 127000, 137000, 141500, 146000, 155000, 173500 };
+                int[] rear = new int[] { 137000, 149500, 156000, 162000, 174500, 187000, 193000, 199500, 212000, 237000 };
+
+                Position position;
+                switch (wheel)
+                {
+                    case Wheel.FrontLeft:
+                        position = Position.Front;
+                        break;
+                    case Wheel.FrontRight:
+                        position = Position.Front;
+                        break;
+
+                    default:
+                        position = Position.Rear;
+                        break;
+                }
+
+                switch (position)
+                {
+                    case Position.Front: return front[rawValue[(int)wheel]];
+                    case Position.Rear: return rear[rawValue[(int)wheel]];
+                    default: return -1;
+                }
+            }
+        }
+
+        public IAeroBalance AeroBalance => new AeroSetup();
+        private class AeroSetup : IAeroBalance
+        {
+            public int BrakeDucts(int rawValue)
+            {
+                return rawValue;
+            }
+
+            public int RideHeight(List<int> rawValue, Position wheel)
+            {
+                switch (wheel)
+                {
+                    case Position.Front: return 53 + rawValue[0];
+                    case Position.Rear: return 55 + rawValue[2];
+                    default: return -1;
+                }
+            }
+        }
+
+    }
+}
---
+++ SetupParser/Program.cs
@@ -0,0 +1,116 @@
+﻿using ACCSetupConverter;
+using ACCSetupConverter.Cars.GT3;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Text.Json;
+using static ACCSetupConverter.SetupJson;
+
+namespace SetupParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Read Json file
+
+            FileInfo jsonFile = new FileInfo(@"C:\Users\Reinier\Documents\Assetto Corsa Competizione\Setups\porsche_991ii_gt3_r\misano\S2.json");
+            string jsonString = string.Empty;
+            try
+            {
+                using (FileStream fileStream = jsonFile.OpenRead())
+                {
+                    using (StreamReader reader = new StreamReader(fileStream))
+                    {
+                        jsonString = reader.ReadToEnd();
+                        reader.Close();
+                        fileStream.Close();
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e);
+            }
+            Root setup = JsonSerializer.Deserialize<Root>(jsonString);
+
+
+            // Create porsche 911.2 GT3 setup converter 
+            ICarSetupConversion carSetup = new Porsche911II();
+
+            TyreCompound compound = carSetup.TyresSetup.Compound(setup.basicSetup.tyres.tyreCompound);
+
+            // Allignment / Tyre Setup
+            double frontLeftPressure = carSetup.TyresSetup.TirePressure(carSetup.CarClass, Wheel.FrontLeft, setup.basicSetup.tyres.tyrePressure);
+            double frontRightPressure = carSetup.TyresSetup.TirePressure(carSetup.CarClass, Wheel.FrontRight, setup.basicSetup.tyres.tyrePressure);
+            double rearLeftPressure = carSetup.TyresSetup.TirePressure(carSetup.CarClass, Wheel.RearLeft, setup.basicSetup.tyres.tyrePressure);
+            double rearRightPressure = carSetup.TyresSetup.TirePressure(carSetup.CarClass, Wheel.RearRight, setup.basicSetup.tyres.tyrePressure);
+
+            double frontLeftCaster = carSetup.TyresSetup.Caster(setup.basicSetup.alignment.casterLF);
+            double frontRightCaster = carSetup.TyresSetup.Caster(setup.basicSetup.alignment.casterRF);
+
+            double frontLeftToe = carSetup.TyresSetup.Toe(Wheel.FrontLeft, setup.basicSetup.alignment.toe);
+            double frontRightToe = carSetup.TyresSetup.Toe(Wheel.FrontRight, setup.basicSetup.alignment.toe);
+            double rearLeftToe = carSetup.TyresSetup.Toe(Wheel.RearLeft, setup.basicSetup.alignment.toe);
+            double rearRightToe = carSetup.TyresSetup.Toe(Wheel.RearRight, setup.basicSetup.alignment.toe);
+
+            double camberFrontLeft = carSetup.TyresSetup.Camber(Wheel.FrontLeft, setup.basicSetup.alignment.camber);
+            double camberFrontRight = carSetup.TyresSetup.Camber(Wheel.FrontRight, setup.basicSetup.alignment.camber);
+            double camberRearLeft = carSetup.TyresSetup.Camber(Wheel.RearLeft, setup.basicSetup.alignment.camber);
+            double camberRearRight = carSetup.TyresSetup.Camber(Wheel.RearRight, setup.basicSetup.alignment.camber);
+
+
+            // Mechnical Setup
+            int wheelRateFrontLeft = carSetup.MechanicalSetup.WheelRate(setup.advancedSetup.mechanicalBalance.wheelRate, Wheel.FrontLeft);
+            int wheelRateFrontRight = carSetup.MechanicalSetup.WheelRate(setup.advancedSetup.mechanicalBalance.wheelRate, Wheel.FrontRight);
+            int wheelRateRearLeft = carSetup.MechanicalSetup.WheelRate(setup.advancedSetup.mechanicalBalance.wheelRate, Wheel.RearLeft);
+            int wheelRateRearRight = carSetup.MechanicalSetup.WheelRate(setup.advancedSetup.mechanicalBalance.wheelRate, Wheel.RearRight);
+
+
+            int bumpStopRateFrontLeft = carSetup.MechanicalSetup.BumpstopRate(setup.advancedSetup.mechanicalBalance.bumpStopRateUp, Wheel.FrontLeft);
+            int bumpStopRateFrontRight = carSetup.MechanicalSetup.BumpstopRate(setup.advancedSetup.mechanicalBalance.bumpStopRateUp, Wheel.FrontRight);
+            int bumpStopRateRearLeft = carSetup.MechanicalSetup.BumpstopRate(setup.advancedSetup.mechanicalBalance.bumpStopRateUp, Wheel.RearLeft);
+            int bumpStopRateRearRight = carSetup.MechanicalSetup.BumpstopRate(setup.advancedSetup.mechanicalBalance.bumpStopRateUp, Wheel.RearRight);
+
+            int bumpStopRangeFrontLeft = carSetup.MechanicalSetup.BumpstopRange(setup.advancedSetup.mechanicalBalance.bumpStopWindow, Wheel.FrontLeft);
+            int bumpStopRangeFrontRight = carSetup.MechanicalSetup.BumpstopRange(setup.advancedSetup.mechanicalBalance.bumpStopWindow, Wheel.FrontRight);
+            int bumpStopRangeRearLeft = carSetup.MechanicalSetup.BumpstopRange(setup.advancedSetup.mechanicalBalance.bumpStopWindow, Wheel.RearLeft);
+            int bumpStopRangeRearRight = carSetup.MechanicalSetup.BumpstopRange(setup.advancedSetup.mechanicalBalance.bumpStopWindow, Wheel.RearRight);
+
+            int differentialPreload = carSetup.MechanicalSetup.PreloadDifferential(setup.advancedSetup.drivetrain.preload);
+
+            int brakePower = carSetup.MechanicalSetup.BrakePower(setup.advancedSetup.mechanicalBalance.brakeTorque);
+            double brakeBias = carSetup.MechanicalSetup.BrakeBias(setup.advancedSetup.mechanicalBalance.brakeBias);
+            int antiRollBarFront = carSetup.MechanicalSetup.AntiRollBarFront(setup.advancedSetup.mechanicalBalance.aRBFront);
+            int antiRollBarRear = carSetup.MechanicalSetup.AntiRollBarFront(setup.advancedSetup.mechanicalBalance.aRBRear);
+            double steeringRatio = carSetup.MechanicalSetup.SteeringRatio(setup.basicSetup.alignment.steerRatio);
+
+
+            // Aero Balance
+            int rideHeightFront = carSetup.AeroBalance.RideHeight(setup.advancedSetup.aeroBalance.rideHeight, Position.Front);
+            int rideHeightRear = carSetup.AeroBalance.RideHeight(setup.advancedSetup.aeroBalance.rideHeight, Position.Rear);
+
+
+            Debug.WriteLine($"\n---- Tyres Setup -----");
+            Debug.WriteLine($" -- Compound -- \n\t {compound}");
+            Debug.WriteLine($" -- PSI -- \n\t FL: {frontLeftPressure}, FR: {frontRightPressure}, RL: {rearLeftPressure}, RR: {rearRightPressure}");
+            Debug.WriteLine($" -- Caster -- \n\t FL: {frontLeftCaster}, FR: {frontRightCaster}");
+            Debug.WriteLine($" -- Toe -- \n\t FL: {frontLeftToe}, FR: {frontRightToe}, RL: {rearLeftToe}, RR: {rearRightToe}");
+            Debug.WriteLine($" -- Camber -- \n\t FL: {camberFrontLeft}, FR: {camberFrontRight}, RL: {camberRearLeft}, RR: {camberRearRight}");
+
+            Debug.WriteLine($"\n---- Mechanical Grip ----");
+            Debug.WriteLine($" -- WheelRates (Nm) -- \n\t FL: {wheelRateFrontLeft}, FR: {wheelRateFrontRight}, RL: {wheelRateRearLeft}, RR: {wheelRateRearRight}");
+            Debug.WriteLine($" -- Bumpstop Rate (Nm) -- \n\t FL: {bumpStopRateFrontLeft}, FR: {bumpStopRateFrontRight}, RL: {bumpStopRateRearLeft}, RR: {bumpStopRateRearRight}");
+            Debug.WriteLine($" -- Bumpstop range -- \n\t FL: {bumpStopRangeFrontLeft}, FR: {bumpStopRangeFrontRight}, RL: {bumpStopRangeRearLeft}, RR: {bumpStopRangeRearRight}");
+            Debug.WriteLine($" -- Differential Preload (Nm) -- \n\t {differentialPreload}");
+            Debug.WriteLine($" -- Brake Power (%) -- \n\t {brakePower}%");
+            Debug.WriteLine($" -- Brake Bias (%) -- \n\t {brakeBias}%");
+            Debug.WriteLine($" -- Anti Roll Bar-- \n\t Front: {antiRollBarFront}, Rear: {antiRollBarRear}");
+            Debug.WriteLine($" -- Steering Ratio -- \n\t {steeringRatio}");
+
+            Debug.WriteLine($"\n---- Aero Balance ----");
+            Debug.WriteLine($" -- Ride Height (mm)-- \n\t Front: {rideHeightFront}, Rear: {rideHeightRear}");
+
+        }
+    }
+}
