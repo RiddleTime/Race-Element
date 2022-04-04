8586d7d0d6d307a78102bdd96d42319ce2dfc8e8
RiddleTime <reinierklarenberg@gmail.com>
2022-04-04 02:56:52 +0200

    Part of livery browser
    shows images now of each livery

--- ACCSetupApp/Controls/Liveries/LiveryBrowser.xaml.cs
+++ ACCSetupApp/Controls/Liveries/LiveryBrowser.xaml.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Net.Cache;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -27,12 +28,57 @@ namespace ACCSetupApp.Controls
         private string AccPath => Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\" + "Assetto Corsa Competizione\\";
         private string CustomsPath => AccPath + "Customs\\";
         private string CarsPath => CustomsPath + "Cars\\";
+        private string LiveriesPath => CustomsPath + "Liveries\\";
 
         public LiveryBrowser()
         {
             InitializeComponent();
 
             FetchAllCars();
+
+            liveriesTreeView.SelectedItemChanged += LiveriesTreeView_SelectedItemChanged;
+        }
+
+        private void LiveriesTreeView_SelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+            if (e.NewValue != null)
+            {
+                if (e.NewValue.GetType() == typeof(TreeViewItem))
+                {
+                    TreeViewItem item = (TreeViewItem)e.NewValue;
+                    if (item.DataContext != null)
+                    {
+                        if (item.DataContext.GetType() == typeof(CarsJson.Root))
+                        {
+                            CarsJson.Root carsRoot = (CarsJson.Root)item.DataContext;
+                            string customSkinName = carsRoot.customSkinName;
+
+                            if (customSkinName != null && customSkinName.Length > 0)
+                            {
+                                DirectoryInfo customSkinDir = new DirectoryInfo(LiveriesPath + customSkinName);
+                                if (customSkinDir.Exists)
+                                {
+                                    FileInfo[] foundFiles = customSkinDir.GetFiles("decals.png");
+                                    if (foundFiles != null && foundFiles.Length > 0)
+                                    {
+                                        FileInfo decalsFile = foundFiles[0];
+
+                                        liveryImage.Source = new BitmapImage(new Uri(decalsFile.FullName, UriKind.Absolute), new RequestCachePolicy(RequestCacheLevel.CacheIfAvailable));
+                                    }
+                                    else
+                                    {
+                                        liveryImage.Source = null;
+                                    }
+                                }
+                            }
+                            else
+                            {
+                                liveryImage.Source = null;
+                            }
+                        }
+                    }
+                }
+            }
         }
 
         private void FetchAllCars()
@@ -59,6 +105,34 @@ namespace ACCSetupApp.Controls
 
                 }
             }
+        }
+
+        private ContextMenu GetTeamContextMenu(DirectoryInfo directory)
+        {
+            ContextMenu menu = new ContextMenu()
+            {
+                Style = Resources["MaterialDesignContextMenu"] as Style,
+                Margin = new Thickness(0),
+                Padding = new Thickness(0),
+                UsesItemContainerTemplate = true,
+                Background = new SolidColorBrush(Color.FromArgb(220, 0, 0, 0))
+            };
+
+
+            Button openFolder = new Button()
+            {
+                Content = "Open in explorer",
+                CommandParameter = directory,
+                Style = Resources["MaterialDesignRaisedButton"] as Style,
+                Margin = new Thickness(0),
+                Height = 30,
+                VerticalAlignment = VerticalAlignment.Center,
+            };
+            openFolder.Click += OpenFolder_Click;
+
+            menu.Items.Add(openFolder);
+
+            return menu;
         }
 
         public CarsJson.Root GetLivery(FileInfo file)
