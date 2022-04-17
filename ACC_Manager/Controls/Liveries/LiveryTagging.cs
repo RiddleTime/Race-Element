d2e45ca69046c788d65baf7919b9a7fd32729460
RiddleTime <reinierklarenberg@gmail.com>
2022-04-17 13:35:47 +0200

    Start of tagging system

---
+++ ACC_Manager/Controls/Liveries/LiveryTagging.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static ACCSetupApp.Controls.LiveryBrowser;
+
+namespace ACCSetupApp.Controls
+{
+    internal static class LiveryTagging
+    {
+        public class LiveryTag
+        {
+            public string Tag { get; set; }
+            public Guid Guid { get; set; }
+
+            public List<LiveryTreeCar> Liveries { get; set; }
+        }
+
+
+    }
+}
