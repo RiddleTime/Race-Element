13340de76cd3d618341cdc738edec67305ec6b9a
RiddleTime <reinierklarenberg@gmail.com>
2022-04-22 21:39:06 +0200

    static info overlay now shows all members of page static

--- ACC_Manager/Controls/HUD/Overlay/OverlayStaticInfo/StaticInfoOverlay.cs
+++ ACC_Manager/Controls/HUD/Overlay/OverlayStaticInfo/StaticInfoOverlay.cs
@@ -1,10 +1,12 @@
 ﻿using ACCSetupApp.Controls.HUD.Overlay.Internal;
 using ACCSetupApp.Controls.Telemetry.SharedMemory;
+using ACCSetupApp.Util;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,10 +14,12 @@ namespace ACCSetupApp.Controls.HUD.Overlay.OverlayStaticInfo
 {
     internal class StaticInfoOverlay : AbstractOverlay
     {
-        private Font inputFont = new Font("Arial", 16);
+        private Font inputFont = new Font("Arial", 13);
 
         public StaticInfoOverlay(Rectangle rectangle) : base(rectangle)
         {
+            this.Width = 275;
+            this.Height = 390;
         }
 
         public override void BeforeStart()
@@ -28,7 +32,26 @@ namespace ACCSetupApp.Controls.HUD.Overlay.OverlayStaticInfo
 
         public override void Render(Graphics g)
         {
-            g.DrawString($"CAR: {pageStatic.CarModel}", inputFont, Brushes.White, 0, 0);
+            g.FillRectangle(new SolidBrush(System.Drawing.Color.FromArgb(140, 0, 0, 0)), new Rectangle(0, 0, this.Width, this.Height));
+
+            int y = 0;
+            FieldInfo[] members = pageStatic.GetType().GetFields();
+            foreach (FieldInfo member in members)
+            {
+                var value = member.GetValue(pageStatic);
+                bool isObsolete = false;
+                foreach (CustomAttributeData cad in member.CustomAttributes)
+                {
+                    if (cad.AttributeType == typeof(ObsoleteAttribute)) { isObsolete = true; break; }
+                }
+
+                if (!isObsolete && !member.Name.Equals("Buffer") && !member.Name.Equals("Size"))
+                {
+                    value = ReflectionUtil.FieldTypeValue(member, value);
+
+                    g.DrawString($"{member.Name}: {value}", inputFont, Brushes.White, 0, y += (int)inputFont.Size + 1);
+                }
+            }
         }
 
         public override bool ShouldRender()
