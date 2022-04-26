e3cf0ff757f470595bdcca09ae0a61a920c5a2fc
KrisV147 <secondlife_builder@hotmail.co.uk>
2022-04-26 21:31:33 +0100

    Update Porsche991IIGT3Cup_2017.cs
    did have to give it GT3 tyres

--- ACC_Manager/SetupParser/Cars/GTC/Porsche991IIGT3Cup_2017.cs
+++ ACC_Manager/SetupParser/Cars/GTC/Porsche991IIGT3Cup_2017.cs
@@ -12,7 +12,7 @@ namespace ACCSetupApp.SetupParser.Cars.GTC
     {
         public CarModels CarModel => CarModels.Porsche_991_II_GT3_Cup_2017;
 
-        CarClasses ICarSetupConversion.CarClass => CarClasses.GTC;
+        CarClasses ICarSetupConversion.CarClass => CarClasses.GT3; // did this for tyre pressures, should be 'GTC' 
 
         AbstractTyresSetup ICarSetupConversion.TyresSetup => new TyreSetup();
         private class TyreSetup : AbstractTyresSetup
