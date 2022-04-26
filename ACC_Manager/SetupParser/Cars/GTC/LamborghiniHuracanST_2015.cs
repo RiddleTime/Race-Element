3d538e36f3dc6e1a1ac25ffabaa5645dfed3b0db
RiddleTime <reinierklarenberg@gmail.com>
2022-04-26 21:51:12 +0200

    Update LamborghiniHuracanST_2015.cs
    stupid lambo tyre pressures

--- ACC_Manager/SetupParser/Cars/GTC/LamborghiniHuracanST_2015.cs
+++ ACC_Manager/SetupParser/Cars/GTC/LamborghiniHuracanST_2015.cs
@@ -12,7 +12,7 @@ namespace ACCSetupApp.SetupParser.Cars.GTC
     {
         public CarModels CarModel => CarModels.Lamborghini_Huracan_ST_2015;
 
-        CarClasses ICarSetupConversion.CarClass => CarClasses.GTC;
+        CarClasses ICarSetupConversion.CarClass => CarClasses.GT3; // did this for tyre pressures, should be 'GTC' 
 
         AbstractTyresSetup ICarSetupConversion.TyresSetup => new TyreSetup();
         private class TyreSetup : AbstractTyresSetup
