76eeec05da05cd511e3e9e83f013a281081049f2
KrisV147 <secondlife_builder@hotmail.co.uk>
2022-05-01 23:34:07 +0100

    Update AMRV12VantageGT3.cs

--- ACC_Manager/SetupParser/Cars/GT3/AMRV12VantageGT3.cs
+++ ACC_Manager/SetupParser/Cars/GT3/AMRV12VantageGT3.cs
@@ -12,11 +12,11 @@ namespace ACCSetupApp.SetupParser.Cars.GT3
 
         CarClasses ICarSetupConversion.CarClass => CarClasses.GT3;
 
-        private static readonly double[] casters = new double[] { 8.3, 8.5, 8.7, 9.0, 9.2, 9.4, 9.6, 9.9, 10.1, 10.3, 10.5, 10.8, 11.0, 
+        private static readonly double[] casters = new double[] { 8.3, 8.5, 8.7, 9.0, 9.2, 9.4, 9.6, 9.9, 10.1, 10.3, 10.5, 10.8, 11.0,
                 11.2, 11.4, 11.6, 11.9, 12.1, 12.3, 12.5, 12.7, 13.0, 13.2, 13.4, 13.6, 13.8, 14.0, 14.3, 14.5, 14.7, 14.9 };
         private static readonly int[] wheelRateFronts = new int[] { 115000, 120000, 125000, 130000, 135000, 140000, 145000, 150000,
                                                         155000, 160000, 165000, 170000, 175000, 180000, 185000 };
-        private static readonly int[] wheelRateRears = new int[] { 95000, 100000, 110000, 115000, 120000, 125000, 130000, 135000, 140000, 145000, 150000,
+        private static readonly int[] wheelRateRears = new int[] { 95000, 100000, 105000, 110000, 115000, 120000, 125000, 130000, 135000, 140000, 145000, 150000,
                                                         155000, 160000, 165000, 170000, 175000, 180000, 185000, 190000, 195000};
 
         ITyreSetupChanger ISetupChanger.TyreSetupChanger => new TyreSetupChanger();
@@ -28,8 +28,8 @@ namespace ACCSetupApp.SetupParser.Cars.GT3
         private class TyreSetupChanger : ITyreSetupChanger
         {
             public SetupDoubleRange TyrePressures => TyrePressuresGT3;
-            public SetupDoubleRange CamberFront => new SetupDoubleRange(-4, -1.5, 0.1);
-            public SetupDoubleRange CamberRear => new SetupDoubleRange(-3.5, -1, 0.1);
+            public SetupDoubleRange CamberFront => CamberFrontGT3;
+            public SetupDoubleRange CamberRear => CamberRearGT3;
             public SetupDoubleRange ToeFront => new SetupDoubleRange(-0.4, 0.4, 0.01);
             public SetupDoubleRange ToeRear => ToeFront;
             public SetupDoubleRange Caster => new SetupDoubleRange(casters);
@@ -39,38 +39,38 @@ namespace ACCSetupApp.SetupParser.Cars.GT3
         {
             public SetupIntRange TractionControl => new SetupIntRange(0, 11, 1);
             public SetupIntRange ABS => new SetupIntRange(0, 11, 1);
-            public SetupIntRange EcuMap => new SetupIntRange(1, 10, 1);
-            public SetupIntRange TractionControlCut => new SetupIntRange(0, 11, 1);
+            public SetupIntRange EcuMap => new SetupIntRange(1, 8, 1);
+            public SetupIntRange TractionControlCut => new SetupIntRange(0, 0, 1);
         }
 
         private class MechSetupChanger : IMechanicalSetupChanger
         {
-            public SetupIntRange AntiRollBarFront => new SetupIntRange(0, 6, 1);
+            public SetupIntRange AntiRollBarFront => new SetupIntRange(0, 8, 1);
             public SetupIntRange AntiRollBarRear => AntiRollBarFront;
-            public SetupDoubleRange BrakeBias => new SetupDoubleRange(43.0, 64.0, 0.2);
+            public SetupDoubleRange BrakeBias => new SetupDoubleRange(57.0, 78.0, 0.2);
             public SetupIntRange PreloadDifferential => new SetupIntRange(20, 300, 10);
             public SetupIntRange BrakePower => new SetupIntRange(80, 100, 1);
-            public SetupDoubleRange SteeringRatio => new SetupDoubleRange(11, 17, 1);
+            public SetupDoubleRange SteeringRatio => new SetupDoubleRange(14, 18, 1);
             public SetupIntRange WheelRateFronts => new SetupIntRange(wheelRateFronts);
             public SetupIntRange WheelRateRears => new SetupIntRange(wheelRateRears);
             public SetupIntRange BumpstopRate => new SetupIntRange(300, 2500, 100);
-            public SetupIntRange BumpstopRangeFronts => new SetupIntRange(0, 49, 1);
-            public SetupIntRange BumpstopRangeRears => new SetupIntRange(0, 50, 1);
+            public SetupIntRange BumpstopRangeFronts => new SetupIntRange(0, 22, 1);
+            public SetupIntRange BumpstopRangeRears => new SetupIntRange(0, 68, 1);
         }
 
         private class AeroSetupChanger : IAeroSetupChanger
         {
-            public SetupIntRange RideHeightFront => new SetupIntRange(53, 85, 1);
+            public SetupIntRange RideHeightFront => new SetupIntRange(55, 80, 1);
             public SetupIntRange RideHeightRear => new SetupIntRange(55, 90, 1);
             public SetupIntRange BrakeDucts => new SetupIntRange(0, 6, 1);
-            public SetupIntRange Splitter => new SetupIntRange(0, 5, 1);
-            public SetupIntRange RearWing => new SetupIntRange(0, 12, 1);
+            public SetupIntRange Splitter => new SetupIntRange(0, 0, 1);
+            public SetupIntRange RearWing => new SetupIntRange(0, 10, 1);
         }
 
         private class DamperSetupChanger : IDamperSetupChanger
         {
-            public SetupIntRange BumpSlow => new SetupIntRange(0, 17, 1);
-            public SetupIntRange BumpFast => new SetupIntRange(0, 12, 1);
+            public SetupIntRange BumpSlow => new SetupIntRange(0, 40, 1);
+            public SetupIntRange BumpFast => new SetupIntRange(0, 49, 1);
             public SetupIntRange ReboundSlow => BumpSlow;
             public SetupIntRange ReboundFast => BumpFast;
         }
@@ -88,10 +88,10 @@ namespace ACCSetupApp.SetupParser.Cars.GT3
                 }
             }
 
-//            private readonly double[] casters = new double[] {
-//               8.3, 8.5, 8.7, 9.0, 9.2, 9.4, 9.6, 9.9, 10.1, 10.3, 10.5, 10.8, 11.0, 11.2, 11.4, 11.6,
-//              11.9, 12.1, 12.3, 12.5, 12.7, 13.0, 13.2, 13.4, 13.6, 13.8, 14.0, 14.3, 14.5, 14.7, 14.9
-//         };
+            //            private readonly double[] casters = new double[] {
+            //               8.3, 8.5, 8.7, 9.0, 9.2, 9.4, 9.6, 9.9, 10.1, 10.3, 10.5, 10.8, 11.0, 11.2, 11.4, 11.6,
+            //              11.9, 12.1, 12.3, 12.5, 12.7, 13.0, 13.2, 13.4, 13.6, 13.8, 14.0, 14.3, 14.5, 14.7, 14.9
+            //         };
             public override double Caster(int rawValue)
             {
                 return Math.Round(casters[rawValue], 2);
@@ -146,10 +146,10 @@ namespace ACCSetupApp.SetupParser.Cars.GT3
                 return Math.Round(14d + rawValue, 2);
             }
 
-//            private readonly int[] fronts = new int[] { 115000, 120000, 125000, 130000, 135000, 140000, 145000, 150000,
-//                                                        155000, 160000, 165000, 170000, 175000, 180000, 185000 };
-//            private readonly int[] rears = new int[] { 95000, 100000, 110000, 115000, 120000, 125000, 130000, 135000, 140000, 145000, 150000,
-//                                                        155000, 160000, 165000, 170000, 175000, 180000, 185000, 190000, 195000 };
+            //            private readonly int[] fronts = new int[] { 115000, 120000, 125000, 130000, 135000, 140000, 145000, 150000,
+            //                                                        155000, 160000, 165000, 170000, 175000, 180000, 185000 };
+            //            private readonly int[] rears = new int[] { 95000, 100000, 110000, 115000, 120000, 125000, 130000, 135000, 140000, 145000, 150000,
+            //                                                        155000, 160000, 165000, 170000, 175000, 180000, 185000, 190000, 195000 };
             public int WheelRate(List<int> rawValue, Wheel wheel)
             {
                 switch (GetPosition(wheel))
