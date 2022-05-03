f5c6dfb90e698c3485c02edb914dc15a0af98317
KrisV147 <secondlife_builder@hotmail.co.uk>
2022-05-04 00:44:00 +0100

    I did a thing!
    Messed about adding weather to the overlay

--- ACC_Manager/Controls/HUD/Overlay/OverlayTrackInfo/TrackInfoOverlay.cs
+++ ACC_Manager/Controls/HUD/Overlay/OverlayTrackInfo/TrackInfoOverlay.cs
@@ -11,10 +11,11 @@ namespace ACCSetupApp.Controls.HUD.Overlay.OverlayTrackInfo
     internal class TrackInfoOverlay : AbstractOverlay
     {
         private Font inputFont = new Font("Arial", 10);
-
+        private static double weather10 = (600.0 / 48.0) / 100.0; //the 48 here represents the time multiplier in-game. ACC shows this as 0.5 minutes - x24 is same.
+        private static double weather30 = (1800.0 / 48.0) / 100.0; //as above, but ACC shows this as 1 minute (x12 0.5/2 - x6 1/5 - x4 2/7 - x2 5/15 - x1 10/30)
         public TrackInfoOverlay(Rectangle rectangle) : base(rectangle, "Track Info Overlay")
         {
-            this.Width = 200;
+            this.Width = 300;
             this.Height = 100;
         }
 
@@ -28,6 +29,10 @@ namespace ACCSetupApp.Controls.HUD.Overlay.OverlayTrackInfo
 
             g.DrawString($"Blinker left is on? : {pageGraphics.BlinkerLeftOn}", inputFont, Brushes.White, new PointF(0, 0));
             //g.DrawString($"")
+            
+            g.DrawString($"Weather is : {pageGraphics.rainIntensity}", inputFont, Brushes.Yellow, new PointF(0, 20));
+            g.DrawString($"Weather in {weather10} minutes will be : {pageGraphics.rainIntensityIn10min}", inputFont, Brushes.LightBlue, new PointF(0, 40));
+            g.DrawString($"Weather in {weather30} minutes will be : {pageGraphics.rainIntensityIn30min}", inputFont, Brushes.LightGreen, new PointF(0, 60));
         }
 
         public override bool ShouldRender()
