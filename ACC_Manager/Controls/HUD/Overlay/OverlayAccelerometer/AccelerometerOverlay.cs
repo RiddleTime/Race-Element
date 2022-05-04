38cdfeb1d8ab3c6a1d80e86ac791b5831426e351
RiddleTime <reinierklarenberg@gmail.com>
2022-05-04 21:46:31 +0200

    small refactor on accelerometer

--- ACC_Manager/Controls/HUD/Overlay/OverlayAccelerometer/AccelerometerOverlay.cs
+++ ACC_Manager/Controls/HUD/Overlay/OverlayAccelerometer/AccelerometerOverlay.cs
@@ -15,7 +15,7 @@ namespace ACCSetupApp.Controls.HUD.Overlay.OverlayAccelerometer
 
         public AccelerometerOverlay(Rectangle rectangle) : base(rectangle, "Accelerometer Overlay")
         {
-            this.Width = 200;
+            this.Width = 300;
             this.Height = this.Width;
         }
 
@@ -29,47 +29,48 @@ namespace ACCSetupApp.Controls.HUD.Overlay.OverlayAccelerometer
 
         public override void Render(Graphics g)
         {
-            int paintWidth = this.Width - 1;
-            int paintHeight = this.Height - 1;
+            DrawGMeter(48, 48, 250, g);
 
-            Pen AccPen = new Pen(Color.FromArgb(100, 255, 255, 255));
-            AccPen.Width = 1;
-            Pen AccPen2 = new Pen(Color.FromArgb(30, 255, 255, 255));
-            AccPen2.Width = 3;
-            Pen AccPen3 = new Pen(Color.FromArgb(100, 255, 255, 255));
-            AccPen3.Width = 4;
-            Pen AccPen4 = new Pen(Color.FromArgb(200, 200, 200, 200));
-            AccPen4.Width = 5;
+            infoPanel.AddLine(new InfoPanel.InfoLine() { Title = "AccG X", Value = $"{pagePhysics.AccG[0]}" });
+            infoPanel.AddLine(new InfoPanel.InfoLine() { Title = "AccG Y", Value = $"{pagePhysics.AccG[2]}" });
+            infoPanel.Draw(g);
+        }
 
-            int gDotWidth = 14;
-            int gDotHeight = gDotWidth;
+        private void DrawGMeter(int x, int y, int size, Graphics g)
+        {
+            int gMeterX = x;
+            int gMeterY = y;
+            int paintWidth = size;
+            int paintHeight = size;
+
+            Pen AccPen = new Pen(Color.FromArgb(100, 255, 255, 255), 1);
+            Pen AccPen2 = new Pen(Color.FromArgb(30, 255, 255, 255), 3);
+            Pen AccPen3 = new Pen(Color.FromArgb(100, 255, 255, 255), 4);
+            Pen AccPen4 = new Pen(Color.FromArgb(200, 200, 200, 200), 5);
+
+            int gDotSize = 14;
 
             double AccGX = (double)pagePhysics.AccG[0] * 100;
             double AccGY = (double)pagePhysics.AccG[2] * 100;
 
-            int gDotPosX = (int)AccGX + (paintWidth / 2) - (gDotWidth / 2);
-            int gDotPosY = (int)AccGY + (paintHeight / 2) - (gDotHeight / 2);
+            int gDotPosX = (int)AccGX + (paintWidth / 2) - (gDotSize / 2);
+            int gDotPosY = (int)AccGY + (paintHeight / 2) - (gDotSize / 2);
 
             //Draws the HUD window
             g.FillRectangle(new SolidBrush(System.Drawing.Color.FromArgb(140, 0, 0, 0)), new Rectangle(0, 0, this.Width, this.Height));
             //Draws the lines and circles
-            g.DrawLine(AccPen, 0, paintHeight / 2, paintWidth, paintHeight / 2);
-            g.DrawLine(AccPen, paintWidth / 2, 0, paintWidth / 2, paintHeight);
+            g.DrawLine(AccPen, 0 + gMeterX, gMeterY + paintHeight / 2, gMeterX + paintWidth, gMeterY + paintHeight / 2);
+            g.DrawLine(AccPen, gMeterX + paintWidth / 2, gMeterY, gMeterX + paintWidth / 2, gMeterY + paintHeight);
             System.Drawing.Drawing2D.SmoothingMode previousSmoothing = g.SmoothingMode;
             g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
-            g.DrawEllipse(AccPen4, 2, 2, paintWidth - 4, paintHeight - 4);
-            g.DrawEllipse(AccPen3, paintWidth / 6, paintHeight / 6, (paintWidth / 3) * 2, (paintHeight / 3) * 2);
-            g.DrawEllipse(AccPen2, paintWidth / 3, paintHeight / 3, paintWidth / 3, paintHeight / 3);
+            g.DrawEllipse(AccPen4, gMeterX + 2, gMeterY + 2, paintWidth - 4, paintHeight - 4);
+            g.DrawEllipse(AccPen3, gMeterX + paintWidth / 6, gMeterY + paintHeight / 6, (paintWidth / 3) * 2, (paintHeight / 3) * 2);
+            g.DrawEllipse(AccPen2, gMeterX + paintWidth / 3, gMeterY + paintHeight / 3, paintWidth / 3, paintHeight / 3);
 
             //Draws the 'dot'
             g.FillEllipse(new SolidBrush(System.Drawing.Color.FromArgb(242, 82, 2)),
-                            new Rectangle(gDotPosX, gDotPosY, gDotWidth, gDotHeight));
+                            new Rectangle(gMeterX + gDotPosX, gMeterY + gDotPosY, gDotSize, gDotSize));
             g.SmoothingMode = previousSmoothing;
-
-
-            infoPanel.AddLine(new InfoPanel.InfoLine() { Title = "AccG X", Value = $"{AccGX}" });
-            infoPanel.AddLine(new InfoPanel.InfoLine() { Title = "AccG Y", Value = $"{AccGY}" });
-            infoPanel.Draw(g);
         }
 
         public override bool ShouldRender()
