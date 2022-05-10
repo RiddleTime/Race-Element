ab094c36283e3ef94b72b648c172a165f779f355
RiddleTime <reinierklarenberg@gmail.com>
2022-05-11 01:01:24 +0200

    Add integer extensions

---
+++ ACC_Manager.Util/NumberExtensions/IntegerExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACC_Manager.Util.NumberExtensions
+{
+    public static class IntegerExtensions
+    {
+        /// <summary>
+        /// Sets this value or returns it, clipped by min and max (inclusive)
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="min"></param>
+        /// <param name="max"></param>
+        /// <returns></returns>
+        public static int Clip(ref this int value, int min, int max)
+        {
+            if (value < min) value = min;
+            if (value > max) value = max;
+            return value;
+        }
+    }
+}
