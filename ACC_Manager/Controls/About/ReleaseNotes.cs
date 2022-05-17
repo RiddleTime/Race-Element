067c66b0f814d4c8020e7bef44443372737646b0
RiddleTime <reinierklarenberg@gmail.com>
2022-05-17 03:21:26 +0200

    update release notes

--- ACC_Manager/Controls/About/ReleaseNotes.cs
+++ ACC_Manager/Controls/About/ReleaseNotes.cs
@@ -11,7 +11,8 @@ namespace ACCManager.Controls
         public readonly static Dictionary<string, string> Notes = new Dictionary<string, string>()
         {
             {"0.0.6.0", "- HUD Tab: Scroll the scale scrollbar to adjust the scale."+
-                        "\n- HUD: ECU Map Overlay can now be scaled and repositioned." },
+                        "\n- HUD: ECU Map Overlay can now be scaled and repositioned."+
+                        "\n- Trees in the GUI now expand on single click."},
             {"0.0.5.9", "- About Tab: Added information about HUDs."+
                         "\n- Added Play ACC Button, launches Assetto Corsa Competizione using the steam link."+
                         "\n- HUD: Added Fuel Info Overlay (By KrisV147)."+
