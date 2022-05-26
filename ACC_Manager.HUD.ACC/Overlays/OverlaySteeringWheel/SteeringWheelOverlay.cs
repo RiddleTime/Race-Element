c2d5ccf892ce6a7ede4575d8eeb11e37a35dd5cb
Florian <Florian@MIRACULIX>
2022-05-26 18:17:43 +0200

    throttle and brake indicator

--- ACC_Manager.HUD.ACC/Overlays/OverlaySteeringWheel/SteeringWheelOverlay.cs
+++ ACC_Manager.HUD.ACC/Overlays/OverlaySteeringWheel/SteeringWheelOverlay.cs
@@ -1,35 +1,45 @@
-﻿using ACCManager.Data;
-using ACCManager.Data.ACC.Cars;
+﻿using ACCManager.Data.ACC.Cars;
 using ACCManager.HUD.Overlay.Configuration;
 using ACCManager.HUD.Overlay.Internal;
 using ACCManager.HUD.Overlay.OverlayUtil;
 using ACCManager.HUD.Overlay.Util;
-using System;
-using System.Collections.Generic;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using static ACCManager.ACCSharedMemory;
-using static ACCManager.Data.ConversionFactory;
-using static ACCManager.Data.SetupConverter;
 
 namespace ACCManager.HUD.ACC.Overlays.OverlaySteeringWheel
 {
     internal sealed class SteeringWheelOverlay : AbstractOverlay
     {
 
-        private int height = 150;
-        private int width = 150;
-        private int wheelWidth = 15;
-        private int indicatorWidth = 15;
+        private const int initialHeight = 150;
+        private const int initialWidth = 150;
+        private const int wheelWidth = 15;
+        private const int indicatorWidth = 15;
+        private const int inputCircleMinAngle = 135;
+        private const int inputCircleSweepAngle = 270;
 
+        private int innerWheelWidth = 0;
+        private readonly SolidBrush bkgrndBrush;
+        private readonly Pen wheelPen;
+        private readonly Pen indicatorPen;
+        private readonly Pen thottleIndicatorBkgrnd;
+        private readonly Pen thottleIndicatorFrgrnd;
+        private readonly Pen brakingIndicatorBkgrnd;
+        private readonly Pen brakingIndicatorFrgrnd;
         private readonly SteeringWheelConfig config = new SteeringWheelConfig();
 
         internal class SteeringWheelConfig : OverlayConfiguration
         {
             [ToolTip("Draw steering indicator with a transparent backround.")]
             public bool TransparentBackground { get; set; } = false;
+
+            [ToolTip("Show throttle input indicator.")]
+            public bool ThrottleIndicator { get; set; } = true;
+            [ToolTip("Show brake input indicator.")]
+            public bool BrakeIndicator { get; set; } = true;
+            [ToolTip("Show selected gear.")]
+            public bool GearIndicator { get; set; } = true;
+
 
             public SteeringWheelConfig()
             {
@@ -39,18 +49,28 @@ namespace ACCManager.HUD.ACC.Overlays.OverlaySteeringWheel
 
         public SteeringWheelOverlay(Rectangle rectangle) : base(rectangle, "Steering Wheel Overlay")
         {
-            this.Height = height;
-            this.Width = width;
+            this.Height = initialHeight;
+            this.Width = initialWidth;
+
+            this.innerWheelWidth = wheelWidth / 2;
+            this.bkgrndBrush = new SolidBrush(Color.FromArgb(100, Color.Black));
+            this.wheelPen = new Pen(Color.FromArgb(100, 150, 150, 150), wheelWidth);
+            this.indicatorPen = new Pen(Color.White, wheelWidth);
+            this.thottleIndicatorBkgrnd = new Pen(Color.FromArgb(50, 10, 255, 10), this.innerWheelWidth);
+            this.thottleIndicatorFrgrnd = new Pen(Color.FromArgb(255, 10, 255, 10), this.innerWheelWidth);
+            this.brakingIndicatorBkgrnd = new Pen(Color.FromArgb(50, 255, 10, 10), this.innerWheelWidth);
+            this.brakingIndicatorFrgrnd = new Pen(Color.FromArgb(255, 255, 10, 10), this.innerWheelWidth);
+
         }
 
         public override void BeforeStart()
         {
-            
+
         }
 
         public override void BeforeStop()
         {
-            
+
         }
 
         public override void Render(Graphics g)
@@ -60,38 +80,91 @@ namespace ACCManager.HUD.ACC.Overlays.OverlaySteeringWheel
 
             if (config.TransparentBackground)
             {
-                Rectangle graphRect = new Rectangle(0, 0, Width, Height);
-                SolidBrush bkgrndBrush = new SolidBrush(Color.FromArgb(100, Color.Black));
+                Rectangle graphRect = new Rectangle(0, 0, this.Width, this.Height);
                 g.FillRoundedRectangle(bkgrndBrush, graphRect, 10);
             }
 
-            Pen wheelPen = new Pen(Color.FromArgb(100, 150, 150, 150), wheelWidth);
-            g.DrawCircle(wheelPen, this.Width / 2, this.Height / 2, (this.Height / 2) - wheelWidth);
+            drawSteeringIndicator(g);
+            drawThrottleIndicator(g);
+            drawBrakingIndicator(g);
+            drawGearIndicator(g);
 
-            SolidBrush brush = new SolidBrush(Color.Red);
-            StringFormat format = new StringFormat();
-            float accSteering = (pagePhysics.SteerAngle + 1) * 100; // map acc value to 0 - 200
-            float angle = toAngle(accSteering);
-
-#if DEBUG
-            Font font = FontUtil.FontUnispace(10);
-            g.DrawString($"angle: {angle.ToString("000.00")}", font, brush, 10, Height-15, format);
-#endif
-
-            Rectangle rect = new Rectangle(0+wheelWidth, 0+wheelWidth, Width-(2*wheelWidth), Height-(2*wheelWidth));
-            Pen indicatorPen = new Pen(Color.FromArgb(255, 255, 10, 10), wheelWidth);
-            float drawAngle = angle+270-(indicatorWidth/2);
-            g.DrawArc(indicatorPen, rect, drawAngle, indicatorWidth);
-             
         }
 
-        private float toAngle(float input)
+        private void drawGearIndicator(Graphics g)
         {
-            int steeringAngle = getMaxSteeringAngle();
-            float steeringAngleMax = steeringAngle*2;
-            float wheelInputMax = 200;
-            return ((steeringAngleMax) / (wheelInputMax) * (input))-(float)steeringAngle;
+            if (!config.GearIndicator)
+            {
+                return;
+            }
 
+            string gearIndicator = "?";
+            int accGear = pagePhysics.Gear;
+            if (accGear == 0) gearIndicator = "R";
+            else if (accGear == 1) gearIndicator = "N";
+            else gearIndicator = ((pagePhysics.Gear) - 1).ToString();
+
+            SolidBrush drawBrush = new SolidBrush(Color.White);
+            Font drawFont = FontUtil.FontOrbitron(30);
+
+            SizeF gearIndicatorSize = g.MeasureString(gearIndicator, drawFont);
+            int xPos = (int)((Width / 2) - (gearIndicatorSize.Width / 2));
+            int yPos = (int)((Height / 2) - (gearIndicatorSize.Height / 2));
+            g.DrawString(gearIndicator, drawFont, drawBrush, xPos, yPos, new StringFormat());
+        }
+
+        private void drawBrakingIndicator(Graphics g)
+        {
+            if (!config.BrakeIndicator)
+            {
+                return;
+            }
+
+            // TODO add ABS indicator
+
+            int x = wheelWidth * 2 + (config.ThrottleIndicator ? this.innerWheelWidth : 0);
+            int y = wheelWidth * 2 + (config.ThrottleIndicator ? this.innerWheelWidth : 0);
+            int width = Width - (4 * wheelWidth) - (2 * (config.ThrottleIndicator ? this.innerWheelWidth : 0));
+            int height = Height - (4 * wheelWidth) - (2 * (config.ThrottleIndicator ? this.innerWheelWidth : 0));
+
+            Rectangle rect = new Rectangle(x, y, width, height);
+            g.DrawArc(brakingIndicatorBkgrnd, rect, inputCircleMinAngle, inputCircleSweepAngle);
+            float brakeAngle = rescale(1, inputCircleSweepAngle, pagePhysics.Brake);
+            g.DrawArc(brakingIndicatorFrgrnd, rect, inputCircleMinAngle, brakeAngle);
+        }
+
+        private void drawThrottleIndicator(Graphics g)
+        {
+            if (!config.ThrottleIndicator)
+            {
+                return;
+            }
+
+            Rectangle rect = new Rectangle(0 + wheelWidth * 2, 0 + wheelWidth * 2, Width - (4 * wheelWidth), Height - (4 * wheelWidth));
+            g.DrawArc(thottleIndicatorBkgrnd, rect, inputCircleMinAngle, inputCircleSweepAngle);
+            float throttleAngle = rescale(1, inputCircleSweepAngle, pagePhysics.Gas);
+            g.DrawArc(thottleIndicatorFrgrnd, rect, inputCircleMinAngle, throttleAngle);
+        }
+
+        private void drawSteeringIndicator(Graphics g)
+        {
+            g.DrawCircle(wheelPen, this.Width / 2, this.Height / 2, (this.Height / 2) - wheelWidth);
+
+            SolidBrush brush = new SolidBrush(Color.White);
+            StringFormat format = new StringFormat();
+            float accSteering = (pagePhysics.SteerAngle + 1) * 100; // map acc value to 0 - 200
+            float angle = rescale(200, getMaxSteeringAngle() * 2, accSteering) - (getMaxSteeringAngle());
+
+            // steering indicator
+            Rectangle rect = new Rectangle(0 + wheelWidth, 0 + wheelWidth, Width - (2 * wheelWidth), Height - (2 * wheelWidth));
+            float drawAngle = angle + 270 - (indicatorWidth / 2);
+            g.DrawArc(indicatorPen, rect, drawAngle, indicatorWidth);
+        }
+
+        // map value input from range 0 - fromMax into range 0 - toMax
+        private float rescale(float fromMax, float toMax, float input)
+        {
+            return ((toMax) / (fromMax) * (input));
         }
 
         private int getMaxSteeringAngle()
