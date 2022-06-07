d943badcda23f823808c888cb2fcfc11f930b8b7
RiddleTime <reinierklarenberg@gmail.com>
2022-06-07 11:38:51 +0200

    bump version in advance

--- ACC_Manager/Controls/About/ReleaseNotes.cs
+++ ACC_Manager/Controls/About/ReleaseNotes.cs
@@ -10,6 +10,7 @@ namespace ACCManager.Controls
     {
         public readonly static Dictionary<string, string> Notes = new Dictionary<string, string>()
         {
+            {"0.0.7.1", "- HUDs: Improve rendering performance." },
             {"0.0.7.0", "- HUD: Added configurable data collection/refresh rate for input trace."+
                         "\n- HUD: Lap delta overlay's recorded laps are now reset in between practice/qualifying/race."},
             {"0.0.6.9", "- HUD: Added configurable potentional best lap time to lap delta overlay."+
