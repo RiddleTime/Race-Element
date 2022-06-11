8e3ede41501b1920a188be8381a2acc906ffaccc
RiddleTime <reinierklarenberg@gmail.com>
2022-06-11 15:54:37 +0200

    add obs websocket dotnet
    for eventual setup hider

---
+++ ACC_Manager.Data.ACC/Tracker/ObsSetupMenuTracker.cs
@@ -0,0 +1,29 @@
+﻿using OBSWebsocketDotNet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACCManager.Data.ACC.Tracker
+{
+    internal class ObsSetupMenuTracker
+    {
+        public static ObsSetupMenuTracker _instance;
+        public static ObsSetupMenuTracker Instance
+        {
+            get
+            {
+                if (_instance == null) _instance = new ObsSetupMenuTracker();
+                return _instance;
+            }
+        }
+
+        private OBSWebsocket _obsWebSocket;
+
+        private ObsSetupMenuTracker()
+        {
+            _obsWebSocket = new OBSWebsocket();
+        }
+    }
+}
