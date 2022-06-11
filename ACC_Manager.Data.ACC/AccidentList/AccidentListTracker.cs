ad110afa417a455824ad7927f97913f9ba7a6dde
Florian <Florian@MIRACULIX>
2022-06-11 21:50:27 +0200

    accident tracker with historic data

--- ACC_Manager.Data.ACC/AccidentList/AccidentListTracker.cs
+++ ACC_Manager.Data.ACC/AccidentList/AccidentListTracker.cs
@@ -5,16 +5,23 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using static ACCManager.ACCSharedMemory;
 
 namespace ACCManager.Data.ACC.AccidentList
 {
-    public class AccidentListTracker
+    public sealed class AccidentListTracker
     {
         private static AccidentListTracker _instance;
-        private float _trackDistance = 0f;
+        private RealtimeUpdate _realtimeUpdate;
+        private float _trackDistance;
+        private DateTime _accidentTime = DateTime.MinValue;
 
-        // the key into the dictionary is the timeMS of the first accident event
-        private Dictionary<int, AccidentData> _accidentDataList = new Dictionary<int, AccidentData>();
+        // History of realtime car updates
+        // Maps session time to a map of carID and realTimeInfo
+        private Dictionary<double, Dictionary<int, RealtimeCarUpdate>> _realTimeCarHistory = new Dictionary<double, Dictionary<int, RealtimeCarUpdate>>();
+
+        private List<CarInfoWithRealTime> _unprocessedAccidents = new List<CarInfoWithRealTime>();
+        //private Dictionary<double, CarInfoWithRealTime> _unprocessedAccidents = new Dictionary<double, CarInfoWithRealTime>();
 
         public static AccidentListTracker Instance
         {
@@ -25,187 +32,190 @@ namespace ACCManager.Data.ACC.AccidentList
             }
             private set { _instance = value; }
         }
-        /*
-        public List<List<BroadcastingEvent>> GetNewAccidents()
-        {
 
-            List<List<BroadcastingEvent>> list = new List<List<BroadcastingEvent>>();
-            DateTime currentTime = DateTime.Now;
-
-            lock (_accidentDataList)
-            {
-                foreach (var accidentDataKV in _accidentDataList.ToList())
-                {
-                    int key = accidentDataKV.Key;
-                    // return accident data older than 1.5 sec.
-                    if ((currentTime - _accidentDataList[key].Timestamp).TotalSeconds > 1.5)
-                    {
-                        //list.Add(_accidentDataList[key].EventList); // TOOD
-                        _accidentDataList.Remove(key);
-                    }
-                }
-            }
-
-            return list;
-        }
-        */
         internal void Start()
         {
-            BroadcastTracker.Instance.OnBroadcastEvent += Broadcast_EventHandler;
             BroadcastTracker.Instance.OnRealTimeCarUpdate += RealTimeCarUpdate_EventHandler;
+            BroadcastTracker.Instance.OnRealTimeUpdate += RealTimeUpdate_EventHandler;
+            BroadcastTracker.Instance.OnBroadcastEvent += BroadcastEvent_EventHandler;
             BroadcastTracker.Instance.OnTrackDataUpdate += TrackDataUpdate_EventHandler;
         }
 
         internal void Stop()
         {
-            BroadcastTracker.Instance.OnBroadcastEvent -= Broadcast_EventHandler;
             BroadcastTracker.Instance.OnRealTimeCarUpdate -= RealTimeCarUpdate_EventHandler;
+            BroadcastTracker.Instance.OnRealTimeUpdate -= RealTimeUpdate_EventHandler;
+            BroadcastTracker.Instance.OnBroadcastEvent -= BroadcastEvent_EventHandler;
             BroadcastTracker.Instance.OnTrackDataUpdate -= TrackDataUpdate_EventHandler;
-            _accidentDataList?.Clear();
         }
 
-        private void TrackDataUpdate_EventHandler(object sender, TrackData trackData)
-        {
-            //if (_trackDistance != 0) return;
-            Debug.WriteLine($"receive track data update, track id=({trackData.TrackId}) track distance=({trackData.TrackMeters})");
-            _trackDistance = trackData.TrackMeters;
-        }
-
-        private void RealTimeCarUpdate_EventHandler(object sender, RealtimeCarUpdate realtimeCarUpdate)
-        {
-
-            AddRealtimeCarUpdate(realtimeCarUpdate);
-            CheckAccidentDistance();
-
-        }
-
-        private void CheckAccidentDistance()
-        {
-            // if we have no track distance, we can not calculate the distance between the accident cars
-            if (_trackDistance == 0) return;
-
-            DateTime currentTime = DateTime.Now;
-
-            foreach (var accidentDataKV in _accidentDataList)
-            {
-                int key = accidentDataKV.Key;
-                // check distance for accidents older than 1.5 sec.
-                if ((currentTime - _accidentDataList[key].Timestamp).TotalSeconds > 1.5)
-                {
-                    
-
-                    var accidentEventList = _accidentDataList[key].AccidentEventList;
-                    // accident event with only one car can be ignored
-                    if (accidentEventList.Count < 2)
-                    {
-                        continue;
-                    }
-
-                    for (int i= 0; i < accidentEventList.Count-1; i++)
-                    {
-                        float distance = (accidentEventList[i].ReadlTimeCarUpdate.SplinePosition - accidentEventList[i + 1].ReadlTimeCarUpdate.SplinePosition) * _trackDistance;
-                        _accidentDataList[key].DistanceList.Add(distance);
-                        //Debug.WriteLine($"car distance accident=({key}) car1=({accidentEventList[i].ReadlTimeCarUpdate.CarIndex}) car2=({accidentEventList[i + 1].ReadlTimeCarUpdate.CarIndex}) distance=({distance})");
-                    }
-
-                    if (_accidentDataList[key].DistanceList.Count == accidentEventList.Count - 1)
-                    {
-                        Debug.WriteLine($"distance list: {string.Join(" ", _accidentDataList[key].DistanceList)}");
-                    }
-
-                }
-            }
-        }
-
-        private void AddRealtimeCarUpdate(RealtimeCarUpdate realtimeCarUpdate)
-        {
-            // check if there is an accident in our list where no realtime car update is set
-            DateTime currentTime = DateTime.Now;
-
-            foreach (var accidentDataKV in _accidentDataList)
-            {
-                int key = accidentDataKV.Key;
-                // irgnore all accident data older than 1 sec.
-                if ((currentTime - _accidentDataList[key].Timestamp).TotalSeconds < 1.0)
-                {
-                    foreach (var accidentEvent in _accidentDataList[key].AccidentEventList)
-                    {
-                        if (accidentEvent.ReadlTimeCarUpdate.CarIndex == 0 && accidentEvent.BroadcastingEvent.CarData.CarIndex == realtimeCarUpdate.CarIndex)
-                        {
-                            Debug.WriteLine($"add realtime car update for car {realtimeCarUpdate.CarIndex} to accident {key} events");
-                            accidentEvent.ReadlTimeCarUpdate = realtimeCarUpdate;
-                        }
-                    }
-                }
-            }
-
-        }
-
-        private void Broadcast_EventHandler(object sender, BroadcastingEvent broadcastingEvent)
+        private void BroadcastEvent_EventHandler(object sender, BroadcastingEvent broadcastingEvent)
         {
             switch (broadcastingEvent.Type)
             {
                 case BroadcastingCarEventType.Accident:
                     {
-
-                        if (broadcastingEvent.CarData == null || broadcastingEvent.Msg == null)
-                            break;
-
-                        Debug.WriteLine($"#{broadcastingEvent.CarData.RaceNumber}|{broadcastingEvent.TimeMs}| {broadcastingEvent.CarData.GetCurrentDriverName()} had an accident. {broadcastingEvent.Msg}");
-
-                        var added = false;
-
-                        lock (_accidentDataList)
-                        {
-                            foreach (var key in _accidentDataList.Keys)
-                            {
-                                //Debug.WriteLine($"- time diff {broadcastingEvent.TimeMs} - {accidentTimeData.accidentTimeMS} = {(broadcastingEvent.TimeMs - accidentTimeData.accidentTimeMS)}");
-                                // Group all accidents within 1 sec. together
-                                // If an accident occurs at the same time on different track positions, we have only one accident.
-                                if ((broadcastingEvent.TimeMs - key) < 1000)
-                                {
-                                    Debug.WriteLine($"add to existing group {key} size: {_accidentDataList[key].AccidentEventList.Count}");
-                                    added = true;
-                                    _accidentDataList[key].AccidentEventList.Add(new AccidentEvents() { BroadcastingEvent = broadcastingEvent, ReadlTimeCarUpdate = new RealtimeCarUpdate()});
-                                }
-                            }
-
-                            if (!added)
-                            {
-                                Debug.WriteLine($"create new accident group group {broadcastingEvent.TimeMs}");
-                                
-                                AccidentData accidentData = new AccidentData()
-                                {
-                                    AccidentTimeMS = broadcastingEvent.TimeMs,
-                                    Timestamp = DateTime.Now,
-                                };
-                                accidentData.AccidentEventList.Add(new AccidentEvents() { BroadcastingEvent = broadcastingEvent, ReadlTimeCarUpdate = new RealtimeCarUpdate() });
-                                _accidentDataList[broadcastingEvent.TimeMs] = accidentData;
-                            }
-                        }
-                        
-
+                        HandleAccidentEvent(broadcastingEvent);
                         break;
                     }
             }
-
         }
 
-        public class AccidentData
+        private void HandleAccidentEvent(BroadcastingEvent broadcastingEvent)
         {
-            public DateTime Timestamp { get; internal set; }
-            public int AccidentTimeMS { get; internal set; }
-            public List<float> DistanceList = new List<float>();
-            public List<AccidentEvents> AccidentEventList = new List<AccidentEvents>();
+
+            if (broadcastingEvent.CarData == null) return;
+
+            Debug.WriteLine($"#{broadcastingEvent.CarData.RaceNumber}|{broadcastingEvent.TimeMs}| {broadcastingEvent.CarData.GetCurrentDriverName()} had an accident. {broadcastingEvent.Msg}");
+
+            double key = GetSessionTimeKey();
+            // accident events seems to be 5000 ms too late
+            double accidentTime = key - 5000;
+
+            //Debug.WriteLine($"session time {key} accident time {accidentTime}");
+            if (!_realTimeCarHistory.ContainsKey(key))
+            {
+                Debug.WriteLine($"! no history data for key=({key})");
+                return;
+            }
+
+            CarInfoWithRealTime carInfoWithRealTime = new CarInfoWithRealTime(broadcastingEvent.CarData);
+            carInfoWithRealTime.RealtimeCarUpdate = _realTimeCarHistory[key][broadcastingEvent.CarId];
+            lock(_unprocessedAccidents)
+            {
+                _unprocessedAccidents.Add(carInfoWithRealTime);
+                Debug.WriteLine($"unprocessed accidents list size: {_unprocessedAccidents.Count}");
+            }
+            
+
+            if (_accidentTime == DateTime.MinValue)
+            {
+                _accidentTime = DateTime.Now;
+                Debug.WriteLine($"set new accident timestamp: {_accidentTime}");
+            }
+                
+
         }
 
-        public class AccidentEvents
+        private void ProcessAccidentData()
         {
-            public BroadcastingEvent BroadcastingEvent { set; get; }
-            public RealtimeCarUpdate ReadlTimeCarUpdate { set; get; }
+            DateTime processTime = DateTime.Now;
+            // Debug.WriteLine($"process old accident data list size {processTime.Millisecond - _accidentTime.Millisecond} ...");
+            // process accidents older than 1 sec.
+            TimeSpan timediff = processTime - _accidentTime;
+           //Debug.WriteLine($"process old accident data list size {(int)timediff.TotalMilliseconds} ...");
+            if (_accidentTime != DateTime.MinValue && timediff.TotalMilliseconds > 1000)
+            {
+                Debug.WriteLine($"process old accident data list size {_unprocessedAccidents.Count} ...");
+                
+                if (_unprocessedAccidents.Count < 2)
+                {
+                    Debug.WriteLine($"accident: car {_unprocessedAccidents[0].RaceNumber}|{_unprocessedAccidents[0].GetCurrentDriverName()}");
+                }
+
+                for (int i=0; i<_unprocessedAccidents.Count-1; i++)
+                {
+                    float distance = (_unprocessedAccidents[i].RealtimeCarUpdate.SplinePosition - _unprocessedAccidents[i + 1].RealtimeCarUpdate.SplinePosition) * _trackDistance;
+                    Debug.WriteLine($"accident: car {_unprocessedAccidents[i].RaceNumber} vs {_unprocessedAccidents[i+1].RaceNumber} distance {distance}");
+                }
+
+                lock(_unprocessedAccidents)
+                {
+                    Debug.WriteLine($"clear unprocessed accident list and reset accident timestamp");
+                    _accidentTime = DateTime.MinValue;
+                    _unprocessedAccidents.Clear();
+                }
+            }
+
+           // double key = GetSessionTimeKey();
+
         }
 
+        private void RealTimeCarUpdate_EventHandler(object sender, RealtimeCarUpdate realtimeCarUpdate)
+        {
+            AddCarUpdateToHistory(realtimeCarUpdate);
+            RemoveOldHistoryData();
+            ProcessAccidentData();
+        }
+
+        private void RemoveOldHistoryData()
+        {
+            double key = GetSessionTimeKey();
+            foreach (var kv in _realTimeCarHistory.ToList())
+            {
+                if (key - kv.Key > 20000)
+                {
+                    _realTimeCarHistory.Remove(kv.Key);
+                }
+            }
+        }
+
+        private void AddCarUpdateToHistory(RealtimeCarUpdate realtimeCarUpdate)
+        {
+            double key = GetSessionTimeKey();
+            if (key == 0) return; // session not started
+
+            if (_realTimeCarHistory.ContainsKey(key)) 
+            {
+                //Debug.WriteLine($"+ add to key {key} car index {realtimeCarUpdate.CarIndex}");
+                var carUpdate = _realTimeCarHistory[key];
+                if (carUpdate.ContainsKey(realtimeCarUpdate.CarIndex))
+                {
+                    carUpdate[realtimeCarUpdate.CarIndex] = realtimeCarUpdate;
+                }
+                else
+                {
+                    _realTimeCarHistory[key].Add(realtimeCarUpdate.CarIndex, realtimeCarUpdate);
+                }
+                
+            } 
+            else 
+            {
+                Dictionary<int, RealtimeCarUpdate> carUpdate = new Dictionary<int, RealtimeCarUpdate>();
+                carUpdate[realtimeCarUpdate.CarIndex] = realtimeCarUpdate;
+                //Debug.WriteLine($"# create key {key} car index {realtimeCarUpdate.CarIndex}");
+                _realTimeCarHistory[key] = carUpdate;
+            }
+        }
+
+        private double GetSessionTimeKey()
+        {
+            return _realtimeUpdate.SessionTime.TotalMilliseconds;
+        }
+
+        private void RealTimeUpdate_EventHandler(object sender, RealtimeUpdate realtimeUpdate)
+        {
+            _realtimeUpdate = realtimeUpdate;
+        }
+
+        private void TrackDataUpdate_EventHandler(object sender, TrackData trackData)
+        {
+            Debug.WriteLine($"receive track data update, track id=({trackData.TrackId}) track distance=({trackData.TrackMeters})");
+            _trackDistance = trackData.TrackMeters;
+        }
+
+        private class CarInfoWithRealTime : CarInfo
+        {
+            public RealtimeCarUpdate RealtimeCarUpdate { get; set; }
+
+            public CarInfoWithRealTime(CarInfo carInfoIn) : base(carInfoIn.CarIndex)
+            {
+                CarModelType = carInfoIn.CarModelType;
+                TeamName = carInfoIn.TeamName;
+                RaceNumber = carInfoIn.RaceNumber;
+                CupCategory = carInfoIn.CupCategory;
+                CurrentDriverIndex = carInfoIn.CurrentDriverIndex;
+                Nationality = carInfoIn.Nationality;
+            }
+
+            public CarInfoWithRealTime(ushort carIndex) : base(carIndex)
+            {
+            }
+        }
+
+        private class ContactData
+        {
+            public double SessionTime { get; set; }
+        }
     }
-
-
 }
