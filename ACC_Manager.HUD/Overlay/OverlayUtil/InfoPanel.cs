ac0fd190a8947351a016a9aee0b32eb771723f7a
RiddleTime <reinierklarenberg@gmail.com>
2022-10-13 04:19:00 +0200

    update: infopanel
    - increase shadow distance to 1f for CenterTextedProgressBarLine

--- ACC_Manager.HUD/Overlay/OverlayUtil/InfoPanel.cs
+++ ACC_Manager.HUD/Overlay/OverlayUtil/InfoPanel.cs
@@ -133,7 +133,7 @@ namespace ACCManager.HUD.Overlay.Util
                         progressBar.Draw(g, bar.BarColor, Brushes.Transparent, new Rectangle(X + 3, Y + counter * FontHeight + 1, (int)MaxWidth - 6, (int)FontHeight - 2), false, false);
 
                         SizeF textWidth = g.MeasureString(bar.CenteredText, _font);
-                        g.DrawStringWithShadow($"{bar.CenteredText}", _font, Brushes.White, new PointF(X + MaxWidth / 2 - textWidth.Width / 2, Y + counter * (this.FontHeight + ExtraLineSpacing) + _addMonoY));
+                        g.DrawStringWithShadow($"{bar.CenteredText}", _font, Color.White, new PointF(X + MaxWidth / 2 - textWidth.Width / 2, Y + counter * (this.FontHeight + ExtraLineSpacing) + _addMonoY), 1f);
                     }
 
                     if (line.GetType() == typeof(CenteredTextedDeltabarLine))
