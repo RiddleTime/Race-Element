6548145798b090877ffad0ebcc54b258e6b5810b
RiddleTime <reinierklarenberg@gmail.com>
2022-10-17 03:29:22 +0200

    feature: add min speed to speedometer

--- ACC_Manager.HUD.ACC/Overlays/OverlaySpeedometer/SpeedometerOverlay.cs
+++ ACC_Manager.HUD.ACC/Overlays/OverlaySpeedometer/SpeedometerOverlay.cs
@@ -4,6 +4,7 @@ using ACCManager.Data.ACC.Tracker.Laps;
 using ACCManager.HUD.Overlay.Configuration;
 using ACCManager.HUD.Overlay.Internal;
 using ACCManager.HUD.Overlay.Util;
+using System;
 using System.Drawing;
 
 namespace ACCManager.HUD.ACC.Overlays.OverlaySpeedometer
@@ -17,6 +18,9 @@ namespace ACCManager.HUD.ACC.Overlays.OverlaySpeedometer
             [ToolTip("Displays the maximum speed reached on each lap.")]
             public bool ShowMaxSpeed { get; set; } = false;
 
+            [ToolTip("Displays the minimum speed reached on each lap.")]
+            public bool ShowMinSpeed { get; set; } = false;
+
             public SpeedometerConfiguration()
             {
                 AllowRescale = true;
@@ -25,6 +29,7 @@ namespace ACCManager.HUD.ACC.Overlays.OverlaySpeedometer
 
         private readonly InfoPanel _panel;
         private float _maxSpeed = 0;
+        private float _minSpeed = 0;
 
         public SpeedometerOverlay(Rectangle rectangle) : base(rectangle, "Speedometer Overlay")
         {
@@ -33,27 +38,31 @@ namespace ACCManager.HUD.ACC.Overlays.OverlaySpeedometer
             {
                 FirstRowLine = 1
             };
-            this.Height = _panel.FontHeight * 2 + 1;
+            this.Height = _panel.FontHeight * 3 + 1;
         }
 
         public sealed override void BeforeStart()
         {
 
+            if (_config.ShowMaxSpeed || _config.ShowMinSpeed)
+                LapTracker.Instance.LapFinished += OnLapFinished;
+
+            if (!_config.ShowMinSpeed)
+                this.Height -= _panel.FontHeight;
             if (!_config.ShowMaxSpeed)
                 this.Height -= _panel.FontHeight;
-            else
-                LapTracker.Instance.LapFinished += OnLapFinished;
         }
 
         public sealed override void BeforeStop()
         {
-            if (_config.ShowMaxSpeed)
+            if (_config.ShowMinSpeed || _config.ShowMaxSpeed)
                 LapTracker.Instance.LapFinished -= OnLapFinished;
         }
 
         private void OnLapFinished(object sender, DbLapData e)
         {
             _maxSpeed.ClipMax(pagePhysics.SpeedKmh);
+            _minSpeed.ClipMin(pagePhysics.SpeedKmh);
         }
 
         public sealed override void Render(Graphics g)
@@ -64,6 +73,12 @@ namespace ACCManager.HUD.ACC.Overlays.OverlaySpeedometer
             {
                 _maxSpeed.ClipMin(pagePhysics.SpeedKmh);
                 _panel.AddLine("Max", $"{_maxSpeed:F2}");
+            }
+
+            if (_config.ShowMinSpeed)
+            {
+                _minSpeed.ClipMax(pagePhysics.SpeedKmh);
+                _panel.AddLine("Min", $"{_minSpeed:F2}");
             }
 
             _panel.Draw(g);
