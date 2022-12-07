9ca72cc09f867b0fb05fc46d3cff6d70d9c51de9
RiddleTime <reinierklarenberg@gmail.com>
2022-12-07 04:04:24 +0100

    Update AssemblyInfo.cs

--- ACC_Manager/Properties/AssemblyInfo.cs
+++ ACC_Manager/Properties/AssemblyInfo.cs
@@ -45,7 +45,7 @@ using System.Windows;
 //      Minor Version
 //      Build Number
 //      Revision
-[assembly: AssemblyVersion("0.1.2.3")]
-[assembly: AssemblyFileVersion("0.1.2.3")]
-[assembly: Guid("6f44e635-5134-44dd-8638-088a881e4a76")]
+[assembly: AssemblyVersion("0.1.3.0")]
+[assembly: AssemblyFileVersion("0.1.3.0")]
+[assembly: Guid("6f44e636-5134-44dd-8638-088a881e4a76")]
 [assembly: NeutralResourcesLanguage("")]
