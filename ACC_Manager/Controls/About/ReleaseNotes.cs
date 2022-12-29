8e6be7407de7b2a2bc51277afbfab6c1c8f261f1
RiddleTime <reinierklarenberg@gmail.com>
2022-12-29 22:52:42 +0100

    update releasenotes

--- ACC_Manager/Controls/About/ReleaseNotes.cs
+++ ACC_Manager/Controls/About/ReleaseNotes.cs
@@ -6,8 +6,9 @@ namespace RaceElement.Controls
     {
         internal readonly static Dictionary<string, string> Notes = new Dictionary<string, string>()
         {
-            {"0.1.4.2", "- Renamed app to Race Element"+
-                        "\n- Removed request for admin rights, if you have run as admin enabled, disable it."},
+            {"0.1.5.0", "- ACC Manager is now known as Race Element."+
+                        "\n- Removed request for admin rights, if you have run as admin enabled, disable it."+
+                        "\n- You can copy all the entire contents of the ACC Manager folder to the Race Element folder. This to keep all your data and settings."},
             {"0.1.4.1", "- Added request for admin rights, the app uses this to detect whether ACC is running. Without these rights you may experience unexpected app crashes." },
             {"0.1.4.0", "- Reworked hud internals."+
                         "\n- Reduced idle cpu usage to almost 0% when ACC is not running."},
