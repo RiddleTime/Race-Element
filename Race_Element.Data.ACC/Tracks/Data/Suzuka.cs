2f66c095a81b147e05cfcf6858b35fac4e4fd319
KrisV147 <secondlife_builder@hotmail.co.uk>
2023-01-14 00:44:35 +0000

    Suzuka
    Corner names

--- Race_Element.Data.ACC/Tracks/Data/Suzuka.cs
+++ Race_Element.Data.ACC/Tracks/Data/Suzuka.cs
@@ -14,6 +14,20 @@ namespace ACCManager.Data.ACC.Tracks.Data
         public override string FullName => "Suzuka Circuit";
         public override int TrackLength => 5807;
 
-        public override Dictionary<FloatRangeStruct, string> CornerNames => new Dictionary<FloatRangeStruct, string>();
+        public override Dictionary<FloatRangeStruct, string> CornerNames => new Dictionary<FloatRangeStruct, string>()
+        {
+            { new FloatRangeStruct(0.03883164f, 0.08408849f), "First Corner" },
+            { new FloatRangeStruct(0.1298746f, 0.1984839f), "Snake" },
+            { new FloatRangeStruct(0.1984840f, 0.2325576f), "Anti-Banked Curve" },
+            { new FloatRangeStruct(0.2386938f, 0.3178378f), "Dunlop" },
+            { new FloatRangeStruct(0.3310606f, 0.3528073f), "Degner 1" },
+            { new FloatRangeStruct(0.3596497f, 0.3821299f), "Degner 2" },
+            { new FloatRangeStruct(0.4306207f, 0.4615963f), "Hairpin" },
+            { new FloatRangeStruct(0.4827491f, 0.5779635f), "200R" },
+            { new FloatRangeStruct(0.5862223f, 0.6618876f), "Spoon Curve" },
+            { new FloatRangeStruct(0.7896389f, 0.8385026f), "130R" },
+            { new FloatRangeStruct(0.8555256f, 0.8949211f), "Casio Triangle" },
+            { new FloatRangeStruct(0.8986408f, 0.9453408f), "Last Curve" }
+        };
     }
 }
