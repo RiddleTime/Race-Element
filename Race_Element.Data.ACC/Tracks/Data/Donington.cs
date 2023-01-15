e766af089f1ae6a3e5f6b1bf94a07ec7e27fbf52
RiddleTime <reinierklarenberg@gmail.com>
2023-01-15 11:18:09 +0100

    add donington park track corners

--- Race_Element.Data.ACC/Tracks/Data/Donington.cs
+++ Race_Element.Data.ACC/Tracks/Data/Donington.cs
@@ -14,6 +14,19 @@ namespace ACCManager.Data.ACC.Tracks.Data
         public override string FullName => "Donington Park";
         public override int TrackLength => 4020;
 
-        public override Dictionary<FloatRangeStruct, string> CornerNames => new Dictionary<FloatRangeStruct, string>();
+        public override Dictionary<FloatRangeStruct, string> CornerNames => new Dictionary<FloatRangeStruct, string>()
+        {
+            { new FloatRangeStruct(0.0772981f, 0.1404632f), "Redgate" },
+            { new FloatRangeStruct(0.1477933f, 0.1860932f), "Hollywood" },
+            { new FloatRangeStruct(0.1924699f, 0.2584147f), "Craner Curves" },
+            { new FloatRangeStruct(0.2683981f, 0.3242396f), "Old Hairpin" },
+            { new FloatRangeStruct(0.340085f, 0.3834941f), "Starkey's Bridge" },
+            { new FloatRangeStruct(0.3931186f, 0.4279727f), "Schwantz Curve" },
+            { new FloatRangeStruct(0.3931186f, 0.4847209f), "McLeans" },
+            { new FloatRangeStruct(0.5150666f, 0.6010549f), "Coppice" },
+            { new FloatRangeStruct(0.7011805f, 0.7645504f), "Fogarty Esses" },
+            { new FloatRangeStruct(0.8194078f, 0.8732706f), "Melbourne Hairpin" },
+            { new FloatRangeStruct(0.9239673f, 0.9740674f), "Goddards" }
+        };
     }
 }
