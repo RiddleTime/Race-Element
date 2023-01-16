b9e3d98dd632b9e47ae452dad40b2b71702ac929
RiddleTime <reinierklarenberg@gmail.com>
2023-01-16 14:40:42 +0100

    add indianapolis motor speedway track corners

--- Race_Element.Data.ACC/Tracks/Data/Indianapolis.cs
+++ Race_Element.Data.ACC/Tracks/Data/Indianapolis.cs
@@ -14,6 +14,23 @@ namespace ACCManager.Data.ACC.Tracks.Data
         public override string FullName => "Indianapolis Motor Speedway";
         public override int TrackLength => 4167;
 
-        public override Dictionary<FloatRangeStruct, string> CornerNames => new Dictionary<FloatRangeStruct, string>();
+        public override Dictionary<FloatRangeStruct, string> CornerNames => new Dictionary<FloatRangeStruct, string>()
+        {
+            { new FloatRangeStruct(0.1243783f, 0.1692718f), "Turn 1" },
+            { new FloatRangeStruct(0.1696899f, 0.195541f), "Turn 2" },
+            { new FloatRangeStruct(0.195921f, 0.2312392f), "Turn 3" },
+            { new FloatRangeStruct(0.2340903f, 0.2812699f), "Turn 4" },
+            { new FloatRangeStruct(0.2904321f, 0.325332f), "Turn 5" },
+            { new FloatRangeStruct(0.3372692f, 0.3911017f), "Turn 6" },
+            { new FloatRangeStruct(0.3939147f, 0.4355434f), "Turn 7" },
+            { new FloatRangeStruct(0.5369371f, 0.5751812f), "Turn 8" },
+            { new FloatRangeStruct(0.5837724f, 0.6061648f), "Turn 9" },
+            { new FloatRangeStruct(0.6077616f, 0.6273403f), "Turn 10" },
+            { new FloatRangeStruct(0.6314081f, 0.6756219f), "Turn 11" },
+            { new FloatRangeStruct(0.6912087f, 0.7700945f), "Turn 12" },
+            { new FloatRangeStruct(0.7792981f, 0.8153729f), "Turn 13" },
+            { new FloatRangeStruct(0.8205053f, 0.8587886f), "Turn 14" },
+            { new FloatRangeStruct(0.8633122f, 0.9517404f), "Turn 15" },
+        };
     }
 }
