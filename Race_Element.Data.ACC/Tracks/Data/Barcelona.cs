ea852096957272b3da84415c06400369fec1bae5
RiddleTime <reinierklarenberg@gmail.com>
2023-01-19 16:03:05 +0100

    add corner numbers for barcelona corners

--- Race_Element.Data.ACC/Tracks/Data/Barcelona.cs
+++ Race_Element.Data.ACC/Tracks/Data/Barcelona.cs
@@ -16,17 +16,22 @@ namespace ACCManager.Data.ACC.Tracks.Data
 
         public override Dictionary<FloatRangeStruct, (int, string)> CornerNames => new Dictionary<FloatRangeStruct, (int, string)>()
         {
-            { new FloatRangeStruct(0.1577717f, 0.2195874f), (1, "Elf") },
-            { new FloatRangeStruct(0.2282194f, 0.3202321f), (1, "Renault") },
-            { new FloatRangeStruct(0.3504574f, 0.4274578f), (1, "Repsol") },
-            { new FloatRangeStruct(0.4452616f, 0.4849422f), (1, "Seat") },
-            { new FloatRangeStruct(0.5345824f, 0.5912151f), (1, "Würth") },
-            { new FloatRangeStruct(0.6078672f, 0.6661669f), (1, "Campsa") },
-            { new FloatRangeStruct(0.7314663f, 0.7708687f), (1, "La Caixa") },
-            { new FloatRangeStruct(0.7779655f, 0.8429551f), (1, "Banc de Sabadell") },
-            { new FloatRangeStruct(0.8548293f, 0.8812364f), (1, "Europcar") },
-            { new FloatRangeStruct(0.8875112f, 0.9133028f), (1, "Chicane RACC") },
-            { new FloatRangeStruct(0.917703f, 0.9730397f), (1, "New Holland") }
+            { new FloatRangeStruct(0.1577717f, 0.1954681f), (1, "Elf") },
+            { new FloatRangeStruct(0.1967648f, 0.2195874f), (2, "Elf") },
+            { new FloatRangeStruct(0.2282194f, 0.3202321f), (3, "Renault") },
+            { new FloatRangeStruct(0.3504574f, 0.4274578f), (4, "Repsol") },
+            { new FloatRangeStruct(0.4452616f, 0.4849422f), (5, "Seat") },
+            { new FloatRangeStruct(0.5049684f, 0.5296696f), (6, string.Empty) },
+            { new FloatRangeStruct(0.5345824f, 0.5626252f), (7, "Würth") },
+            { new FloatRangeStruct(0.5643317f, 0.5912151f), (8, "Würth") },
+            { new FloatRangeStruct(0.6078672f, 0.6661669f), (9, "Campsa") },
+            { new FloatRangeStruct(0.7314663f, 0.7708687f), (10, "La Caixa") },
+            { new FloatRangeStruct(0.7779655f, 0.792363f), (11, "Banc de Sabadell") },
+            { new FloatRangeStruct(0.7934548f, 0.8429551f), (12, "Banc de Sabadell") },
+            { new FloatRangeStruct(0.8548293f, 0.8812364f), (13, "Europcar") },
+            { new FloatRangeStruct(0.8875112f, 0.8994124f), (14, "Chicane RACC") },
+            { new FloatRangeStruct(0.9000266f, 0.9133028f), (15, "Chicane RACC") },
+            { new FloatRangeStruct(0.917703f, 0.9730397f), (16, "New Holland") }
         };
     }
 }
