bcbc0d6cee7bb68022665db909f30cb17115623d
RiddleTime <reinierklarenberg@gmail.com>
2023-01-26 23:00:58 +0100

    Update StartScreenOverlay.cs
    add caching and improved render quality

--- Race_Element.HUD.ACC/Overlays/OverlayStartScreen/StartScreenOverlay.cs
+++ Race_Element.HUD.ACC/Overlays/OverlayStartScreen/StartScreenOverlay.cs
@@ -3,12 +3,14 @@ using RaceElement.HUD.Overlay.OverlayUtil;
 using RaceElement.HUD.Overlay.Util;
 using System.Drawing;
 using System.Drawing.Drawing2D;
+using System.Drawing.Text;
 
 namespace RaceElement.HUD.ACC.Overlays.OverlayStartScreen
 {
     public class StartScreenOverlay : AbstractOverlay
     {
-        private Font _font;
+        private CachedBitmap cachedBitmap;
+
         public StartScreenOverlay(Rectangle rectangle) : base(rectangle, "Start Screen")
         {
             this.X = rectangle.X;
@@ -18,16 +20,24 @@ namespace RaceElement.HUD.ACC.Overlays.OverlayStartScreen
             this.RefreshRateHz = 1;
         }
 
-        public override void BeforeStart() => _font = FontUtil.FontConthrax(15);
-        public override void BeforeStop() => _font.Dispose();
+        public override void BeforeStart()
+        {
+            cachedBitmap = new CachedBitmap(this.Width, this.Height, g =>
+            {
+                var brush = new LinearGradientBrush(new Point(0, 0), new Point(Width, 0), Color.FromArgb(225, Color.Black), Color.FromArgb(185, Color.Black));
+                g.FillRoundedRectangle(brush, new Rectangle(0, 0, Width, Height), 6);
+
+                g.TextRenderingHint = TextRenderingHint.AntiAlias;
+                Font font = FontUtil.FontConthrax(15);
+                g.DrawStringWithShadow("Race Element is starting...", font, Brushes.White, new Point(0, Height / 2 - font.Height / 2));
+                font.Dispose();
+            });
+        }
+
+        public override void BeforeStop() => cachedBitmap?.Dispose();
 
         public override bool ShouldRender() => true;
 
-        public override void Render(Graphics g)
-        {
-            var brush = new LinearGradientBrush(new Point(0, 0), new Point(Width, 0), Color.FromArgb(225, Color.Black), Color.FromArgb(185, Color.Black));
-            g.FillRoundedRectangle(brush, new Rectangle(0, 0, Width, Height), 6);
-            g.DrawStringWithShadow("Race Element is starting...", _font, Brushes.White, new Point(0, Height / 2 - _font.Height / 2));
-        }
+        public override void Render(Graphics g) => cachedBitmap?.Draw(g);
     }
 }
