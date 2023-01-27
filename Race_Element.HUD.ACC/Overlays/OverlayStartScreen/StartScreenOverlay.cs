dc6888042071bb749a7f2094afcc7adbe82c0dcf
RiddleTime <reinierklarenberg@gmail.com>
2023-01-27 15:07:25 +0100

    Update StartScreenOverlay.cs
    update simple splash screen

--- Race_Element.HUD.ACC/Overlays/OverlayStartScreen/StartScreenOverlay.cs
+++ Race_Element.HUD.ACC/Overlays/OverlayStartScreen/StartScreenOverlay.cs
@@ -16,7 +16,7 @@ namespace RaceElement.HUD.ACC.Overlays.OverlayStartScreen
             this.X = rectangle.X;
             this.Y = rectangle.Y;
             this.Width = 350;
-            this.Height = 30;
+            this.Height = 45;
             this.RefreshRateHz = 1;
         }
 
@@ -24,13 +24,25 @@ namespace RaceElement.HUD.ACC.Overlays.OverlayStartScreen
         {
             cachedBitmap = new CachedBitmap(this.Width, this.Height, g =>
             {
-                var brush = new LinearGradientBrush(new Point(0, 0), new Point(Width, 0), Color.FromArgb(225, Color.Black), Color.FromArgb(185, Color.Black));
-                g.FillRoundedRectangle(brush, new Rectangle(0, 0, Width, Height), 6);
+                Rectangle rectangle = new Rectangle(0, 0, Width - 1, Height - 1);
+                var brush = new LinearGradientBrush(new Point(0, 0), new Point(Width, 0), Color.FromArgb(147, Color.Black), Color.FromArgb(137, Color.Black));
+                g.FillRoundedRectangle(brush, rectangle, 8);
 
-                g.TextRenderingHint = TextRenderingHint.AntiAlias;
-                Font font = FontUtil.FontConthrax(15);
-                g.DrawStringWithShadow("Race Element is starting...", font, Brushes.White, new Point(0, Height / 2 - font.Height / 2));
-                font.Dispose();
+                g.TextRenderingHint = TextRenderingHint.AntiAliasGridFit;
+                g.TextContrast = 1;
+
+                string header = "Race Element is starting";
+                Font font16 = FontUtil.FontConthrax(16);
+                int font16Height = font16.Height;
+                int halfStringLength = (int)(g.MeasureString(header, font16).Width / 2);
+                int x = this.Width / 2 - halfStringLength;
+                g.DrawStringWithShadow(header, font16, Color.FromArgb(215, Color.White), new Point(x, 0), 2f);
+                font16.Dispose();
+
+                string subHeader = "by RiddleTime";
+                Font font11 = FontUtil.FontConthrax(11);
+                g.DrawStringWithShadow(subHeader, font11, Color.FromArgb(185, Color.White), new Point(x + 2, font16Height));
+                font11.Dispose();
             });
         }
 
