9078b81f64d1558ec54863869090710b6d12782e
RiddleTime <reinierklarenberg@gmail.com>
2023-02-10 19:25:32 +0100

    update releasenotes

--- Race_Element/Controls/About/ReleaseNotes.cs
+++ Race_Element/Controls/About/ReleaseNotes.cs
@@ -6,6 +6,7 @@ namespace RaceElement.Controls
     {
         internal readonly static Dictionary<string, string> Notes = new Dictionary<string, string>()
         {
+            {"0.1.6.9", "- Fuel Info HUD: Add option to change fuel bar color based on fuel percentage." },
             {"0.1.6.8", "- HUD:"+
                         "\n  - Fix: Dropdown menus not saving."+
                         "\n  - Input Trace: customizable height."+
--- Race_Element/Properties/AssemblyInfo.cs
+++ Race_Element/Properties/AssemblyInfo.cs
@@ -42,6 +42,6 @@ using System.Windows;
 )]
 
 //      Major Version, Minor Version, Build Number, Revision
-[assembly: AssemblyVersion("0.1.6.8")]
-[assembly: AssemblyFileVersion("0.1.6.8")]
+[assembly: AssemblyVersion("0.1.6.9")]
+[assembly: AssemblyFileVersion("0.1.6.9")]
 [assembly: NeutralResourcesLanguage("")]
