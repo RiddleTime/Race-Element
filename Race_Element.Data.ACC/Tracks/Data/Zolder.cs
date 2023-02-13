fbb3dda144d38bdeb3c02e329434c4cde19967f9
RiddleTime <reinierklarenberg@gmail.com>
2023-02-13 04:48:42 +0100

    added some todos for track data

--- Race_Element.Data.ACC/Tracks/Data/Zolder.cs
+++ Race_Element.Data.ACC/Tracks/Data/Zolder.cs
@@ -16,12 +16,13 @@ namespace ACCManager.Data.ACC.Tracks.Data
 
         public override Dictionary<FloatRangeStruct, (int, string)> CornerNames => new Dictionary<FloatRangeStruct, (int, string)>()
         {
+            // TODO Split up some corners into seperate Turn numbers: http://www.racb-track-marshals.be/images/circuit_zolder.jpg
             { new FloatRangeStruct(0.04245049f, 0.0946003f), (1, "Earste Links") },
-            { new FloatRangeStruct(0.1296046f, 0.1782782f), (1, "Sterrenwachtbocht") },
-            { new FloatRangeStruct(0.17827821f, 0.2351831f), (1, "Kanaalbocht") },
-            { new FloatRangeStruct(0.2595046f, 0.3127186f), (1, "Lucien Bianchibocht") },
-            { new FloatRangeStruct(0.4122366f, 0.4614663f), (1, "Kleine Chicane") },
-            { new FloatRangeStruct(0.5095074f, 0.5512679f), (1, "Butte") },
+            { new FloatRangeStruct(0.1296046f, 0.1782782f), (2, "Sterrenwachtbocht") },
+            { new FloatRangeStruct(0.17827821f, 0.2351831f), (3, "Kanaalbocht") },
+            { new FloatRangeStruct(0.2595046f, 0.3127186f), (4, "Lucien Bianchibocht") },
+            { new FloatRangeStruct(0.4122366f, 0.4614663f), (5, "Kleine Chicane") },
+            { new FloatRangeStruct(0.5095074f, 0.5512679f), (6, "Butte") },
             { new FloatRangeStruct(0.5667552f, 0.6171156f), (1, "Gille Villeneuve Chicane") },
             { new FloatRangeStruct(0.6240816f, 0.6695773f), (1, "Terlamenbocht") },
             { new FloatRangeStruct(0.7329645f, 0.7745231f), (1, "Bolderberghaarspeldbocht") },
