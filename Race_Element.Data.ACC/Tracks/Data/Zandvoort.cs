3293fb66084599b39e367e454e947ea2b2e7ae22
RiddleTime <reinierklarenberg@gmail.com>
2023-02-13 22:06:58 +0100

    finish zandvoort track data

--- Race_Element.Data.ACC/Tracks/Data/Zandvoort.cs
+++ Race_Element.Data.ACC/Tracks/Data/Zandvoort.cs
@@ -1,9 +1,6 @@
 ﻿using RaceElement.Util.DataTypes;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using static RaceElement.Data.ACC.Tracks.TrackData;
 
 namespace ACCManager.Data.ACC.Tracks.Data
@@ -20,14 +17,14 @@ namespace ACCManager.Data.ACC.Tracks.Data
             { new FloatRangeStruct(0.1480813f, 0.1814992f), (2, "Gerlachbocht") },
             { new FloatRangeStruct(0.1887138f, 0.2235506f), (3, "Hugenholzbocht") },
             { new FloatRangeStruct(0.2407838f, 0.2725571f), (4, "Hunzerug") },
-            // TODO: Add Corner 5  https://en.wikipedia.org/wiki/File:Zandvoort_Circuit.png                                                                              
-            { new FloatRangeStruct(0.3335241f, 0.3628302f), (6, "Rob Slotemakerbocht") },
+            { new FloatRangeStruct(0.2839962f,  0.3221979f), (5, "Rob Slotemakerbocht") },
+            { new FloatRangeStruct(0.3257863f, 0.3628302f), (6, "Rob Slotemakerbocht") },
             { new FloatRangeStruct(0.3733709f, 0.4406184f), (7, "Scheivlak") },
             { new FloatRangeStruct(0.4704477f, 0.5094342f), (8, "Mastersbocht") },
             { new FloatRangeStruct(0.5207254f, 0.558478f), (9, "Bocht 9") },
             { new FloatRangeStruct(0.5789623f, 0.6246402f), (10, "Bocht 10") },
-            { new FloatRangeStruct(0.7076252f, 0.7710944f), (11, "Hans Ernst Bocht") },
-            // TODO: Add Corner 12 https://en.wikipedia.org/wiki/File:Zandvoort_Circuit.png
+            { new FloatRangeStruct(0.7076252f, 0.7403311f), (11, "Hans Ernst Bocht") },
+            { new FloatRangeStruct(0.7405928f, 0.7710944f), (12, "Hans Ernst Bocht") },
             { new FloatRangeStruct(0.8137466f, 0.8532166f), (13, "Kumho") },
             { new FloatRangeStruct(0.85321661f, 0.9344479f), (14, "Arie Luyendijk Bocht") }
         };
