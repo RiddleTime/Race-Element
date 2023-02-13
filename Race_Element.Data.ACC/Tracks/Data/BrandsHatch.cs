47d6678203abb4ea87dd80c188ea74415e0ac6e0
RiddleTime <reinierklarenberg@gmail.com>
2023-02-13 04:35:29 +0100

    Update BrandsHatch.cs
    add corner numbers

--- Race_Element.Data.ACC/Tracks/Data/BrandsHatch.cs
+++ Race_Element.Data.ACC/Tracks/Data/BrandsHatch.cs
@@ -17,15 +17,15 @@ namespace ACCManager.Data.ACC.Tracks.Data
         public override Dictionary<FloatRangeStruct, (int, string)> CornerNames => new Dictionary<FloatRangeStruct, (int, string)>()
         {
             { new FloatRangeStruct(0.02583388f, 0.09228041f), (1, "Paddock Hill Bend") },
-            { new FloatRangeStruct(0.128806f, 0.1749151f), (1, "Druids") },
-            { new FloatRangeStruct(0.2037492f, 0.2366925f), (1, "Graham Hill Bend") },
-            { new FloatRangeStruct(0.2918635f, 0.3475159f), (1, "Surtees") },
-            { new FloatRangeStruct(0.4960743f, 0.5494739f), (1, "Hawthorn Bend") },
-            { new FloatRangeStruct(0.5836455f, 0.6203906f), (1, "Westfield Bend") },
-            { new FloatRangeStruct(0.65591f, 0.6856291f), (1, "Dingle Dell") },
-            { new FloatRangeStruct(0.6919114f, 0.7209466f), (1, "Sheene's") },
-            { new FloatRangeStruct(0.7484918f, 0.7844133f), (1, "Stirlings") },
-            { new FloatRangeStruct(0.8544036f, 0.9092523f), (1, "Clark Curve") }
+            { new FloatRangeStruct(0.128806f, 0.1749151f), (2, "Druids") },
+            { new FloatRangeStruct(0.2037492f, 0.2366925f), (3, "Graham Hill Bend") },
+            { new FloatRangeStruct(0.2918635f, 0.3475159f), (4, "Surtees") },
+            { new FloatRangeStruct(0.4960743f, 0.5494739f), (5, "Hawthorn Bend") },
+            { new FloatRangeStruct(0.5836455f, 0.6203906f), (6, "Westfield Bend") },
+            { new FloatRangeStruct(0.65591f, 0.6856291f), (7, "Dingle Dell") },
+            { new FloatRangeStruct(0.6919114f, 0.7209466f), (8, "Sheene's") },
+            { new FloatRangeStruct(0.7484918f, 0.7844133f), (9, "Stirlings") },
+            { new FloatRangeStruct(0.8544036f, 0.9092523f), (10, "Clark Curve") }
         };
     }
 }
