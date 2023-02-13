aea48985baf4b93d1a7f8ea6f88bd2d60c47bb3d
RiddleTime <reinierklarenberg@gmail.com>
2023-02-13 04:38:33 +0100

    Update Donington.cs
    add corner numbers

--- Race_Element.Data.ACC/Tracks/Data/Donington.cs
+++ Race_Element.Data.ACC/Tracks/Data/Donington.cs
@@ -1,9 +1,6 @@
 ﻿using RaceElement.Util.DataTypes;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using static RaceElement.Data.ACC.Tracks.TrackData;
 
 namespace ACCManager.Data.ACC.Tracks.Data
@@ -16,17 +13,17 @@ namespace ACCManager.Data.ACC.Tracks.Data
 
         public override Dictionary<FloatRangeStruct, (int, string)> CornerNames => new Dictionary<FloatRangeStruct, (int, string)>()
         {
-            { new FloatRangeStruct(0.0772981f, 0.1404632f), (1,"Redgate") },
-            { new FloatRangeStruct(0.1477933f, 0.1860932f), (1, "Hollywood") },
-            { new FloatRangeStruct(0.1924699f, 0.2584147f), (1, "Craner Curves") },
-            { new FloatRangeStruct(0.2683981f, 0.3242396f), (1, "Old Hairpin") },
-            { new FloatRangeStruct(0.340085f, 0.3834941f), (1, "Starkey's Bridge") },
-            { new FloatRangeStruct(0.3931186f, 0.4279727f), (1, "Schwantz Curve") },
-            { new FloatRangeStruct(0.3931186f, 0.4847209f), (1, "McLeans") },
-            { new FloatRangeStruct(0.5150666f, 0.6010549f), (1, "Coppice") },
-            { new FloatRangeStruct(0.7011805f, 0.7645504f), (1, "Fogarty Esses") },
-            { new FloatRangeStruct(0.8194078f, 0.8732706f), (1, "Melbourne Hairpin") },
-            { new FloatRangeStruct(0.9239673f, 0.9740674f), (1, "Goddards") }
+            { new FloatRangeStruct(0.0772981f, 0.1404632f), (1, "Redgate") },
+            { new FloatRangeStruct(0.1477933f, 0.1860932f), (2, "Hollywood") },
+            { new FloatRangeStruct(0.1924699f, 0.2584147f), (3, "Craner Curves") },
+            { new FloatRangeStruct(0.2683981f, 0.3242396f), (4, "Old Hairpin") },
+            { new FloatRangeStruct(0.340085f, 0.3834941f), (5, "Starkey's Bridge") },
+            { new FloatRangeStruct(0.3931186f, 0.4279727f), (6, "Schwantz Curve") },
+            { new FloatRangeStruct(0.3931186f, 0.4847209f), (7, "McLeans") },
+            { new FloatRangeStruct(0.5150666f, 0.6010549f), (8, "Coppice") },
+            { new FloatRangeStruct(0.7011805f, 0.7645504f), (9, "Fogarty Esses") },
+            { new FloatRangeStruct(0.8194078f, 0.8732706f), (10, "Melbourne Hairpin") },
+            { new FloatRangeStruct(0.9239673f, 0.9740674f), (11, "Goddards") }
         };
     }
 }
