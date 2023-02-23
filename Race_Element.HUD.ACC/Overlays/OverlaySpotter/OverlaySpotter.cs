5e9ebf15d4eea791613ffd9f058bfb21e1d27b93
RiddleTime <reinierklarenberg@gmail.com>
2023-02-23 04:26:32 +0100

    refactor

--- Race_Element.HUD.ACC/Overlays/OverlaySpotter/OverlaySpotter.cs
+++ Race_Element.HUD.ACC/Overlays/OverlaySpotter/OverlaySpotter.cs
@@ -1,30 +1,19 @@
 ﻿using RaceElement.HUD.Overlay.Internal;
-using System;
-using System.Collections.Generic;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace RaceElement.HUD.ACC.Overlays.OverlaySpotter
 {
 #if DEBUG
-    [Overlay(Name = "Spotter", Version = 1.00, OverlayType = OverlayType.Release,
-   Description = "TODO (spots things?)")]
+    [Overlay(Name = "Spotter",
+        Description = "TODO (spots things?)",
+        Version = 1.00,
+        OverlayType = OverlayType.Release,
+        OverlayCategory = OverlayCategory.Driving
+    )]
 #endif
     internal sealed class SpotterOverlay : AbstractOverlay
     {
         public SpotterOverlay(Rectangle rectangle) : base(rectangle, "Spotter")
-        {
-        }
-
-        public override bool ShouldRender() => DefaultShouldRender();
-
-        public override void BeforeStart()
-        {
-        }
-
-        public override void BeforeStop()
         {
         }
 
