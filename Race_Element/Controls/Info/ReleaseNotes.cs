7468cdb840cce774668afad77b293ff250bf4be8
RiddleTime <reinierklarenberg@gmail.com>
2023-02-28 10:36:16 +0100

    Update ReleaseNotes.cs

--- Race_Element/Controls/Info/ReleaseNotes.cs
+++ Race_Element/Controls/Info/ReleaseNotes.cs
@@ -6,7 +6,8 @@ namespace RaceElement.Controls
     {
         internal readonly static Dictionary<string, string> Notes = new Dictionary<string, string>()
         {
-            {"0.1.7.6", "- Add Electronics HUD showing TC1, TC2, ABS, BB and chosen engine map."+
+            {"0.1.7.6", "- Upgraded to net framework to 4.8.1."+
+                        "\n- Add Electronics HUD showing TC1, TC2, ABS, BB and chosen engine map."+
                         "\n- Added configurable assists color on Input Bars."+
                         "\n- Lap Delta: added option to hide the HUD during race sessions."},
             {"0.1.7.4", "- HUDs: Centered value texts in Info Panels"+
