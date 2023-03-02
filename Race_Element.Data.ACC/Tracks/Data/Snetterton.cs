3f18d3b59a81202ea0c2f2c8b192e7cf14fa16a6
RiddleTime <reinierklarenberg@gmail.com>
2023-03-02 18:25:58 +0100

    finish snetterton cornersr

--- Race_Element.Data.ACC/Tracks/Data/Snetterton.cs
+++ Race_Element.Data.ACC/Tracks/Data/Snetterton.cs
@@ -1,9 +1,6 @@
 ﻿using RaceElement.Util.DataTypes;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using static RaceElement.Data.ACC.Tracks.TrackData;
 
 namespace ACCManager.Data.ACC.Tracks.Data
@@ -14,6 +11,20 @@ namespace ACCManager.Data.ACC.Tracks.Data
         public override string FullName => "Snetterton Circuit";
         public override int TrackLength => 4779;
 
-        public override Dictionary<FloatRangeStruct, (int, string)> CornerNames => new Dictionary<FloatRangeStruct, (int, string)>();
+        public override Dictionary<FloatRangeStruct, (int, string)> CornerNames => new Dictionary<FloatRangeStruct, (int, string)>()
+        {
+              { new FloatRangeStruct(0.05055097f, 0.1186151f), (1, "Riches")},
+              { new FloatRangeStruct(0.1454172f, 0.1875129f), (2, "Montreal")},
+              { new FloatRangeStruct(0.2215991f, 0.2735485f ), (3, "Palmer")},
+              { new FloatRangeStruct(0.329199f, 0.3779144f), (4, "Agostini")},
+              { new FloatRangeStruct(0.4053102f, 0.4402506f), (5, "Hamilton")},
+              { new FloatRangeStruct(0.454165f, 0.4927266f), (6, "Oggies")},
+              { new FloatRangeStruct(0.5022557f, 0.5485336f), (7, "Williams")},
+              { new FloatRangeStruct(0.6869881f, 0.7165877f), (8, "Brundle")},
+              { new FloatRangeStruct(0.7165878f, 0.739463f), (9, "Nelson")},
+              { new FloatRangeStruct(0.757171f, 0.801405f), (10, "Bomb Hole")},
+              { new FloatRangeStruct(0.8077023f, 0.884012f), (11, "Coram")},
+              { new FloatRangeStruct(0.884013f, 0.9138725f), (12, "Murrays")},
+        };
     }
 }
