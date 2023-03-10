59e73c20656c071b8ce24cd9310e114ba29e6c6b
RiddleTime <reinierklarenberg@gmail.com>
2023-03-10 19:41:42 +0100

    design of wheelslipoverlay

--- Race_Element.HUD.ACC/Overlays/Driving/OverlayWheelSlip/WheelSlipOverlay.cs
+++ Race_Element.HUD.ACC/Overlays/Driving/OverlayWheelSlip/WheelSlipOverlay.cs
@@ -3,6 +3,7 @@ using RaceElement.HUD.Overlay.Internal;
 using RaceElement.HUD.Overlay.OverlayUtil;
 using RaceElement.Util.SystemExtensions;
 using System;
+using System.Buffers.Text;
 using System.Drawing;
 using static RaceElement.Data.SetupConverter;
 
@@ -23,23 +24,34 @@ namespace RaceElement.HUD.ACC.Overlays.Driving.OverlayWheelSlip
         public WheelSlipOverlay(Rectangle rectangle) : base(rectangle, "Wheel Slip")
         {
             RefreshRateHz = 20;
+            Width = 130;
+            Height = 130;
+        }
+
+        public override void SetupPreviewData()
+        {
+            pagePhysics.WheelSlip = new float[] { 0.3f, 0.6f, 0.3f, 0.745f };
         }
 
         public override void Render(Graphics g)
         {
-            int wheelSize = 30;
+            int baseX = 10;
+            int baseY = 10;
+            int wheelSize = 50;
             int gap = 10;
 
-            DrawWheelSlip(g, 0, 0, wheelSize, Wheel.FrontLeft);
-            DrawWheelSlip(g, wheelSize + gap, 0, wheelSize, Wheel.FrontRight);
-            DrawWheelSlip(g, 0, wheelSize + gap, wheelSize, Wheel.RearLeft);
-            DrawWheelSlip(g, wheelSize + gap, wheelSize + gap, wheelSize, Wheel.RearRight);
+            DrawWheelSlip(g, baseX + 0, baseY + 0, wheelSize, Wheel.FrontLeft);
+            DrawWheelSlip(g, baseX + wheelSize + gap, baseY + 0, wheelSize, Wheel.FrontRight);
+            DrawWheelSlip(g, baseX + 0, baseY + wheelSize + gap, wheelSize, Wheel.RearLeft);
+            DrawWheelSlip(g, baseX + wheelSize + gap, baseY + wheelSize + gap, wheelSize, Wheel.RearRight);
         }
 
         private void DrawWheelSlip(Graphics g, int x, int y, int size, Wheel wheel)
         {
+            var wheelRect = new Rectangle(x, y, size, size);
             // draw outline
-            g.DrawEllipse(Pens.Black, new Rectangle(x, y, size, size));
+            g.FillEllipse(Brushes.Black, wheelRect);
+            g.DrawEllipse(Pens.Red, wheelRect);
 
 
             // draw wheel specific slip based on outline size
@@ -56,7 +68,7 @@ namespace RaceElement.HUD.ACC.Overlays.Driving.OverlayWheelSlip
             g.FillEllipse(Brushes.Red, centerX, centerY, size / 2 * percentage / 100);
 
             float slipAngle = (float)(pagePhysics.SlipAngle[(int)wheel] * 180d / Math.PI) - 90;
-            g.DrawArc(new Pen(Brushes.White, 5), new Rectangle(x, y, size, size), slipAngle - 5, 10);
+            g.DrawArc(new Pen(Brushes.White, 4), wheelRect, slipAngle - 5, 10);
         }
     }
 }
