165e4b2cf14f930c9cbb5287f13068520041cdfe
RiddleTime <reinierklarenberg@gmail.com>
2023-04-29 20:39:55 +0200

    bump app version to 0.1.9.2

--- Race_Element/Properties/AssemblyInfo.cs
+++ Race_Element/Properties/AssemblyInfo.cs
@@ -42,6 +42,6 @@ using System.Windows;
 )]
 
 //      Major Version, Minor Version, Build Number, Revision
-[assembly: AssemblyVersion("0.1.9.1")]
-[assembly: AssemblyFileVersion("0.1.9.1")]
+[assembly: AssemblyVersion("0.1.9.2")]
+[assembly: AssemblyFileVersion("0.1.9.2")]
 [assembly: NeutralResourcesLanguage("")]
