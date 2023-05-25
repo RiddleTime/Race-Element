ebf692daa458ec223edfdaf2071b1717b0340b24
RiddleTime <reinierklarenberg@gmail.com>
2023-05-25 16:59:15 +0200

    add support for McLaren Evo 2

---
+++ Race_Element.Data.ACC/SetupParser/Cars/GT3/Mclaren720sEvoGT3.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using static RaceElement.Data.ConversionFactory;
+using static RaceElement.Data.SetupConverter;
+
+namespace RaceElement.Data.Cars.GT3
+{
+    internal class Mclaren720sEvoGT3 : ICarSetupConversion
+    {
+        public CarModels CarModel => CarModels.McLaren_720s_GT3_Evo_2023;
+
+        CarClasses ICarSetupConversion.CarClass => CarClasses.GT3;
+        public DryTyreCompounds DryTyreCompound => DryTyreCompounds.DHF2023;
+
+        AbstractTyresSetup ICarSetupConversion.TyresSetup => new TyreSetup();
+        private class TyreSetup : AbstractTyresSetup
+        {
+            public override double Camber(Wheel wheel, List<int> rawValue)
+            {
+                switch (GetPosition(wheel))
+                {
+                    case Position.Front: return Math.Round(-4 + 0.1 * rawValue[(int)wheel], 2);
+                    case Position.Rear: return Math.Round(-3.5 + 0.1 * rawValue[(int)wheel], 2);
+                    default: return -1;
+                }
+            }
+
+            private readonly double[] casters = new double[] { 5.3, 5.6, 5.8, 6.0, 6.3, 6.5, 6.8, 7.0,
+                7.3, 7.5, 7.8, 8.0, 8.2, 8.5, 8.7, 9.0, 9.2, 9.4, 9.7, 9.9, 10.2, 10.4, 10.7, 10.9,
+                11.1, 11.4, 11.6, 11.8, 12.1, 12.3, 12.6, 12.8, 13.0, 13.3, 13.5, 13.7, 14.0, 14.2,
+                14.4, 14.7, 14.9, 15.1, 15.4, 15.6, 15.8, 16.0, 16.3 };
+            public override double Caster(int rawValue)
+            {
+                return Math.Round(casters[rawValue], 2);
+            }
+
+            public override double Toe(Wheel wheel, List<int> rawValue)
+            {
+                switch (GetPosition(wheel))
+                {
+                    case Position.Front: return Math.Round(-0.48 + 0.01 * rawValue[(int)wheel], 2);
+                    case Position.Rear: return Math.Round(-0.1 + 0.01 * rawValue[(int)wheel], 2);
+                    default: return -1;
+                }
+            }
+        }
+
+        IMechanicalSetup ICarSetupConversion.MechanicalSetup => new MechSetup();
+        private class MechSetup : IMechanicalSetup
+        {
+            public int AntiRollBarFront(int rawValue)
+            {
+                return rawValue;
+            }
+
+            public int AntiRollBarRear(int rawValue)
+            {
+                return rawValue;
+            }
+
+            public double BrakeBias(int rawValue)
+            {
+                return Math.Round(47 + 0.2 * rawValue, 2);
+            }
+
+            public int BrakePower(int rawValue)
+            {
+                return 80 + rawValue;
+            }
+
+            public int BumpstopRange(List<int> rawValue, Wheel wheel)
+            {
+                return rawValue[(int)wheel];
+            }
+
+            public int BumpstopRate(List<int> rawValue, Wheel wheel)
+            {
+                return 300 + 100 * rawValue[(int)wheel];
+            }
+
+            public int PreloadDifferential(int rawValue)
+            {
+                return 20 + rawValue * 10;
+            }
+
+            public double SteeringRatio(int rawValue)
+            {
+                return Math.Round(11d + rawValue, 2);
+            }
+
+            private readonly int[] fronts = new int[] { 118000, 134000, 150000, 166000, 182000, 198000, 214000, 230000 };
+            private readonly int[] rears = new int[] { 83000, 97000, 111000, 125000, 139000, 153000, 167000, 181000, 195000, 209000 };
+            public int WheelRate(List<int> rawValue, Wheel wheel)
+            {
+                switch (GetPosition(wheel))
+                {
+                    case Position.Front: return fronts[rawValue[(int)wheel]];
+                    case Position.Rear: return rears[rawValue[(int)wheel]];
+                    default: return -1;
+                }
+            }
+        }
+
+        IDamperSetup ICarSetupConversion.DamperSetup => DefaultDamperSetup;
+
+        IAeroBalance ICarSetupConversion.AeroBalance => new AeroSetup();
+        private class AeroSetup : IAeroBalance
+        {
+            public int BrakeDucts(int rawValue)
+            {
+                return rawValue;
+            }
+
+            public int RearWing(int rawValue)
+            {
+                return rawValue + 1;
+            }
+
+            public int RideHeight(List<int> rawValue, Position position)
+            {
+                switch (position)
+                {
+                    case Position.Front: return 50 + rawValue[0];
+                    case Position.Rear: return 64 + rawValue[2];
+                    default: return -1;
+                }
+            }
+
+            public int Splitter(int rawValue)
+            {
+                return rawValue;
+            }
+        }
+    }
+}
+
