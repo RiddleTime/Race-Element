56fb7dd07f8668e15e8648eaa79e93e6597a75ea
RiddleTime <reinierklarenberg@gmail.com>
2023-07-12 01:49:42 +0200

    remove full window drag

--- Race_Element/MainWindow.xaml.cs
+++ Race_Element/MainWindow.xaml.cs
@@ -61,8 +61,6 @@ namespace RaceElement
             _uiSettings = new UiSettings();
             _accManagerSettings = new AccManagerSettings();
 
-            this.MouseLeftButtonDown += TitleBar_MouseLeftButtonDown;
-            this.MouseLeftButtonUp += TitleBar_MouseLeftButtonUp;
             this.titleBar.MouseLeftButtonDown += TitleBar_MouseLeftButtonDown;
             this.titleBar.MouseLeftButtonUp += TitleBar_MouseLeftButtonUp;
             this.titleBar.MouseLeave += (s, e) => { _stopDecreaseOpacty = true; e.Handled = true; this.Opacity = MaxOpacity; };
