06c97644f53304b3e713ca8f2367917afe8558f1
RiddleTime <reinierklarenberg@gmail.com>
2023-09-07 18:37:08 +0200

    add more hud settings
    - fps widget enabled or disabled

--- Race_Element/Controls/Settings/AccSettings/AccHudSettings.xaml.cs
+++ Race_Element/Controls/Settings/AccSettings/AccHudSettings.xaml.cs
@@ -38,6 +38,8 @@ namespace RaceElement.Controls.AccHudSettingsNS
             toggleRatingWidget.Unchecked += (s, e) => SaveSettings();
             toggleServerStats.Checked += (s, e) => SaveSettings();
             toggleServerStats.Unchecked += (s, e) => SaveSettings();
+            toggleFps.Checked += (s, e) => SaveSettings();
+            toggleFps.Unchecked += (s, e) => SaveSettings();
         }
 
         private void LoadSettings()
@@ -47,6 +49,7 @@ namespace RaceElement.Controls.AccHudSettingsNS
 
             toggleRatingWidget.IsChecked = json.RatingWidgetVisible == 1;
             toggleServerStats.IsChecked = json.ServerStatsVisible == 1;
+            toggleFps.IsChecked = json.FPSVisible == 1;
         }
 
         private void SaveSettings()
@@ -55,6 +58,7 @@ namespace RaceElement.Controls.AccHudSettingsNS
 
             json.RatingWidgetVisible = toggleRatingWidget.IsChecked.Value ? 1 : 0;
             json.ServerStatsVisible = toggleServerStats.IsChecked.Value ? 1 : 0;
+            json.FPSVisible = toggleFps.IsChecked.Value ? 1 : 0;
 
             _accHud.Save(json);
         }
