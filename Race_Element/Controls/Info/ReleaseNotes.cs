6bdc21bc0ca60788207426fa674125ca4ef3e04b
RiddleTime <reinierklarenberg@gmail.com>
2023-10-28 20:37:09 +0200

    Update ReleaseNotes.cs

--- Race_Element/Controls/Info/ReleaseNotes.cs
+++ Race_Element/Controls/Info/ReleaseNotes.cs
@@ -6,7 +6,9 @@ namespace RaceElement.Controls
     {
         internal readonly static Dictionary<string, string> Notes = new Dictionary<string, string>()
         {
-            {"0.2.4.4", "- Added Corner Data HUD, showing min speed and max lateral g forces." },
+            {"0.2.4.4", "- Added Corner Data HUD, showing corner delta and optionally: min speed, avg speed and max lateral g force."+
+                        "\n- DualSenseX module now allows to set a custom port for DSX(6969 is default)."+
+                        "\n- Updated styling of info tables uses in a various amount of HUDs."},
             {"0.2.4.2", "- Removed affiliate links:"+
                         "\nRace Element is free to run software, I do not want to encourage anyone ever again to pay to become faster."+
                         "\nLearn to create your own setups, there are tons of guides out there, even from one of the developers of Assetto Corsa Competizione."+
