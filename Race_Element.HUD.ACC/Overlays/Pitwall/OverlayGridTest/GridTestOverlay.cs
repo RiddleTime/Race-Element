1b8d987199530e23fc04a5356c5df0054f7f4691
RiddleTime <reinierklarenberg@gmail.com>
2023-11-13 05:49:02 +0100

    Update GridTestOverlay.cs
    added some configs

--- Race_Element.HUD.ACC/Overlays/Pitwall/OverlayGridTest/GridTestOverlay.cs
+++ Race_Element.HUD.ACC/Overlays/Pitwall/OverlayGridTest/GridTestOverlay.cs
@@ -1,4 +1,5 @@
-﻿using RaceElement.HUD.Overlay.Internal;
+﻿using RaceElement.HUD.Overlay.Configuration;
+using RaceElement.HUD.Overlay.Internal;
 using RaceElement.HUD.Overlay.OverlayUtil;
 using RaceElement.HUD.Overlay.OverlayUtil.Drawing;
 using System;
@@ -6,18 +7,39 @@ using System.Drawing;
 
 namespace RaceElement.HUD.ACC.Overlays.Pitwall.OverlayGridTest
 {
-    [Overlay(Name = "Grid Test", Description = "Testing of the grid", OverlayType = OverlayType.Pitwall)]
+#if DEBUG
+    [Overlay(Name = "Grid Test", Description = "Testing of the grid", OverlayType = OverlayType.Release)]
+#endif
     internal sealed class GridTestOverlay : AbstractOverlay
     {
+        private readonly GridTestConfiguration _config = new GridTestConfiguration();
+        private sealed class GridTestConfiguration : OverlayConfiguration
+        {
+            public GridTestConfiguration() => AllowRescale = true;
+
+            [ConfigGrouping("Grid", "Customize rows and columns")]
+            public GridGrouping Grid { get; set; } = new GridGrouping();
+            public sealed class GridGrouping
+            {
+                [IntRange(1, 50, 1)]
+                public int Rows { get; set; } = 20;
+
+                [IntRange(1, 50, 1)]
+                public int Columns { get; set; } = 20;
+            }
+
+        }
+
         private readonly GraphicsGrid testgrid;
 
-        public GridTestOverlay(Rectangle rectangle) : base(rectangle, "Grid Test") => testgrid = new GraphicsGrid(10, 10);
+        public GridTestOverlay(Rectangle rectangle) : base(rectangle, "Grid Test") => testgrid = new GraphicsGrid(_config.Grid.Rows, _config.Grid.Columns);
 
         public override void BeforeStart()
         {
             int columnWidth = 10;
             int columnHeight = 10;
             Random rand = new Random();
+
             for (int row = 0; row < testgrid.Rows; row++)
                 for (int column = 0; column < testgrid.Columns; column++)
                 {
@@ -30,7 +52,12 @@ namespace RaceElement.HUD.ACC.Overlays.Pitwall.OverlayGridTest
 
                     testgrid.Grid[row][column] = cell;
                 }
+
+            Width = testgrid.Rows * columnWidth;
+            Height = testgrid.Columns * columnHeight;
         }
+
+        public override void BeforeStop() => testgrid?.Dispose();
 
         public override void Render(Graphics g) => testgrid?.Draw(g);
     }
