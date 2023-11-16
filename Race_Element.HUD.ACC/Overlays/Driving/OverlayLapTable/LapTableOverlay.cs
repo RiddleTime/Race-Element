a38a2492fd0add5dd4108b2530f57ce4be6bb38d
RiddleTime <reinierklarenberg@gmail.com>
2023-11-16 02:45:42 +0100

    Convert lap table to graphics grid

--- Race_Element.HUD.ACC/Overlays/Driving/OverlayLapTable/LapTableOverlay.cs
+++ Race_Element.HUD.ACC/Overlays/Driving/OverlayLapTable/LapTableOverlay.cs
@@ -2,10 +2,12 @@
 using RaceElement.Data.ACC.Tracker.Laps;
 using RaceElement.HUD.Overlay.Internal;
 using RaceElement.HUD.Overlay.OverlayUtil;
+using RaceElement.HUD.Overlay.OverlayUtil.Drawing;
+using RaceElement.HUD.Overlay.Util;
 using System;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
-using static RaceElement.HUD.Overlay.OverlayUtil.InfoTable;
 
 namespace RaceElement.HUD.ACC.Overlays.OverlayLapTimeTable
 {
@@ -15,28 +17,96 @@ namespace RaceElement.HUD.ACC.Overlays.OverlayLapTimeTable
     {
         private readonly LapTimeTableConfiguration _config = new LapTimeTableConfiguration();
 
-        private InfoTable _table;
+        private GraphicsGrid _graphicsGrid;
+        private Font _font;
+
         public LapTableOverlay(Rectangle rectangle) : base(rectangle, "Lap Table")
         {
             this.Width = 51;
             this.Height = 1;
             this.RefreshRateHz = 2;
         }
-        public override void SetupPreviewData()
-        {
-            
-        }
 
         public override void BeforeStart()
         {
-            int[] columnWidths = _config.Table.ShowSectors switch
+            _font = FontUtil.FontSegoeMono(12 * Scale);
+
+            int rows = 1;
+            rows += _config.Table.Rows;
+            int columns = 2;
+            if (_config.Table.ShowSectors) columns += 3;
+
+            _graphicsGrid = new GraphicsGrid(rows, columns);
+
+            float fontHeight = (int)(_font.GetHeight(120));
+            int columnHeight = (int)(fontHeight - 2f * Scale);
+            int[] columnWidths = new int[] { (int)(45f * Scale), (int)(130f * Scale), (int)(90f * Scale), (int)(90f * Scale), (int)(90f * Scale) };
+
+            // set up background rendering
+            Color color = Color.FromArgb(230, Color.Black);
+            using HatchBrush hatchBrush = new HatchBrush(HatchStyle.LightUpwardDiagonal, color, Color.FromArgb(color.A - 75, color));
+            CachedBitmap[] columnBackgrounds = new CachedBitmap[columns];
+            for (int i = 0; i < columns; i++)
+                columnBackgrounds[i] = new CachedBitmap(columnWidths[i], columnHeight, g =>
+                {
+                    g.FillRoundedRectangle(hatchBrush, new Rectangle(0, 0, columnWidths[i], columnHeight), (int)(3 * Scale));
+                });
+
+            int totalWidth = columnWidths[0] + columnWidths[1];
+            // add header row
+            DrawableTextCell col0 = new DrawableTextCell(new Rectangle(0, 0, columnWidths[0], columnHeight), _font);
+            col0.CachedBackground = columnBackgrounds[0];
+            col0.UpdateText("#");
+            _graphicsGrid.Grid[0][0] = col0;
+
+            DrawableTextCell col1 = new DrawableTextCell(new RectangleF(col0.Rectangle.Width, 0, columnWidths[1], columnHeight), _font);
+            col1.CachedBackground = columnBackgrounds[1];
+            col1.UpdateText("Time");
+            _graphicsGrid.Grid[0][1] = col1;
+
+            if (_config.Table.ShowSectors)
             {
-                true => new int[] { 130, 90, 90, 90 },
-                false => new int[] { 130 }
-            };
-            _table = new InfoTable(12, columnWidths);
-            this.Width += columnWidths.Sum();
-            this.Height += _table.FontHeight * (_config.Table.Rows + 1);
+                totalWidth += columnWidths[2] + columnWidths[3] + columnWidths[4];
+
+                // add header row                
+                DrawableTextCell col2 = new DrawableTextCell(new RectangleF(col1.Rectangle.X + columnWidths[1], 0, columnWidths[2], columnHeight), _font);
+                col2.CachedBackground = columnBackgrounds[2];
+                col2.UpdateText("S1");
+                DrawableTextCell col3 = new DrawableTextCell(new RectangleF(col2.Rectangle.X + columnWidths[2], 0, columnWidths[3], columnHeight), _font);
+                col3.CachedBackground = columnBackgrounds[3];
+                col3.UpdateText("S2");
+                DrawableTextCell col4 = new DrawableTextCell(new RectangleF(col3.Rectangle.X + columnWidths[3], 0, columnWidths[4], columnHeight), _font);
+                col4.CachedBackground = columnBackgrounds[4];
+                col4.UpdateText("S3");
+                _graphicsGrid.Grid[0][2] = col2;
+                _graphicsGrid.Grid[0][3] = col3;
+                _graphicsGrid.Grid[0][4] = col4;
+            }
+
+            this.Width = totalWidth;
+            this.Height = columnHeight * (_config.Table.Rows + 1); // +1 for header
+
+            // config data rows
+
+            for (int row = 1; row <= _config.Table.Rows; row++)
+            {
+                for (int column = 0; column < columns; column++)
+                {
+                    int x = columnWidths.Take(column).Sum();
+                    int y = row * columnHeight;
+                    int width = columnWidths[column];
+                    RectangleF rect = new RectangleF(x, y, width, columnHeight);
+                    DrawableTextCell cell = new DrawableTextCell(rect, _font);
+                    cell.CachedBackground = columnBackgrounds[column];
+                    _graphicsGrid.Grid[row][column] = cell;
+                }
+            }
+        }
+
+        public override void BeforeStop()
+        {
+            _graphicsGrid?.Dispose();
+            _font?.Dispose();
         }
 
         public override void Render(Graphics g)
@@ -44,56 +114,35 @@ namespace RaceElement.HUD.ACC.Overlays.OverlayLapTimeTable
             var laps = LapTracker.Instance.Laps.ToList();
             var lapList = laps.OrderByDescending(x => x.Key).Take(_config.Table.Rows);
 
-            switch (_config.Table.ShowSectors)
+            int row = 1;
+            foreach (var lap in lapList)
             {
-                case true:
-                    {
-                        _table?.AddRow("#  ", new string[] { "Time", "S1", "S2", "S3" });
+                using Brush textBrush = lap.Value.IsValid ? Brushes.White : Brushes.OrangeRed;
 
-                        foreach (var lap in lapList)
-                        {
-                            string lapTimeValue = $"--:--.---";
-                            if (lap.Value.Time != -1)
-                            {
-                                TimeSpan best = TimeSpan.FromMilliseconds(lap.Value.Time);
-                                lapTimeValue = $"{best:mm\\:ss\\:fff}";
-                            }
+                DrawableTextCell lapCell = ((DrawableTextCell)_graphicsGrid.Grid[row][0]);
+                lapCell.TextBrush = textBrush;
+                lapCell.UpdateText($"{lap.Key}");
 
-                            string sector1 = $"{lap.Value.GetSector1():F3}";
-                            string sector2 = $"{lap.Value.GetSector2():F3}";
-                            string sector3 = $"{lap.Value.GetSector3():F3}";
+                string lapTimeValue = $"--:--.---";
+                if (lap.Value.Time != -1)
+                {
+                    TimeSpan best = TimeSpan.FromMilliseconds(lap.Value.Time);
+                    lapTimeValue = $"{best:mm\\:ss\\:fff}";
+                }
 
-                            TableRow row = new TableRow() { Header = $"{lap.Key}", Columns = new string[] { $"{lapTimeValue}", sector1, sector2, sector3 } };
-                            if (!lap.Value.IsValid)
-                                row.ColumnColors = new Color[] { Color.OrangeRed, Color.OrangeRed, Color.OrangeRed, Color.OrangeRed };
+                ((DrawableTextCell)_graphicsGrid.Grid[row][1]).UpdateText($"{lapTimeValue}");
 
-                            _table?.AddRow(row);
-                        }
-                        break;
-                    }
-                case false:
-                    {
-                        _table?.AddRow("#  ", new string[] { "Time" });
+                if (_config.Table.ShowSectors)
+                {
+                    ((DrawableTextCell)_graphicsGrid.Grid[row][2]).UpdateText($"{lap.Value.GetSector1():F3}");
+                    ((DrawableTextCell)_graphicsGrid.Grid[row][3]).UpdateText($"{lap.Value.GetSector2():F3}");
+                    ((DrawableTextCell)_graphicsGrid.Grid[row][4]).UpdateText($"{lap.Value.GetSector3():F3}");
+                }
 
-                        foreach (var lap in lapList)
-                        {
-                            string lapTimeValue = $"--:--.---";
-                            if (lap.Value.Time != -1)
-                            {
-                                TimeSpan best = TimeSpan.FromMilliseconds(lap.Value.Time);
-                                lapTimeValue = $"{best:mm\\:ss\\:fff}";
-                            }
-                            TableRow row = new TableRow() { Header = $"{lap.Key}", Columns = new string[] { $"{lapTimeValue}" } };
-                            if (!lap.Value.IsValid)
-                                row.ColumnColors = new Color[] { Color.OrangeRed };
-
-                            _table?.AddRow(row);
-                        }
-                        break;
-                    }
+                row++;
             }
 
-            _table?.Draw(g);
+            _graphicsGrid?.Draw(g);
         }
     }
 }
