5472479181e1e4b840d78c99f25ad3f74038dd17
RiddleTime <reinierklarenberg@gmail.com>
2023-11-17 05:14:44 +0100

    bump beta to 0.2.9.9

--- Race_Element/Properties/AssemblyInfo.cs
+++ Race_Element/Properties/AssemblyInfo.cs
@@ -42,6 +42,6 @@ using System.Windows;
 )]
 
 //      Major Version, Minor Version, Build Number, Revision
-[assembly: AssemblyVersion("0.2.4.3")]
-[assembly: AssemblyFileVersion("0.2.4.3")]
+[assembly: AssemblyVersion("0.2.9.9")]
+[assembly: AssemblyFileVersion("0.2.9.9")]
 [assembly: NeutralResourcesLanguage("")]
