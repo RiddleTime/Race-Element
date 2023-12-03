d10fba18730cf1ea409675fe9741016142b0b42c
RiddleTime <reinierklarenberg@gmail.com>
2023-12-03 03:11:50 +0100

    update releasenotes

--- Race_Element/Controls/Info/ReleaseNotes.cs
+++ Race_Element/Controls/Info/ReleaseNotes.cs
@@ -6,8 +6,9 @@ namespace RaceElement.Controls
     {
         internal readonly static Dictionary<string, string> Notes = new Dictionary<string, string>()
         {
-            {"0.3.0.0", "- Added Corner Data HUD, showing corner delta and optionally: minimum speed, average speed and max lateral g force."+
+            {"0.3.0.0", "- Added Corner Data HUD(Alpha version/early access), showing corner delta and optionally: minimum speed, average speed and max lateral g force."+
                         "\n- DualSenseX module now allows to set a custom port for DSX(6969 is default)."+
+                        "\n- Lap table HUD: Reworked design and functionality, now shows personal fastest and best sectors based on valid laps."+
                         "\n- HUD Tab: Switched movement mode and always visible buttons around"+
                         "\n- Revamped internals of several HUDs to lower cpu usage:"+
                         "\n  - Lap Table HUD"+
--- Race_Element/Properties/AssemblyInfo.cs
+++ Race_Element/Properties/AssemblyInfo.cs
@@ -42,6 +42,6 @@ using System.Windows;
 )]
 
 //      Major Version, Minor Version, Build Number, Revision
-[assembly: AssemblyVersion("0.2.9.9")]
-[assembly: AssemblyFileVersion("0.2.9.9")]
+[assembly: AssemblyVersion("0.3.0.0")]
+[assembly: AssemblyFileVersion("0.3.0.0")]
 [assembly: NeutralResourcesLanguage("")]
