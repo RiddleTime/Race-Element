9a7ce9c0fb6dc9c53e2e4f0137573ed050fb9274
RiddleTime <reinierklarenberg@gmail.com>
2023-12-16 17:46:23 +0100

    Update TrackCircleOverlay.cs
    add yellow flagged cars with coloring

--- Race_Element.HUD.ACC/Overlays/Driving/OverlayTrackCircle/TrackCircleOverlay.cs
+++ Race_Element.HUD.ACC/Overlays/Driving/OverlayTrackCircle/TrackCircleOverlay.cs
@@ -42,7 +42,14 @@ namespace RaceElement.HUD.ACC.Overlays.Driving.OverlayTrackCircle
             {
                 float penWidth = 35f;
                 Rectangle rect = new Rectangle((int)penWidth, (int)penWidth, (int)(scaledDimension - penWidth * 2), (int)(scaledDimension - penWidth * 2));
-                using Pen pen = new Pen(Brushes.Black, penWidth);
+
+                using GraphicsPath gradientPath = new GraphicsPath();
+                gradientPath.AddEllipse(0, 0, scaledDimension, scaledDimension);
+                using PathGradientBrush pthGrBrush = new PathGradientBrush(gradientPath);
+                pthGrBrush.CenterColor = Color.FromArgb(40, 0, 0, 0);
+                pthGrBrush.SurroundColors = new Color[] { Color.FromArgb(220, 0, 0, 0) };
+
+                using Pen pen = new Pen(pthGrBrush, penWidth);
                 g.DrawArc(pen, rect, 0, 360);
             });
             _font = FontUtil.FontSegoeMono(11);
@@ -61,6 +68,8 @@ namespace RaceElement.HUD.ACC.Overlays.Driving.OverlayTrackCircle
 
             float penWidth = 35f;
             using Pen pen = new Pen(Brushes.White, penWidth);
+            using Pen penRed = new Pen(Brushes.Red, penWidth);
+            using Pen penYellow = new Pen(Brushes.Yellow, penWidth);
             Rectangle circleRect = new Rectangle((int)(penWidth), (int)(penWidth), (int)(dimension * Scale - penWidth * 2), (int)(dimension * Scale - penWidth * 2));
             g.DrawArc(pen, circleRect, 269, 2);
 
@@ -70,7 +79,10 @@ namespace RaceElement.HUD.ACC.Overlays.Driving.OverlayTrackCircle
             g.TextRenderingHint = TextRenderingHint.AntiAlias;
 
             using StringFormat stringFormat = new StringFormat() { Alignment = StringAlignment.Center, LineAlignment = StringAlignment.Center };
-            foreach (var item in EntryListTracker.Instance.Cars)
+            var data = EntryListTracker.Instance.Cars;
+            data.Sort((a, b) => a.Value.RealtimeCarUpdate.Position.CompareTo(b.Value.RealtimeCarUpdate.Position));
+            data.Reverse();
+            foreach (var item in data)
             {
                 float carProgression = 0;
                 if (!_Progression.TryGetValue(item.Key, out carProgression))
@@ -79,15 +91,22 @@ namespace RaceElement.HUD.ACC.Overlays.Driving.OverlayTrackCircle
                 _Progression[item.Key] = item.Value.RealtimeCarUpdate.SplinePosition;
                 carProgression = _Progression[item.Key];
 
-                Point center = PointOnCircle((dimension - 20) * Scale / 2, -90 + 360 * carProgression, origin);
+                bool inPitlane = item.Value.RealtimeCarUpdate.CarLocation == Broadcast.CarLocationEnum.Pitlane;
+
+                int offset = inPitlane ? 130 : 20;
+
+                Point center = PointOnCircle((dimension - offset) * Scale / 2, -90 + 360 * carProgression, origin);
                 float size = 26;
                 Rectangle rect = new Rectangle((int)(center.X - size / 2f), (int)(center.Y - size / 2f), (int)size, (int)size);
                 // g.DrawArc(item.Value.RealtimeCarUpdate.CarLocation == Broadcast.CarLocationEnum.Pitlane ? Pens.Red : Pens.White, rect, 0, 360);
 
-                g.DrawArc(pen, circleRect, -90 + 360 * carProgression - 1, .5f);
+                bool isSpeedZero = item.Value.RealtimeCarUpdate.Kmh < 20;
+
+                g.DrawArc(inPitlane ? penRed : isSpeedZero ? penYellow : pen, circleRect, -90 + 360 * carProgression - 1, .5f);
                 using Brush brush = new SolidBrush(Color.FromArgb(170, 0, 0, 0));
+
                 g.FillEllipse(brush, rect);
-                Brush textBrush = item.Value.RealtimeCarUpdate.CarLocation == Broadcast.CarLocationEnum.Pitlane ? Brushes.Red : Brushes.White;
+                Brush textBrush = inPitlane ? Brushes.Red : isSpeedZero ? Brushes.Yellow : Brushes.White;
                 g.DrawStringWithShadow($"{item.Value.RealtimeCarUpdate.Position}", _font, textBrush, rect, stringFormat);
             }
         }
