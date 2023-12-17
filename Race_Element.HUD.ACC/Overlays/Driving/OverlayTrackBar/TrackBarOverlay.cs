1af7d4151fd6e266bd8fbffc6d05295d840387e9
RiddleTime <reinierklarenberg@gmail.com>
2023-12-17 03:35:01 +0100

    add track bar hud

---
+++ Race_Element.HUD.ACC/Overlays/Driving/OverlayTrackBar/TrackBarOverlay.cs
@@ -0,0 +1,23 @@
+﻿using RaceElement.HUD.Overlay.Internal;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaceElement.HUD.ACC.Overlays.Driving.OverlayTrackBar
+{
+    [Overlay(Name = "Track Bar",
+             Description = "A bar displaying a flat and zoomed in version of the Track Circle HUD.")]
+    internal sealed class TrackBarOverlay : AbstractOverlay
+    {
+        public TrackBarOverlay(Rectangle rectangle) : base(rectangle, "Track Bar")
+        {
+        }
+
+        public override void Render(Graphics g)
+        {
+        }
+    }
+}
