755218675a10c29a40434fefa1b692951e4adfba
RiddleTime <reinierklarenberg@gmail.com>
2024-01-11 13:51:55 +0100

    improve rendering of text overflow in twitch chat hud

--- Race_Element.HUD.ACC/Overlays/Pitwall/OverlayTwitchChat/TwitchChatConfiguration.cs
+++ Race_Element.HUD.ACC/Overlays/Pitwall/OverlayTwitchChat/TwitchChatConfiguration.cs
@@ -4,10 +4,10 @@ namespace RaceElement.HUD.ACC.Overlays.Pitwall.OverlayTwitchChat;
 
 internal class TwitchChatConfiguration : OverlayConfiguration
 {
-    public TwitchChatConfiguration() { }
+    public TwitchChatConfiguration() => AllowRescale = true;
 
     [ConfigGrouping("Connection", "Set up the username and O Auth token")]
-    public CredentialsGrouping Credentials { get; init; } = new CredentialsGrouping();
+    public CredentialsGrouping Credentials { get; init; } = new();
     public class CredentialsGrouping
     {
         public string TwitchUser { get; init; } = "";
@@ -15,5 +15,16 @@ internal class TwitchChatConfiguration : OverlayConfiguration
         [ToolTip("Create an O Auth token at https://twitchapps.com/tmi/ and copy/paste the entire result in here.")]
         [StringOptions(isPassword: true)]
         public string OAuthToken { get; init; } = "";
+    }
+
+    [ConfigGrouping("Shape", "Adjust the size of the twitch chat box")]
+    public ShapeGrouping Shape { get; init; } = new();
+    public class ShapeGrouping
+    {
+        [IntRange(100, 400, 2)]
+        public int Width = 400;
+
+        [IntRange(100, 400, 2)]
+        public int Height = 150;
     }
 }
--- Race_Element.HUD.ACC/Overlays/Pitwall/OverlayTwitchChat/TwitchChatOverlay.cs
+++ Race_Element.HUD.ACC/Overlays/Pitwall/OverlayTwitchChat/TwitchChatOverlay.cs
@@ -1,10 +1,12 @@
 ﻿using RaceElement.HUD.Overlay.Internal;
+using RaceElement.HUD.Overlay.OverlayUtil;
 using RaceElement.HUD.Overlay.Util;
-using RaceElement.Util.SystemExtensions;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Text;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,18 +24,16 @@ internal class TwitchChatOverlay : AbstractOverlay
     private readonly TwitchChatConfiguration _config = new TwitchChatConfiguration();
 
     private TwitchClient _twitchClient = null;
-    private readonly List<(string, string)> _messages = new();
-    private InfoPanel _panel;
+    private readonly List<(string, string)> _messages = [];
 
     private bool _isPreviewing = false;
 
-    private const int maxTwitchUserNameLength = 25;
-    private const int maxValueLength = 25;
+    private Font _font;
 
     public TwitchChatOverlay(Rectangle rectangle) : base(rectangle, "Twitch Chat")
     {
-        Width = 450;
-        Height = 160;
+        Width = _config.Shape.Width;
+        Height = _config.Shape.Height;
         RefreshRateHz = 1;
     }
 
@@ -60,15 +60,16 @@ internal class TwitchChatOverlay : AbstractOverlay
         _twitchClient.OnConnected += TwitchClient_OnConnected;
         _twitchClient.OnLog += (s, e) => { Debug.WriteLine(e.Data); };
 
-        _panel = new InfoPanel(10, Width);
+        _font = FontUtil.FontSegoeMono(9);
     }
+
     public override void BeforeStop()
     {
         if (_isPreviewing) return;
 
         if (_twitchClient.IsConnected)
         {
-            _twitchClient.SendMessage(_twitchClient.JoinedChannels[0], "Race Element Disconnected from Twitch chat.");
+            _twitchClient.Disconnect();
         }
     }
 
@@ -80,7 +81,7 @@ internal class TwitchChatOverlay : AbstractOverlay
 
     private void TwitchClient_OnMessageReceived(object sender, TwitchLib.Client.Events.OnMessageReceivedArgs e)
     {
-        _messages.Add(new($"{e.ChatMessage.Username}:", e.ChatMessage.Message));
+        _messages.Add(new($"{e.ChatMessage.DisplayName}", e.ChatMessage.Message));
         Debug.WriteLine(e.ChatMessage.Message);
     }
 
@@ -93,55 +94,27 @@ internal class TwitchChatOverlay : AbstractOverlay
         if (!_twitchClient.IsConnected)
             _twitchClient.Connect();
 
+        g.CompositingQuality = CompositingQuality.HighQuality;
+        g.SmoothingMode = SmoothingMode.AntiAlias;
+        g.TextRenderingHint = TextRenderingHint.AntiAliasGridFit;
 
+        g.FillRoundedRectangle(Brushes.Black, new Rectangle(0, 0, _config.Shape.Width, _config.Shape.Height), 3);
 
         if (_messages.Count > 0)
         {
-            Font font = FontUtil.FontSegoeMono(10);
+            var stringFormat = new StringFormat() { Alignment = StringAlignment.Center, LineAlignment = StringAlignment.Near };
 
-            //var stringFormat = new StringFormat() { Alignment = StringAlignment.Center, LineAlignment = StringAlignment.Near };
-            foreach ((string user, string message) in _messages.TakeLast(10))
+            int y = 0;
+            foreach ((string user, string message) in _messages.TakeLast(10).Reverse())
             {
-                //var size = g.MeasureString(message, font, 300, stringFormat);
+                string text = $"{user}: {message}";
 
+                var size = g.MeasureString(text, _font, _config.Shape.Width, stringFormat);
 
-                if (message.Length > maxValueLength)
-                {
-                    _panel.AddLine(user, message[0..maxValueLength]);
+                g.DrawStringWithShadow(text, _font, Brushes.White, new RectangleF(0, y, size.Width, size.Height), stringFormat);
 
-                    int count = maxValueLength;
-                    while (count < message.Length)
-                    {
-                        int maxIndex = count + maxValueLength;
-                        maxIndex.ClipMax(message.Length - 1);
-                        _panel.AddLine("", message[count..maxIndex]);
-                        count += maxValueLength;
-                    }
-                }
-                else
-                {
-                    _panel.AddLine(user, message);
-                }
+                y += (int)Math.Ceiling(size.Height);
             }
         }
-        else
-        {
-            string maxTwitchUserLength = string.Empty;
-            for (int i = 0; i < maxTwitchUserNameLength + 2; i++)
-                _ = maxTwitchUserLength.Append(' ');
-
-
-            string valueTemp = string.Empty;
-            for (int i = 0; i < maxValueLength + 1; i++)
-                _ = valueTemp.Append(' ');
-
-            for (int i = 0; i < 10; i++)
-            {
-                _panel.AddLine(maxTwitchUserLength, valueTemp);
-            }
-        }
-
-
-        _panel.Draw(g);
     }
 }
