1eed3bfe7665f39694c1e773d4d43f0d08d0d77c
RiddleTime <reinierklarenberg@gmail.com>
2024-01-23 17:09:36 +0100

    Update RedBullRing.cs

--- Race_Element.Data.ACC/Tracks/Data/RedBullRing.cs
+++ Race_Element.Data.ACC/Tracks/Data/RedBullRing.cs
@@ -8,28 +8,28 @@ internal class RedBullRing : AbstractTrackData
 {
     public override Guid Guid => new("f40d09f5-d548-4206-978c-61275840e808");
 
-    // -- TODO, collect the game name for the track itself
-    public override string GameName => throw new NotImplementedException();
+    // -- TODO, collect the game name for the track itself, verify this (this is based on the pattern that kunos simulazioni has used for the most recent new tracks).
+    public override string GameName => "red_bull_ring";
 
     public override string FullName => "Red Bull Ring";
 
     public override int TrackLength => 4318;
 
     // -- TODO, collect the sector transition
-    public override List<float> Sectors => throw new NotImplementedException();
+    public override List<float> Sectors => [];
 
-    // -- TODO, fill in the float range structs
+    // -- TODO, fill out the start and end the float range structs
     public override Dictionary<FloatRangeStruct, (int, string)> CornerNames => new()
     {
-        { new FloatRangeStruct(0, 0.1f), (1, "Niki Lauda")},
-        { new FloatRangeStruct(0, 0.2f), (2, "Münzer")},
-        { new FloatRangeStruct(0, 0.3f), (3, "")},
-        { new FloatRangeStruct(0, 0.4f), (4, "Rauch")},
-        { new FloatRangeStruct(0, 0.5f), (5, "")},
-        { new FloatRangeStruct(0, 0.6f), (6, "")},
-        { new FloatRangeStruct(0, 0.7f), (7, "Graz")},
-        { new FloatRangeStruct(0, 0.8f), (8, "")},
-        { new FloatRangeStruct(0, 0.9f), (9, "Jochen Rindt")},
-        { new FloatRangeStruct(0, 0.95f), (10, "")},
+        //{ new FloatRangeStruct(0, 0.1f), (1, "Niki Lauda")},
+        //{ new FloatRangeStruct(0, 0.2f), (2, "Münzer")},
+        //{ new FloatRangeStruct(0, 0.3f), (3, "")},
+        //{ new FloatRangeStruct(0, 0.4f), (4, "Rauch")},
+        //{ new FloatRangeStruct(0, 0.5f), (5, "")},
+        //{ new FloatRangeStruct(0, 0.6f), (6, "")},
+        //{ new FloatRangeStruct(0, 0.7f), (7, "Graz")},
+        //{ new FloatRangeStruct(0, 0.8f), (8, "")},
+        //{ new FloatRangeStruct(0, 0.9f), (9, "Jochen Rindt")},
+        //{ new FloatRangeStruct(0, 0.95f), (10, "")},
     };
 }
