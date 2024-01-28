b9a3b82f09e264e9e099947b02d70bf57ffdd318
RiddleTime <reinierklarenberg@gmail.com>
2024-01-28 19:31:44 +0100

    Update Vector3Extensions.cs

--- Race Element.Data/Extensions/Vector3Extensions.cs
+++ Race Element.Data/Extensions/Vector3Extensions.cs
@@ -1,60 +1,55 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Numerics;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Numerics;
 
-namespace RaceElement.Data.Extensions
+namespace RaceElement.Data.Extensions;
+
+// https://stackoverflow.com/a/70462919
+internal static class Vector3Extensions
 {
-    internal static class Vector3Extensions
+    public static Quaternion ToQuaternion(this Vector3 v)
     {
-        public static Quaternion ToQuaternion(this Vector3 v)
+
+        float cy = (float)Math.Cos(v.Z * 0.5);
+        float sy = (float)Math.Sin(v.Z * 0.5);
+        float cp = (float)Math.Cos(v.Y * 0.5);
+        float sp = (float)Math.Sin(v.Y * 0.5);
+        float cr = (float)Math.Cos(v.X * 0.5);
+        float sr = (float)Math.Sin(v.X * 0.5);
+
+        return new Quaternion
         {
+            W = (cr * cp * cy + sr * sp * sy),
+            X = (sr * cp * cy - cr * sp * sy),
+            Y = (cr * sp * cy + sr * cp * sy),
+            Z = (cr * cp * sy - sr * sp * cy)
+        };
 
-            float cy = (float)Math.Cos(v.Z * 0.5);
-            float sy = (float)Math.Sin(v.Z * 0.5);
-            float cp = (float)Math.Cos(v.Y * 0.5);
-            float sp = (float)Math.Sin(v.Y * 0.5);
-            float cr = (float)Math.Cos(v.X * 0.5);
-            float sr = (float)Math.Sin(v.X * 0.5);
+    }
 
-            return new Quaternion
-            {
-                W = (cr * cp * cy + sr * sp * sy),
-                X = (sr * cp * cy - cr * sp * sy),
-                Y = (cr * sp * cy + sr * cp * sy),
-                Z = (cr * cp * sy - sr * sp * cy)
-            };
+    public static Vector3 FromEulerAngles(this Quaternion q)
+    {
+        Vector3 angles = new();
 
+        // roll / x
+        double sinr_cosp = 2 * (q.W * q.X + q.Y * q.Z);
+        double cosr_cosp = 1 - 2 * (q.X * q.X + q.Y * q.Y);
+        angles.X = (float)Math.Atan2(sinr_cosp, cosr_cosp);
+
+        // pitch / y
+        double sinp = 2 * (q.W * q.Y - q.Z * q.X);
+        if (Math.Abs(sinp) >= 1)
+        {
+            angles.Y = (float)Math.CopySign(Math.PI / 2, sinp);
+        }
+        else
+        {
+            angles.Y = (float)Math.Asin(sinp);
         }
 
-        public static Vector3 FromEulerAngles(this Quaternion q)
-        {
-            Vector3 angles = new();
+        // yaw / z
+        double siny_cosp = 2 * (q.W * q.Z + q.X * q.Y);
+        double cosy_cosp = 1 - 2 * (q.Y * q.Y + q.Z * q.Z);
+        angles.Z = (float)Math.Atan2(siny_cosp, cosy_cosp);
 
-            // roll / x
-            double sinr_cosp = 2 * (q.W * q.X + q.Y * q.Z);
-            double cosr_cosp = 1 - 2 * (q.X * q.X + q.Y * q.Y);
-            angles.X = (float)Math.Atan2(sinr_cosp, cosr_cosp);
-
-            // pitch / y
-            double sinp = 2 * (q.W * q.Y - q.Z * q.X);
-            if (Math.Abs(sinp) >= 1)
-            {
-                angles.Y = (float)Math.CopySign(Math.PI / 2, sinp);
-            }
-            else
-            {
-                angles.Y = (float)Math.Asin(sinp);
-            }
-
-            // yaw / z
-            double siny_cosp = 2 * (q.W * q.Z + q.X * q.Y);
-            double cosy_cosp = 1 - 2 * (q.Y * q.Y + q.Z * q.Z);
-            angles.Z = (float)Math.Atan2(siny_cosp, cosy_cosp);
-
-            return angles;
-        }
+        return angles;
     }
 }
