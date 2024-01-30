c50e6d857dfb316518a1f3ade1501db2c53cf3d0
RiddleTime <reinierklarenberg@gmail.com>
2024-01-30 22:04:07 +0100

    fix fastest lap sectors picking out on invalid sectors

--- Race_Element.Data.ACC/Database/LapDataDB/LapDataExtensions.cs
+++ Race_Element.Data.ACC/Database/LapDataDB/LapDataExtensions.cs
@@ -146,6 +146,8 @@ public static class LapDataExtensions
     /// <returns>true if the given sector time is faster than any others of that sector in these laps</returns>
     public static bool IsSectorFastest(this Dictionary<int, DbLapData> laps, int sector, int time)
     {
+        if (time < 1) return false;
+
         Dictionary<int, DbLapData> data = laps;
 
         sector.Clip(1, 3);
@@ -193,19 +195,19 @@ public static class LapDataExtensions
         {
             case 1:
                 foreach (DbLapData lap in laps.Select(x => x.Value))
-                    if (lap.IsValid && (lap.Sector1 < fastest) && lap.Sector1 != -1)
+                    if (lap.IsValid && (lap.Sector1 < fastest) && lap.Sector1 > 0)
                         fastest = lap.Sector1;
                 break;
 
             case 2:
                 foreach (DbLapData lap in laps.Select(x => x.Value))
-                    if (lap.IsValid && (lap.Sector2 < fastest) && lap.Sector2 != -1)
+                    if (lap.IsValid && (lap.Sector2 < fastest) && lap.Sector2 > 0)
                         fastest = lap.Sector2;
                 break;
 
             case 3:
                 foreach (DbLapData lap in laps.Select(x => x.Value))
-                    if (lap.IsValid && (lap.Sector3 < fastest) && lap.Sector3 != -1)
+                    if (lap.IsValid && (lap.Sector3 < fastest) && lap.Sector3 > 0)
                         fastest = lap.Sector3;
                 break;
 
