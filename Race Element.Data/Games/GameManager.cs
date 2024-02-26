d9e8732520b85c7b7a6b6e46074fc5c2fe52e6a0
RiddleTime <reinierklarenberg@gmail.com>
2024-02-26 22:04:42 +0100

    Update GameManager.cs
    add possible solution for graceful disposal of last set game

--- Race Element.Data/Games/GameManager.cs
+++ Race Element.Data/Games/GameManager.cs
@@ -10,11 +10,28 @@ public static class GameManager
     public static event EventHandler<Game>? OnGameChanged;
     public static void SetCurrentGame(Game game)
     {
-        if (CurrentGame == Game.iRacing)
-            IRacingDataProvider.Stop();
+        ExitGameData(CurrentGame);
 
         CurrentGame = game;
         SimDataProvider.Clear();
         OnGameChanged?.Invoke(null, game);
+    }
+
+
+    /// <summary>
+    /// Gracefully disposes and stops all mechanisms that are required for a game so they do not interfere with other games.
+    /// </summary>
+    /// <param name="game"></param>
+    private static void ExitGameData(Game game)
+    {
+        switch (game)
+        {
+            case Game.iRacing:
+                {
+                    IRacingDataProvider.Stop();
+                    break;
+                }
+        }
+
     }
 }
