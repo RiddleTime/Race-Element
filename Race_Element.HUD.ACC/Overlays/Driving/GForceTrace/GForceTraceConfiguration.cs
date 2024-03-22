1354233847095a44cb174be0016714f389f95f69
RiddleTime <reinierklarenberg@gmail.com>
2024-03-22 01:14:24 +0100

    add some fake data collection

--- Race_Element.HUD.ACC/Overlays/Driving/GForceTrace/GForceTraceConfiguration.cs
+++ Race_Element.HUD.ACC/Overlays/Driving/GForceTrace/GForceTraceConfiguration.cs
@@ -5,5 +5,13 @@ namespace RaceElement.HUD.ACC.Overlays.Driving.GForceTrace;
 internal sealed class GForceTraceConfiguration : OverlayConfiguration
 {
     public GForceTraceConfiguration() => GenericConfiguration.AllowRescale = true;
+
+    [ConfigGrouping("Chunks", "Adjust chunk settings")]
+    public ChunkGrouping Chunks { get; init; } = new();
+    public sealed class ChunkGrouping
+    {
+        [IntRange(1, 200, 1)]
+        public int MaxChunks { get; init; } = 10;
+    }
 }
 
--- Race_Element.HUD.ACC/Overlays/Driving/GForceTrace/GForceTraceOverlay.cs
+++ Race_Element.HUD.ACC/Overlays/Driving/GForceTrace/GForceTraceOverlay.cs
@@ -3,6 +3,7 @@ using RaceElement.HUD.Overlay.Configuration;
 using RaceElement.HUD.Overlay.Internal;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,7 @@ Description = "A graph that shows you both lateral and longitudinal forces over 
 internal sealed class GForceTraceOverlay(Rectangle rectangle) : AbstractOverlay(rectangle, "G-Force Trace")
 {
     private readonly GForceTraceConfiguration _config = new();
+    private List<GForceDataChunk> _chunks = [];
 
     private GForceDataJob _dataJob;
 
@@ -23,13 +25,24 @@ internal sealed class GForceTraceOverlay(Rectangle rectangle) : AbstractOverlay(
         if (IsPreviewing) return;
 
         _dataJob = new GForceDataJob() { IntervalMillis = 20 };
+        _dataJob.OnNewDataChunk += OnNewDataChunk;
         _dataJob.Run();
+    }
+
+    private void OnNewDataChunk(GForceDataChunk chunk)
+    {
+        if (_chunks.Count >= _config.Chunks.MaxChunks)
+            _chunks.RemoveAt(0);
+        _chunks.Add(chunk);
+
+        Debug.WriteLine(_chunks.Count);
     }
 
     public override void BeforeStop()
     {
         if (IsPreviewing) return;
 
+        _dataJob.OnNewDataChunk -= OnNewDataChunk;
         _dataJob.CancelJoin();
     }
 
@@ -40,25 +53,38 @@ internal sealed class GForceTraceOverlay(Rectangle rectangle) : AbstractOverlay(
 
     internal sealed class GForceDataJob : AbstractLoopJob
     {
+        private GForceDataChunk _dataChunk = new();
+
+        private int index = 0;
         public sealed override void RunAction() => Collect();
 
+        public Action<GForceDataChunk> OnNewDataChunk;
         private void Collect()
         {
-
+            if (index < _dataChunk.X.Length - 1)
+            {
+                _dataChunk.X[index] = 0f;
+                _dataChunk.Y[index] = 0f;
+                index++;
+            }
+            else
+            {
+                OnNewDataChunk(_dataChunk);
+                _dataChunk = new();
+                index = 0;
+            }
         }
     }
 
     internal readonly record struct GForceDataChunk
     {
-        public int ChunkSize { get; init; }
-        public float[] X { get; init; }
-        public float[] Y { get; init; }
-
-        public GForceDataChunk(float[] x, float[] y, int chunkSize)
+        private const int ChunkSize = 256;
+        public readonly float[] X { get; init; } = new float[ChunkSize];
+        public readonly float[] Y { get; init; } = new float[ChunkSize];
+        public GForceDataChunk()
         {
-            this.X = x;
-            this.Y = y;
-            this.ChunkSize = chunkSize;
+            X = new float[ChunkSize];
+            Y = new float[ChunkSize];
         }
     }
 }
