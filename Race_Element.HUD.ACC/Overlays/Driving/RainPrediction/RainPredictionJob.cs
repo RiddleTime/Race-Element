4ae2bcb5322f41c7ae22471996309215e418dd06
RiddleTime <reinierklarenberg@gmail.com>
2024-03-29 01:47:39 +0100

    refactor weather to rain prediction hud

---
+++ Race_Element.HUD.ACC/Overlays/Driving/RainPrediction/RainPredictionJob.cs
@@ -0,0 +1,60 @@
+﻿using RaceElement.Core.Jobs.LoopJob;
+using RaceElement.HUD.ACC.Overlays.OverlayRainPrediction;
+using System;
+using System.Collections.Generic;
+using static RaceElement.ACCSharedMemory;
+
+namespace RaceElement.HUD.ACC.Overlays.Driving.Weather;
+
+public record struct RealtimeWeather
+{
+    public RealtimeWeather() { }
+    public AcRainIntensity Now { get; set; }
+    public AcRainIntensity In10 { get; set; }
+    public AcRainIntensity In30 { get; set; }
+}
+
+internal sealed class RainPredictionJob(RainPredictionOverlay Overlay) : AbstractLoopJob
+{
+    private readonly Dictionary<DateTime, RealtimeWeather> WeatherChanges = [];
+    public readonly Dictionary<DateTime, AcRainIntensity> UpcomingChanges = [];
+    private RealtimeWeather _lastWeather;
+
+    public override void RunAction()
+    {
+        if (Overlay.pageGraphics.Status == AcStatus.AC_OFF)
+        {
+            WeatherChanges.Clear();
+            _lastWeather = new();
+            UpcomingChanges.Clear();
+            return;
+        }
+
+        if (WeatherChanges.Count == 0)
+            _lastWeather = new()
+            {
+                Now = Overlay.pageGraphics.rainIntensity,
+                In10 = Overlay.pageGraphics.rainIntensityIn10min,
+                In30 = Overlay.pageGraphics.rainIntensityIn30min
+            };
+
+        RealtimeWeather newScan = new()
+        {
+            Now = Overlay.pageGraphics.rainIntensity,
+            In10 = Overlay.pageGraphics.rainIntensityIn10min,
+            In30 = Overlay.pageGraphics.rainIntensityIn30min
+        };
+
+        if (newScan != _lastWeather || UpcomingChanges.Count == 0)
+        {
+            DateTime change = DateTime.UtcNow;
+            WeatherChanges.Add(change, newScan);
+            lock (UpcomingChanges)
+            {
+                UpcomingChanges.Add(change.AddMinutes(10), newScan.In10);
+                UpcomingChanges.Add(change.AddMinutes(30), newScan.In30);
+            }
+            _lastWeather = newScan;
+        }
+    }
+}
---
+++ Race_Element.HUD.ACC/Overlays/Driving/RainPrediction/RainPredictionOverlay.cs
@@ -0,0 +1,100 @@
+﻿using RaceElement.Data.ACC.Session;
+using RaceElement.HUD.ACC.Overlays.Driving.Weather;
+using RaceElement.HUD.Overlay.Configuration;
+using RaceElement.HUD.Overlay.Internal;
+using RaceElement.HUD.Overlay.Util;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using static RaceElement.ACCSharedMemory;
+
+namespace RaceElement.HUD.ACC.Overlays.OverlayRainPrediction;
+
+[Overlay(Name = "Rain Prediction",
+Description = "Shows predicted rain forecasts.",
+OverlayCategory = OverlayCategory.Track,
+OverlayType = OverlayType.Drive,
+Version = 1.00,
+Authors = ["Reinier Klarenberg"])]
+internal sealed class RainPredictionOverlay : AbstractOverlay
+{
+    private readonly WeatherConfiguration _config = new();
+    private sealed class WeatherConfiguration : OverlayConfiguration
+    {
+        public WeatherConfiguration() => this.GenericConfiguration.AllowRescale = true;
+    }
+
+    private RainPredictionJob _weatherJob;
+
+    private readonly InfoPanel _panel;
+    private int _timeMultiplier = -1;
+
+    public RainPredictionOverlay(Rectangle rectangle) : base(rectangle, "Rain Prediction")
+    {
+        this.RefreshRateHz = 1;
+        int panelWidth = 200;
+        _panel = new InfoPanel(10, panelWidth);
+
+        this.Width = panelWidth + 1;
+        this.Height = _panel.FontHeight * 10;
+    }
+
+    private void Instance_OnMultiplierChanged(object sender, int e)
+    {
+        _timeMultiplier = e;
+    }
+
+    public sealed override void BeforeStart()
+    {
+
+        if (!IsPreviewing)
+        {
+            SessionTimeTracker.Instance.OnMultiplierChanged += Instance_OnMultiplierChanged;
+
+            _weatherJob = new RainPredictionJob(this) { IntervalMillis = 1000 };
+            _weatherJob.Run();
+        }
+    }
+
+    public sealed override void BeforeStop()
+    {
+
+        if (!IsPreviewing)
+        {
+            SessionTimeTracker.Instance.OnMultiplierChanged -= Instance_OnMultiplierChanged;
+
+            _weatherJob.CancelJoin();
+        }
+    }
+
+    public sealed override void Render(Graphics g)
+    {
+        if (_weatherJob != null)
+        {
+            _panel.AddLine($"Now", $"{pageGraphics.rainIntensity}");
+
+            List<KeyValuePair<DateTime, AcRainIntensity>> data;
+            lock (_weatherJob.UpcomingChanges)
+            {
+                data = [.. _weatherJob.UpcomingChanges.Where(x => x.Key > DateTime.UtcNow).OrderBy(x => x.Key)];
+            }
+
+            if (data.Count == 0)
+            {
+                _panel.AddLine("--:--", "No data yet");
+            }
+            else
+            {
+                for (int i = 0; i < data.Count; i++)
+                    _panel.AddLine($"{data[i].Key.Subtract(DateTime.UtcNow):mm\\:ss}", $"{data[i].Value}");
+            }
+        }
+
+        //string multiplierText = _timeMultiplier > 0 ? $"{_timeMultiplier}x" : "Detecting";
+        //_panel.AddLine("Multiplier", $"{multiplierText}");
+
+        _panel.Draw(g);
+    }
+}
