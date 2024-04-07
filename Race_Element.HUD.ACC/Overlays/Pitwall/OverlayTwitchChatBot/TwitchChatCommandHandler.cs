c62f39c1670a12e79239e50cb4df5f088b222810
RiddleTime <reinierklarenberg@gmail.com>
2024-04-07 05:01:03 +0200

    Update TwitchChatCommandHandler.cs
    refactor, add extra comma

--- Race_Element.HUD.ACC/Overlays/Pitwall/OverlayTwitchChatBot/TwitchChatCommandHandler.cs
+++ Race_Element.HUD.ACC/Overlays/Pitwall/OverlayTwitchChatBot/TwitchChatCommandHandler.cs
@@ -139,7 +139,9 @@ internal sealed class TwitchChatBotCommandHandler
         if (_overlay.pagePhysics.AirTemp > 0)
         {
             sb.Append($"Air {_overlay.pagePhysics.AirTemp:F3}°, Track {_overlay.pagePhysics.RoadTemp:F3}°, Wind {_overlay.pageGraphics.WindSpeed:F1} km/h, Grip: {_overlay.pageGraphics.trackGripStatus}");
-            if (_overlay.pageGraphics.rainIntensity != ACCSharedMemory.AcRainIntensity.No_Rain) sb.Append($"{ACCSharedMemory.AcRainIntensityToString(_overlay.pageGraphics.rainIntensity)} Rain");
+
+            if (_overlay.pageGraphics.rainIntensity != ACCSharedMemory.AcRainIntensity.No_Rain)
+                sb.Append($", {ACCSharedMemory.AcRainIntensityToString(_overlay.pageGraphics.rainIntensity)} Rain");
         }
         else
         {
