36cefd7785a56b16220e831ef19de5ace3e17989
RiddleTime <reinierklarenberg@gmail.com>
2024-04-13 20:47:45 +0200

    Update ReleaseNotes.cs

--- Race_Element/Controls/Info/ReleaseNotes.cs
+++ Race_Element/Controls/Info/ReleaseNotes.cs
@@ -8,7 +8,8 @@ public static class ReleaseNotes
     {
         {"1.0.6.0", "\n- Twitch Chat Bot:"+
                     "\n  - +temps now also shows rain condition when it's not dry."+
-                    "\n- Twitch Chat HUD: Added behaviuor category, adding Always Visible and Hide in Qualifying."+
+                    "\n  - +pos, +ahead and +behind now all use the same response, including the lap index of the last lap."+
+                    "\n- Twitch Chat HUD: Added behaviuor category to options: Always Visible and Hide In Qualifying."+
                     "\n- Rain Prediction HUD: Smaller dimension."},
         {"1.0.5.6", "- Added preliminary support for ACC Nordschleife 24H DLC, expect an update for corner names and sector data."+
                     "\n- Corner Data HUD: Increased width of speed delta columns."+
