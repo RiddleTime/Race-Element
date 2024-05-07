57d75df56b0c7d5282ebd99ed12d6373e61c326d
RiddleTime <reinierklarenberg@gmail.com>
2024-05-07 16:41:58 +0200

    add base for pressure history HUD

---
+++ Race_Element.HUD.ACC/Overlays/Driving/PressureHistory/PressureHistoryJob.cs
@@ -0,0 +1,82 @@
+﻿using RaceElement.Core.Jobs.LoopJob;
+using RaceElement.Data.ACC.Tracker.Laps;
+using RaceElement.HUD.Overlay.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaceElement.HUD.ACC.Overlays.Driving.PressureHistory;
+
+internal readonly record struct PressureHistoryModel
+{
+    public float[] Averages { get; init; }
+    public float[] Min { get; init; }
+    public float[] Max { get; init; }
+}
+
+internal sealed class PressureHistoryJob : AbstractLoopJob
+{
+    private readonly AbstractOverlay _overlay;
+
+    private List<float>[] LapPressures = new List<float>[4];
+
+    private EventHandler<PressureHistoryModel> OnNewHistory;
+
+    public PressureHistoryJob(AbstractOverlay overlay)
+    {
+        _overlay = overlay;
+    }
+
+    public sealed override void BeforeRun()
+    {
+        LapTracker.Instance.LapFinished += Instance_LapFinished;
+    }
+
+    private void Instance_LapFinished(object sender, RaceElement.Data.ACC.Database.LapDataDB.DbLapData e)
+    {
+        OnNewHistory?.Invoke(this, GetHistoryModel());
+        ResetLapData();
+    }
+
+    public sealed override void AfterCancel()
+    {
+        LapTracker.Instance.LapFinished -= Instance_LapFinished;
+    }
+
+    private PressureHistoryModel GetHistoryModel()
+    {
+        float[] averages = new float[4];
+        float[] mins = new float[4];
+        float[] maxs = new float[4];
+        for (int i = 0; i < 4; i++)
+        {
+            averages[i] = LapPressures[i].Average();
+            mins[i] = LapPressures[i].Min();
+            maxs[i] = LapPressures[i].Max();
+        }
+
+        return new()
+        {
+            Averages = averages,
+            Min = mins,
+            Max = maxs,
+        };
+    }
+
+    private void ResetLapData()
+    {
+        for (int i = 0; i < 4; i++)
+            LapPressures[i].Clear();
+    }
+
+    public sealed override void RunAction()
+    {
+        if (_overlay.DefaultShouldRender())
+        {
+            for (int i = 0; i < 4; i++)
+                LapPressures[i].Add(_overlay.pagePhysics.WheelPressure[i]);
+        }
+    }
+}
---
+++ Race_Element.HUD.ACC/Overlays/Driving/PressureHistory/PressureHistoryOverlay.cs
@@ -0,0 +1,50 @@
+﻿using RaceElement.HUD.Overlay.Configuration;
+using RaceElement.HUD.Overlay.Internal;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaceElement.HUD.ACC.Overlays.Driving.PressureHistory;
+
+[Overlay(Name = "Pressure History",
+Description = "TODO",
+OverlayCategory = OverlayCategory.Physics,
+OverlayType = OverlayType.Drive,
+Authors = ["Reinier Klarenberg"],
+Version = 1.0)]
+internal sealed class PressureHistoryOverlay : AbstractOverlay
+{
+    private readonly PressureHistoryConfiguration _config = new();
+    private sealed class PressureHistoryConfiguration : OverlayConfiguration
+    {
+
+    }
+
+    private PressureHistoryJob _historyJob;
+
+    public PressureHistoryOverlay(Rectangle rectangle) : base(rectangle, "Pressure History")
+    {
+    }
+
+    public sealed override void BeforeStart()
+    {
+        if (IsPreviewing) return;
+
+        _historyJob = new(this) { IntervalMillis = 100 };
+        _historyJob.Run();
+    }
+
+    public sealed override void BeforeStop()
+    {
+        if (IsPreviewing) return;
+
+        _historyJob?.CancelJoin();
+    }
+
+    public sealed override void Render(Graphics g)
+    {
+    }
+}
