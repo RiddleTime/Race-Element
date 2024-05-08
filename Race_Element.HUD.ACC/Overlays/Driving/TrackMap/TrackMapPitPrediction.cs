0a623b766d6e52acad6c3f7110eafe86a8874eef
iFuSiiOnzZ <andreisystem@gmail.com>
2024-05-08 08:57:42 +0200

    Apply code review comments

--- Race_Element.HUD.ACC/Overlays/Driving/TrackMap/TrackMapPitPrediction.cs
+++ Race_Element.HUD.ACC/Overlays/Driving/TrackMap/TrackMapPitPrediction.cs
@@ -14,65 +14,57 @@ public class PitStop
 
 public class TrackMapPitPrediction
 {
-    private static float  _defaultPitTimeMs = 30 * 1000;
+    private static readonly float DefaultPitTimeMs = 30_000;
 
-    private static Dictionary<string, float> _trackPitLineTime = new Dictionary<string, float>
+    private static readonly Dictionary<string, float> TrackPitLineTimeMs = new Dictionary<string, float>
     {
-        { "barcelona"       , 30 * 1000 },
-        { "bathurst"        , 19 * 1000 },
-        { "brands_hatch"    , 19 * 1000 },
-        { "cota"            , 29 * 1000 },
-        { "donington"       , 19 * 1000 },
-        { "hungaroring"     , 24 * 1000 },
-        { "imola"           , 38 * 1000 },
-        { "indianapolis"    , 44 * 1000 },
-        { "kyalami"         , 18 * 1000 },
-        { "laguna_seca"     , 21 * 1000 },
-        { "misano"          , 28 * 1000 },
-        { "monza"           , 31 * 1000 },
-        { "nurburgring"     , 25 * 1000 },
-        { "nurburgring_24h" , 25 * 1000 },
-        { "oulton_park"     , 14 * 1000 },
-        { "paul_ricard"     , 27 * 1000 },
-        { "silverstone"     , 23 * 1000 },
-        { "snetterton"      , 19 * 1000 },
-        { "spa"             , 57 * 1000 },
-        { "suzuka"          , 27 * 1000 },
-        { "valencia"        , 39 * 1000 },
-        { "watkins_glen"    , 27 * 1000 },
-        { "zandvoort"       , 19 * 1000 },
-        { "zolder"          , 30 * 1000 }
+        { "barcelona"       , 30_000 },
+        { "bathurst"        , 19_000 },
+        { "brands_hatch"    , 19_000 },
+        { "cota"            , 29_000 },
+        { "donington"       , 19_000 },
+        { "hungaroring"     , 24_000 },
+        { "imola"           , 38_000 },
+        { "indianapolis"    , 44_000 },
+        { "kyalami"         , 18_000 },
+        { "laguna_seca"     , 21_000 },
+        { "misano"          , 28_000 },
+        { "monza"           , 31_000 },
+        { "nurburgring"     , 25_000 },
+        { "nurburgring_24h" , 25_000 },
+        { "oulton_park"     , 14_000 },
+        { "paul_ricard"     , 27_000 },
+        { "silverstone"     , 23_000 },
+        { "snetterton"      , 19_000 },
+        { "spa"             , 57_000 },
+        { "suzuka"          , 27_000 },
+        { "valencia"        , 27_000 },
+        { "watkins_glen"    , 27_000 },
+        { "zandvoort"       , 19_000 },
+        { "zolder"          , 30_000 }
     };
 
     public static PitStop GetPitStop(List<PointF> track)
     {
-        float pitLaneTime = 0;
         var name = ACCSharedMemory.Instance.PageFileStatic.Track.ToLower();
-
-        _trackPitLineTime.TryGetValue(name, out pitLaneTime);
-        return ComputePitStop(track, _defaultPitTimeMs + pitLaneTime);
+        float ms = TrackPitLineTimeMs.GetValueOrDefault(name, 0.0f);
+        return ComputePitStop(track, DefaultPitTimeMs + ms);
     }
 
     public static PitStop GetPitStopWithDamage(List<PointF> track)
     {
-        var a = ACCSharedMemory.Instance.PageFilePhysics.CarDamage[0];
-        var b = ACCSharedMemory.Instance.PageFilePhysics.CarDamage[1];
-        var c = ACCSharedMemory.Instance.PageFilePhysics.CarDamage[2];
-        var d = ACCSharedMemory.Instance.PageFilePhysics.CarDamage[3];
-        var e = ACCSharedMemory.Instance.PageFilePhysics.CarDamage[4];
-        var result = a + b + c + d + e;
+        var p = ACCSharedMemory.Instance.PageFilePhysics;
+        var result = RaceElement.Data.ACC.Cars.Damage.GetTotalRepairTime(p);
 
         if (result == 0)
         {
             return null;
         }
 
-        float pitLaneTime = 0;
         var name = ACCSharedMemory.Instance.PageFileStatic.Track.ToLower();
+        var ms = TrackPitLineTimeMs.GetValueOrDefault(name, 0.0f);
 
-        _trackPitLineTime.TryGetValue(name, out pitLaneTime);
-        result = result * 0.142f * 1000 + _defaultPitTimeMs + pitLaneTime;
-
+        result = result * 1000 + DefaultPitTimeMs + ms;
         return ComputePitStop(track, result);
     }
 
