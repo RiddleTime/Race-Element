d41bd63d5c683de25dca76c3201297bf40c4d586
iFuSiiOnzZ <andreisystem@gmail.com>
2024-05-10 12:58:51 +0200

    Add some doxygen for LapInfo

--- Race_Element.Broadcast/Structs/LapInfo.cs
+++ Race_Element.Broadcast/Structs/LapInfo.cs
@@ -4,20 +4,48 @@ namespace RaceElement.Broadcast.Structs;
 
 public class LapInfo
 {
+    /// <summary>
+    /// Lap time in milliseconds. Use "GetLapTimeMS()"
+    /// as this value sometime is reported wrong by
+    /// the server.
+    /// </summary>
     public int? LaptimeMS { get; internal set; }
+
+    /// <summary>
+    /// Sector time in milliseconds.
+    /// </summary>
     public List<int?> Splits { get; } = [];
+
+    /// <summary>
+    /// Internal server identifier assigned to the car.
+    /// </summary>
     public ushort CarIndex { get; internal set; }
+
+    /// <summary>
+    ///  TODO
+    /// </summary>
     public ushort DriverIndex { get; internal set; }
+
+    /// <summary>
+    /// Lap is not valid due corner cut, out of track, etc.
+    /// </summary>
     public bool IsInvalid { get; internal set; }
+
+    /// <summary>
+    /// Improving its own the best time.
+    /// </summary>
     public bool IsValidForBest { get; internal set; }
+
+    /// <summary>
+    /// TODO
+    /// </summary>
     public LapType Type { get; internal set; }
 
     /// <summary>
-    /// Prefered to use this method instead of LapTimeMS property.
-    /// The LapTimeMS Property sometimes doesn't return the correct laptime.
+    /// Preferred to use this method instead of LapTimeMS property.
+    /// The LapTimeMS Property sometimes doesn't return the correct
+    /// laptime.
     /// </summary>
-    /// <param name="splits"></param>
-    /// <returns></returns>
     public int GetLapTimeMS()
     {
         int lapTimeMs = 0;
