744caca8323bb6b452a3e465497afff57d0d7441
RiddleTime <reinierklarenberg@gmail.com>
2024-05-16 18:31:36 +0200

    Update ReleaseNotes.cs

--- Race_Element/Controls/Info/ReleaseNotes.cs
+++ Race_Element/Controls/Info/ReleaseNotes.cs
@@ -7,6 +7,7 @@ public static class ReleaseNotes
     internal readonly static Dictionary<string, string> Notes = new()
     {
         {"1.0.8.6", "- Internal optimizations to rendering."+
+                    "\n- Improved data detection for cars leaving the server by Andrei Jianu."+
                     "\n- Added hud to benchmark rendering optimizations (Pitwall->CB Benchmark)."},
         {"1.0.8.4", "- Fuel Info HUD: Added option to set which laptime is used in the fuel calculation."+
                     "\n- Damage HUD: Added additional colors for various amounts of damage."+
