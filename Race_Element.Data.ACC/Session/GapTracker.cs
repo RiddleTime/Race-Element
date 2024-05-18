0ece8f91590cc31c9dbe59e05bf1c51cc2bb7d23
RiddleTime <reinierklarenberg@gmail.com>
2024-05-18 23:47:32 +0200

    add base class for gap tracker
    might rename this class, but for now it's here

---
+++ Race_Element.Data.ACC/Session/GapTracker.cs
@@ -0,0 +1,20 @@
+﻿using RaceElement.Core.Jobs.LoopJob;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaceElement.Data.ACC.Session;
+
+internal sealed class GapTracker : AbstractLoopJob
+{
+    public GapTracker()
+    {
+    }
+
+    public override void RunAction()
+    {
+        throw new NotImplementedException();
+    }
+}
