5976dcd03796bfb82aeba2c96f5a71ff7f667592
RiddleTime <reinierklarenberg@gmail.com>
2024-06-01 19:45:50 +0200

    Update ReleaseNotes.cs

--- Race_Element/Controls/Info/ReleaseNotes.cs
+++ Race_Element/Controls/Info/ReleaseNotes.cs
@@ -6,6 +6,9 @@ public static class ReleaseNotes
 {
     internal readonly static Dictionary<string, string> Notes = new()
     {
+        {"1.0.8.8", "- Rain Prediction HUD: Added configurable time multiplier."+
+                    "\n- Track Info HUD: Increased precision of air and track temps."+
+                    "\n- Brake Temp History HUD: Updated background set min and max values."},
         {"1.0.8.6", "- Damage HUD: Added configurable colors for each level of body damage"+
                     "\n- Internal optimizations to rendering."+
                     "\n- Improved data detection for cars leaving the server by Andrei Jianu."+
