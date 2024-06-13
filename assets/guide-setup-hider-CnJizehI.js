e7028ba47e0cfe02a7a27b1a850795b729e7038e
Reinier Klarenberg <reinierklarenberg@gmail.com>
2024-06-13 17:41:24 +0000

    Auto-generated commit
    Triggered by commit: https://github.com/RiddleTime/Race-Element/commit/aa06bbfdc95c3a7c1a334dab8bcef969e4a88346

--- assets/guide-setup-hider-CnJizehI.js
+++ assets/guide-setup-hider-CnJizehI.js
@@ -5,10 +5,10 @@ description: Setting up the setup hider with OBS or Streamlabs
 type: guide 
 ---
 
-# How does it hide?
+# How does it work?
 Assetto Corsa Competizione has data for developers that allows us to see when the Setup Screen is open. Race Element uses this with the integration of streamlabs or OBS. It sends a signal to the configured streaming software to make a Source item visible or to hide it based on the data from Assetto Corsa Competizione.
 
-## OBS
+# OBS
 Make sure you are running OBS version 28 or higher.
 1. *OBS*: **Add Any type of Source to your active Scene, name it \`SetupHider\`. This can be text, image, video, a website, basically anything even a Scene Source.**
 2. *OBS*: **At the top menu: Click Tools and Open the WebSocket Server Settings.**
@@ -18,7 +18,7 @@ Make sure you are running OBS version 28 or higher.
 6. *OBS/Race Element*: **Copy the Server IP, Server Port and Server Password from OBS and paste them in Race Element's OBS Settings.**
 7. *Race Element:*: **Click Save and then at the bottom Enable the SetupHider.**
 
-## StreamLabs
+# StreamLabs
 1. *StreamLabs*: **Add Any type of Source to your active Scene, name it \`SetupHider\`. This can be text, image, video, a website, basically anything even a Scene Source.**
 2. *Race Element:* **Open the Main Settings Tab on the left -> Open the Streaming Tab.**
 3. *Race Element:* **Set the Software to \`StreamLabs\`.**
