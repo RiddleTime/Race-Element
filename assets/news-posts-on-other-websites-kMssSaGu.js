7f825f9eb4f3440f45e826c611aee5cef4a2727c
Reinier Klarenberg <reinierklarenberg@gmail.com>
2024-06-16 18:45:57 +0000

    Auto-generated commit
    Triggered by commit: https://github.com/RiddleTime/Race-Element/commit/c5f346a6c1a2ae20347380669042c08a263916f0

---
+++ assets/news-posts-on-other-websites-kMssSaGu.js
@@ -0,0 +1,13 @@
+const e=`---
+title: Official Posts on Other Websites 
+slug: 2023-06-16-official-posts
+description: The only places where Race ELement is officially posted.
+date: 2024-06-16
+type: news
+---
+# Official Listings
+To prevent confusion what is not and what is, here is a list of websites we have posted Race Element:
+- Official Assetto Corsa Forums: **<a href="https://www.assettocorsa.net/forum/index.php?threads/71454" target="_blank">https://www.assettocorsa.net/forum/index.php?threads/71454</a>**
+- Overtake.GG: **<a href="https://www.overtake.gg/downloads/race-element.50578/" target="_blank">https://www.overtake.gg/downloads/race-element.50578/</a>**
+- Steam Guide: **<a href="https://steamcommunity.com/sharedfiles/filedetails/?id=2811480721">https://steamcommunity.com/sharedfiles/filedetails/?id=2811480721</a>**
+`;export{e as default};
