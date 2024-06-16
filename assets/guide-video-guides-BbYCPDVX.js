f3e35aa38bda52a4c9579138567f4d2497d863fb
Reinier Klarenberg <reinierklarenberg@gmail.com>
2024-06-16 19:59:52 +0000

    Auto-generated commit
    Triggered by commit: https://github.com/RiddleTime/Race-Element/commit/8ab47f4bb08d853b76e99b74f997f9aac22154c3

---
+++ assets/guide-video-guides-BbYCPDVX.js
@@ -0,0 +1,13 @@
+const e=`---
+title: "Video Guides"
+slug: video-guides
+description: A collection of video-guides made by other people in various languages
+type: guide 
+---
+# Video Guides
+As my spare time is mostly involving the development of the app, I rely on the community to create video guides in for example other languages than English. If you have made a guide, please let me know so I can add it.
+
+# Portuguese - Português
+- [![IMAGE ALT TEXT](http://img.youtube.com/vi/D8XTKFrADiM/0.jpg)](https://www.youtube.com/watch?v=D8XTKFrADiM "RACE ELEMENT | ESSE APP VAI SER O SEU MELHOR AMIGO NO ACC!")
+- [![IMAGE ALT TEXT](http://img.youtube.com/vi/dP3ELQzdgG0/0.jpg)](https://www.youtube.com/watch?v=dP3ELQzdgG0 "COMO utilizar o Race Elements | Assetto Corsa Competizione")
+`;export{e as default};
