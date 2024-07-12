3a5e5c7359cb91ccb54d1945b926985dc6ae29bf
Reinier Klarenberg <reinierklarenberg@gmail.com>
2024-07-12 16:58:57 +0200

    comment preprocessor for track map overlay
    will allow the hud to be shown in release builds

--- Race_Element.HUD.ACC/Overlays/Driving/TrackMap/TrackMapOverlay.cs
+++ Race_Element.HUD.ACC/Overlays/Driving/TrackMap/TrackMapOverlay.cs
@@ -16,14 +16,14 @@ using RaceElement.Data;
 
 namespace RaceElement.HUD.ACC.Overlays.Driving.TrackMap;
 
-#if DEBUG
+//#if DEBUG
 [Overlay(Name = "Track Map",
     Description = "Shows a track map",
     OverlayCategory = OverlayCategory.Track,
     OverlayType = OverlayType.Drive,
     Authors = ["Andrei Jianu"]
 )]
-#endif
+//#endif
 internal sealed class TrackMapOverlay : AbstractOverlay
 {
     private readonly TrackMapConfiguration _config = new();
