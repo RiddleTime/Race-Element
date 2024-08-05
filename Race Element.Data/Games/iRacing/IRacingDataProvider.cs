3ca152bcb49629a4f95f428bf0fe910a94ab4240
Dirk <>
2024-08-05 14:35:54 -0700

    Add AC1 and iRacing support for 3 simple HUDs as PoC

--- Race Element.Data/Games/iRacing/IRacingDataProvider.cs
+++ Race Element.Data/Games/iRacing/IRacingDataProvider.cs
@@ -6,6 +6,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
+using static RaceElement.Data.Games.iRacing.SDK.IRacingSdkEnum;
 
 // https://github.com/mherbold/IRSDKSharper
 // https://sajax.github.io/irsdkdocs/telemetry/
@@ -14,7 +16,7 @@ namespace RaceElement.Data.Games.iRacing
     internal static class IRacingDataProvider
     {
         private static IRSDKSharper _iRacingSDK;
-        internal static IRSDKSharper IRacingSDK
+        internal static IRSDKSharper IRacingSDK        
         {
             get
             {
@@ -49,20 +51,31 @@ namespace RaceElement.Data.Games.iRacing
                 if (driverModel != null)
                 {
                     localCar.Race.CarNumber = driverModel.CarNumberRaw;
-                    localCar.Engine.MaxRpm = (int)iRacing.Data.SessionInfo.DriverInfo.DriverCarSLBlinkRPM;
+                    localCar.Engine.MaxRpm = (int)iRacing.Data.SessionInfo.DriverInfo.DriverCarSLLastRPM;
                     localCar.Engine.Rpm = (int)iRacing.Data.GetFloat("RPM");
+                    localCar.CarModel.GameName = driverModel.CarScreenNameShort;
+                    // TODO: find pit limiter flag in iRacing SDK                                           
+                    //if (EngineWarnings.PitSpeedLimiter.HasFlag(EngineWarnings.PitSpeedLimiter)) {
+                    //    Debug.WriteLine("pit limiter on");
+                    //} else {
+                        //Debug.WriteLine("Engine warnings " + EngineWarnings.PitSpeedLimiter.ToString());
+                    //}
+                    localCar.Engine.IsPitLimiterOn = false;
                 }
 
                 localCar.Physics.Velocity = iRacing.Data.GetFloat("Speed") * 3.6f;
                 localCar.Race.GlobalPosition = iRacing.Data.GetInt("PlayerCarPosition");
-
-
+                
+                localCar.Inputs.Gear = iRacing.Data.GetInt("Gear") + 1;
+                localCar.Inputs.Brake = iRacing.Data.GetFloat("Brake");
+                localCar.Inputs.Throttle = iRacing.Data.GetFloat("Throttle");
+                localCar.Inputs.Steering = iRacing.Data.GetFloat("SteeringWheelAngle");
 
                 sessionData.Weather.AirTemperature = iRacing.Data.GetFloat("AirTemp");
                 sessionData.Weather.AirVelocity = iRacing.Data.GetFloat("WindVel") * 3.6f;
                 sessionData.Track.Temperature = iRacing.Data.GetFloat("TrackTempCrew");
                 sessionData.Track.GameName = iRacing.Data.SessionInfo.WeekendInfo.TrackName;
-
+                
             }
             catch (Exception)
             {
--- Race_Element.HUD.ACC/Overlays/Driving/InputTrace/InputDataJob.cs
+++ Race_Element.HUD.ACC/Overlays/Driving/InputTrace/InputDataJob.cs
@@ -1,6 +1,6 @@
 ﻿using RaceElement.Core.Jobs.LoopJob;
+using RaceElement.Data.Common;
 using System.Collections.Generic;
-using static RaceElement.ACCSharedMemory;
 
 namespace RaceElement.HUD.ACC.Overlays.Driving.InputTrace;
 
@@ -49,23 +49,22 @@ internal sealed class InputDataJob : AbstractLoopJob
         if (!Overlay.ShouldRender())
             return;
 
-        SPageFilePhysics filePhysics = Overlay.pagePhysics;
         lock (Throttle)
-        {
-            Throttle.AddFirst((int)(filePhysics.Gas * 100));
+        {            
+            Throttle.AddFirst((int)(SimDataProvider.LocalCar.Inputs.Throttle * 100));
             if (Throttle.Count > DataCount)
                 Throttle.RemoveLast();
         }
         lock (Brake)
-        {
-            Brake.AddFirst((int)(filePhysics.Brake * 100));
+        {            
+            Brake.AddFirst((int)(SimDataProvider.LocalCar.Inputs.Brake * 100));
             if (Brake.Count > DataCount)
                 Brake.RemoveLast();
         }
 
         lock (Steering)
         {
-            Steering.AddFirst((int)((filePhysics.SteerAngle + 1.0) / 2 * 100));
+            Steering.AddFirst((int)((SimDataProvider.LocalCar.Inputs.Steering + 1.0) / 2 * 100));
             if (Steering.Count > DataCount)
                 Steering.RemoveLast();
         }
