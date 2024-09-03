abd873d18e3c5b1900d7edaa40f3ce8433a02b81
Reinier Klarenberg <reinierklarenberg@gmail.com>
2024-09-03 19:53:04 +0200

    add track distance overlay

---
+++ Race_Element.HUD.ACC/Overlays/Driving/TrackDistance/TrackDistanceOverlay.cs
@@ -0,0 +1,40 @@
+﻿using RaceElement.Data.ACC.Tracks;
+using RaceElement.HUD.Overlay.Configuration;
+using RaceElement.HUD.Overlay.Internal;
+using RaceElement.HUD.Overlay.Util;
+using System.Drawing;
+
+namespace RaceElement.HUD.ACC.Overlays.Driving.TrackDistance;
+
+[Overlay(Name = "Track Distance", Description = "Shows the total track distance and current distance on track.")]
+internal sealed class TrackDistanceOverlay(Rectangle rectangle) : AbstractOverlay(rectangle, "Track Distance")
+{
+    private readonly TrackDistanceConfiguration _config = new();
+    private sealed class TrackDistanceConfiguration : OverlayConfiguration
+    {
+        public TrackDistanceConfiguration() => GenericConfiguration.AllowRescale = true;
+    }
+
+    private InfoPanel _panel;
+
+    public override void SetupPreviewData()
+    {
+        pageGraphics.NormalizedCarPosition = 0.312f;
+        pageStatic.Track = "Paul_Ricard";
+    }
+
+    public sealed override void BeforeStart() => _panel = new InfoPanel(12, 300);
+
+    public sealed override void Render(Graphics g)
+    {
+        var currentTrack = TrackData.GetCurrentTrack(pageStatic.Track);
+        if (currentTrack == null)
+            _panel.AddLine($"No Track", "Waiting...");
+        else
+            _panel.AddProgressBarWithCenteredText($"{pageGraphics.NormalizedCarPosition * currentTrack.TrackLength:F1}M", 0, 1, pageGraphics.NormalizedCarPosition);
+
+        _panel.AddLine("Stint Distance", $"{pageGraphics.DistanceTraveled:F0}M");
+
+        _panel.Draw(g);
+    }
+}
