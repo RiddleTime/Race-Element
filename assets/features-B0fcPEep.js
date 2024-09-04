ffa562d72b2235d2a5904a906e731a75852d1716
Reinier Klarenberg <reinierklarenberg@gmail.com>
2024-09-04 18:40:45 +0000

    Auto-generated commit
    Triggered by commit: https://github.com/RiddleTime/Race-Element/commit/6b9d5042cbe66bd63f8969d1492c3c2b3f6cf596

--- assets/features-B0fcPEep.js
+++ assets/features-B0fcPEep.js
@@ -13,7 +13,7 @@ Get started and download the app: **[Download](/guide/how-to-get-started)**
 - Assetto Corsa Competizione
 - Assetto Corsa (beta)
 - iRacing (beta)
-- RaceRoom
+- RaceRoom (beta)
 
 # HUDs (40+)
 HUDs(Head-up displays) are 2D graphics that can be used to provide you data whilst driving. There are a various amount of HUDs that each provides you useful information in a minimalistic manner whilst being highly configurable.
