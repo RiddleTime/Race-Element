2d25da7d7b69de132fec619190023f1a1764aded
Reinier Klarenberg <reinierklarenberg@gmail.com>
2024-09-04 22:38:23 +0200

    Update RR3LocalCarMapper.cs
    map fuel and some race data

--- Race Element.Data/Games/RaceRoom/DataMapper/RR3LocalCarMapper.cs
+++ Race Element.Data/Games/RaceRoom/DataMapper/RR3LocalCarMapper.cs
@@ -13,9 +13,15 @@ internal static partial class RR3LocalCarMapper
     [MapProperty(nameof(Shared.Clutch), nameof(@LocalCarData.Inputs.Clutch))]
     [MapProperty(nameof(Shared.SteerInputRaw), nameof(@LocalCarData.Inputs.Steering))]
     [MapProperty(nameof(Shared.SteerWheelRangeDegrees), nameof(@LocalCarData.Inputs.MaxSteeringAngle))]
+    // Fuel
+    [MapProperty(nameof(Shared.FuelLeft), nameof(@LocalCarData.Engine.FuelLiters))]
+    [MapProperty(nameof(Shared.FuelPerLap), nameof(@LocalCarData.Engine.FuelLitersXLap))]
+    [MapProperty(nameof(Shared.FuelCapacity), nameof(@LocalCarData.Engine.MaxFuelLiters))]
     // Race
     [MapProperty(nameof(Shared.Position), nameof(@LocalCarData.Race.GlobalPosition))]
     [MapProperty(nameof(Shared.PositionClass), nameof(@LocalCarData.Race.ClassPosition))]
+    [MapProperty(nameof(Shared.LapDistanceFraction), nameof(@LocalCarData.Race.LapPositionPercentage))]
+    [MapProperty(nameof(Shared.CompletedLaps), nameof(@LocalCarData.Race.LapsDriven))]
     static partial void WithR3SharedMemory(Shared sharedData, LocalCarData localCarData);
 
     public static void AddR3SharedMemory(Shared sharedData, LocalCarData localCarData)
