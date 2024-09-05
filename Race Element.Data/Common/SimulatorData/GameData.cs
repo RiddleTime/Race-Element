bde587fb852aff0fc2aad2deddfaaa8c41d25e51
Reinier Klarenberg <reinierklarenberg@gmail.com>
2024-09-05 03:22:18 +0200

    Update GameData.cs
    Name and version are now non-nullable, they are either string.empty or they are filled out

--- Race Element.Data/Common/SimulatorData/GameData.cs
+++ Race Element.Data/Common/SimulatorData/GameData.cs
@@ -3,7 +3,7 @@ namespace RaceElement.Data.Common.SimulatorData
 {
     public sealed record GameData
     {
-        public string? Name { get; set; } = string.Empty;
-        public string? Version { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string Version { get; set; } = string.Empty;
     }
 }
