aefb62e4326dcbd98d9cc31e6f116e1437d4fec8
Reinier Klarenberg <reinierklarenberg@gmail.com>
2024-09-20 20:20:10 +0200

    Update InputTraceOverlay.cs
    refactor to primary constructor and add nullables to private members for code clarity

--- Race Element.HUD.Common/Overlays/Driving/InputTrace/InputTraceOverlay.cs
+++ Race Element.HUD.Common/Overlays/Driving/InputTrace/InputTraceOverlay.cs
@@ -4,29 +4,28 @@ using System.Drawing;
 
 namespace RaceElement.HUD.Common.Overlays.Driving.InputTrace;
 
-[Overlay(Name = "Input Trace",
-Description = "Live graph of steering, throttle and brake inputs.",
-Version = 1.00,
-Authors = ["Reinier Klarenberg, Dirk Wolf"],
-OverlayType = OverlayType.Drive,
-OverlayCategory = OverlayCategory.Inputs)]
-internal sealed class InputTraceOverlay : CommonAbstractOverlay
+[Overlay(
+    Name = "Input Trace",
+    Description = "Live graph of steering, throttle and brake inputs.",
+    Version = 1.00,
+    Authors = ["Reinier Klarenberg, Dirk Wolf"],
+    OverlayType = OverlayType.Drive,
+    OverlayCategory = OverlayCategory.Inputs
+)]
+internal sealed class InputTraceOverlay(Rectangle rectangle) : CommonAbstractOverlay(rectangle, "Input Trace")
 {
     private readonly InputTraceConfiguration _config = new();
 
-    private InputGraph _graph;
-    private InputDataJob _dataJob;
+    private InputGraph? _graph;
+    private InputDataJob? _dataJob;
 
-    public InputTraceOverlay(Rectangle rectangle) : base(rectangle, "Input Trace")
+    public sealed override void BeforeStart()
     {
         this.Width = _config.Chart.Width;
         this.Height = _config.Chart.Height;
         this.RefreshRateHz = _config.Chart.HudRefreshRate;
         this.RefreshRateHz.ClipMax(_config.Data.Herz);
-    }
 
-    public sealed override void BeforeStart()
-    {
         _dataJob = new(this, _config.Chart.Width - 1) { IntervalMillis = 1000 / _config.Data.Herz };
         _graph = new InputGraph(0, 0, _config.Chart.Width - 1, _config.Chart.Height - 1, _dataJob, this._config);
 
