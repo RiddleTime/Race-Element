4f474218488ebaabe045ee422429b5df9848103d
Reinier Klarenberg <reinierklarenberg@gmail.com>
2024-09-23 18:37:11 +0000

    Auto-generated commit
    Triggered by commit: https://github.com/RiddleTime/Race-Element/commit/71c880fdb2659c63609d025181e43efd39f58242

--- assets/version-2-released-B_q0NazV.js
+++ assets/version-2-released-B_q0NazV.js
@@ -10,9 +10,9 @@ With version 2 the app has become a multi-sim app, it now supports: Assetto Cors
 The original ACC support remains with the 3 new simulators gaining support for the HUD system.
 
 # Dynamic HUD Tab
-Not every HUD being supported for every simulator as not every simulator provides the same data about the cars and other drivers, the HUD tab now reloads itself and only shows those HUDs that are compatible with the selected simulator.
+Not every HUD is supported for every simulator as not every simulator provides the same data about the cars and other drivers, the HUD tab now reloads itself and only shows those HUDs that are compatible with the selected simulator.
 
 # What's to come?
-There is more common data to be mapped for the existing simulator to provide a better experience for HUDs like standings and relatives. 
+There is more common data to be mapped for the existing simulators to provide a better experience for HUDs like standings and relatives. 
 Additionally other simulators like rFactor 2 and perhaps automobilista will be added.
 `;export{e as default};
