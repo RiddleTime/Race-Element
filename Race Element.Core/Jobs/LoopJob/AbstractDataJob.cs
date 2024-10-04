ae1ac54d62ee2b865c1eabf647ab4cdca42fd8c6
Reinier Klarenberg <reinierklarenberg@gmail.com>
2024-10-05 00:47:26 +0200

    Update AbstractDataJob.cs

--- Race Element.Core/Jobs/LoopJob/AbstractDataJob.cs
+++ Race Element.Core/Jobs/LoopJob/AbstractDataJob.cs
@@ -6,10 +6,5 @@ public abstract class AbstractDataJob<T> : AbstractLoopJob where T : struct
 {
     public event EventHandler<T> OnCollected;
     public abstract T Collect();
-
-    public sealed override void RunAction()
-    {
-        T collectedType = Collect();
-        OnCollected.Invoke(this, collectedType);
-    }
+    public sealed override void RunAction() => OnCollected.Invoke(this, Collect());
 }
