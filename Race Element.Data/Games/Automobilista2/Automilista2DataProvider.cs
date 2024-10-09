9f535fca85b185676eccd5c073f7cbeed3da616f
iFuSiiOnzZ <andreisystem@gmail.com>
2024-10-09 20:11:12 +0200

    Create data provider and translate data to Local Car

---
+++ Race Element.Data/Games/Automobilista2/Automilista2DataProvider.cs
@@ -0,0 +1,59 @@
+﻿using System.Diagnostics;
+using RaceElement.Data.Common.SimulatorData;
+using RaceElement.Data.Common.SimulatorData.LocalCar;
+using RaceElement.Data.Games.Automobilista2.DataMapper;
+using RaceElement.Data.Games.Automobilista2.SharedMemory;
+
+namespace RaceElement.Data.Games.Automobilista2;
+
+internal sealed class Automobilista2DataProvider : AbstractSimDataProvider
+{
+    private bool _isGameRunning = false;
+    private DateTime _lastGameRunningCheck = DateTime.MinValue;
+
+    internal sealed override void Start()
+    {
+    }
+
+    internal sealed override void Stop()
+    {
+    }
+
+    public override List<string> GetCarClasses() => [];
+    public override bool HasTelemetry() => false;
+    internal override int PollingRate() => 300;
+
+    public sealed override void Update(ref LocalCarData localCar, ref SessionData sessionData, ref GameData gameData)
+    {
+        if (!_isGameRunning)
+        {
+            if (DateTime.UtcNow.Subtract(TimeSpan.FromSeconds(2)) > _lastGameRunningCheck)
+            {
+                if (Process.GetProcessesByName("AMS2AVX").Length > 0)
+                    _isGameRunning = true;
+                else
+                    _lastGameRunningCheck = DateTime.UtcNow;
+            }
+        }
+        else
+        {
+            try
+            {
+                Shared sharedMemory = AMS2SharedMemory.ReadSharedMemory();
+                Ams2LocalCarMapper.ToLocalCar(sharedMemory, localCar);
+
+                // Game Data
+                gameData.Name = Game.Automobilista2.ToShortName();
+                gameData.Version = $"{sharedMemory.mVersion} - {sharedMemory.mBuildVersionNumber}";
+                gameData.IsGamePaused = sharedMemory.mGameState == (UInt32)(Constants.GameState.GAME_INGAME_PAUSED);
+                gameData.IsInReplay = sharedMemory.mGameState == (UInt32)(Constants.GameState.GAME_INGAME_REPLAY);
+            }
+            catch (Exception e)
+            {
+                _isGameRunning = false;
+                _lastGameRunningCheck = DateTime.UtcNow;
+                Debug.WriteLine(e);
+            }
+        }
+    }
+}
---
+++ Race Element.Data/Games/Automobilista2/DataMapper/AMS2LocalCarMapper.cs
@@ -0,0 +1,53 @@
+﻿using RaceElement.Data.Games.Automobilista2.SharedMemory;
+using RaceElement.Data.Common.SimulatorData.LocalCar;
+using Riok.Mapperly.Abstractions;
+
+namespace RaceElement.Data.Games.Automobilista2.DataMapper;
+
+[Mapper]
+internal static partial class Ams2LocalCarMapper
+{
+    public static void ToLocalCar(Shared sharedData, LocalCarData localCarData)
+    {
+        // Is running as long as current session is something valid.
+        localCarData.Engine.IsRunning = sharedData.mSessionState != (int)Constants.GameSession.SESSION_INVALID;
+
+        // Player info
+        localCarData.Race.GlobalPosition = (int)sharedData.mParticipantInfo[0].mRacePosition;
+        localCarData.CarModel.CarClass = new string(sharedData.mCarClassName.Data);
+
+        // Car physics
+        localCarData.Physics.Location = sharedData.mParticipantInfo[0].mWorldPosition.ToVector3();
+        localCarData.Physics.Acceleration = sharedData.mWorldAcceleration.ToVector3();
+        localCarData.Physics.Velocity = sharedData.mOdometerKM;
+
+        // Car engine
+        localCarData.Engine.MaxFuelLiters = sharedData.mFuelCapacity;
+        localCarData.Engine.FuelLiters = sharedData.mFuelLevel * sharedData.mFuelCapacity;
+
+        // Car inputs
+        localCarData.Inputs.Steering = sharedData.mSteering;
+        localCarData.Inputs.Throttle = sharedData.mThrottle;
+        localCarData.Inputs.Clutch = sharedData.mClutch;
+        localCarData.Inputs.Brake = sharedData.mBrake;
+        localCarData.Inputs.Gear = sharedData.mGear + 1;
+
+        // Tyres core temp
+        localCarData.Tyres.CoreTemperature[0] = sharedData.mTyreTemp.FL;
+        localCarData.Tyres.CoreTemperature[1] = sharedData.mTyreTemp.FR;
+        localCarData.Tyres.CoreTemperature[2] = sharedData.mTyreTemp.RL;
+        localCarData.Tyres.CoreTemperature[3] = sharedData.mTyreTemp.RR;
+
+        // Tyres surface temp
+        localCarData.Tyres.SurfaceTemperature[0] = sharedData.mTyreCarcassTemp.FL - 273.15f;
+        localCarData.Tyres.SurfaceTemperature[1] = sharedData.mTyreCarcassTemp.FR - 273.15f;
+        localCarData.Tyres.SurfaceTemperature[2] = sharedData.mTyreCarcassTemp.RL - 273.15f;
+        localCarData.Tyres.SurfaceTemperature[3] = sharedData.mTyreCarcassTemp.RR - 273.15f;
+
+        // Brake temp
+        localCarData.Brakes.DiscTemperature[0] = sharedData.mBrakeTempCelsius.FL;
+        localCarData.Brakes.DiscTemperature[1] = sharedData.mBrakeTempCelsius.FR;
+        localCarData.Brakes.DiscTemperature[2] = sharedData.mBrakeTempCelsius.RL;
+        localCarData.Brakes.DiscTemperature[3] = sharedData.mBrakeTempCelsius.RR;
+    }
+}
