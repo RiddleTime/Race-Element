1de8ee3765f6dc8073fbad1920e1e2d4cdda9868
Reinier Klarenberg <reinierklarenberg@gmail.com>
2024-11-09 20:54:17 +0000

    Auto-generated commit
    Triggered by commit: https://github.com/RiddleTime/Race-Element/commit/ea5a2a4a6772dd372efada2feb6a53fceb93e8c9

--- assets/news-highspeed-rendering--xfhbFW0.js
+++ assets/news-highspeed-rendering--xfhbFW0.js
@@ -1,7 +1,7 @@
 const e=`---
 title: Highspeed Rendering
 slug: highspeed-rendering
-description: Rendering at 200 Hz with minimal CPU usage
+description: Rendering Overlays at 200 Hz with minimal CPU usage
 date: 2024-11-09
 type: news
 ---
