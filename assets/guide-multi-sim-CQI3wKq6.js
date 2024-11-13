ae62135820cb91d6a35a54df6bfabbac77a2df3b
Reinier Klarenberg <reinierklarenberg@gmail.com>
2024-11-13 22:50:53 +0000

    Auto-generated commit
    Triggered by commit: https://github.com/RiddleTime/Race-Element/commit/6bfbe72b7bdee2e376f21dc2d177ca46df5a1fe1

---
+++ assets/guide-multi-sim-CQI3wKq6.js
@@ -0,0 +1,23 @@
+const e=`---
+title: "Multi-Sim"
+slug: multi-sim
+description: Getting Race Element Ready for Multi-Sim
+type: guide
+---
+
+<h1 id="prequisites">Prequisites</h1>
+<p>For some games you need are required to do some small steps so Race Element has access to all data it needs.</p>
+<h2 id="assetto-corsa-1">Assetto Corsa 1</h2>
+<p>AC 1 needs the Crew Chief plugin installed. It will provide more telemetry on opponents cars than the telemetry that comes with AC1 out of the box. Once Crew Chief installs the plugin, it doesn&#39;t need to run while playing AC and using Race Element HUDs. The installation steps are:</p>
+<ol>
+<li>Install Crew Chief from <a href="https://thecrewchief.org/forumdisplay.php?28-Download-and-Links">https://thecrewchief.org/forumdisplay.php?28-Download-and-Links</a></li>
+<li>Start Crew Chief. Select &quot;Assetto Corsa&quot; as &quot;Game&quot; and use the &quot;Start Crew Chief Button&quot;. It should provide a dialog saying it will install the Crew Chief plugin</li>
+<li>Start AC1. Select the &quot;Crew Chief&quot; app on the right border where all the apps are listed. At this point Crew Chief does not have to be running anymore.</li>
+</ol>
+<h2 id="american-truck-simulator--euro-truck-simulator-2">American Truck Simulator &amp; Euro Truck Simulator 2</h2>
+<p>For both simulators Download the Latest Plugin Release and Follow the instructions here:</p>
+<ol>
+<li>For both simulators you will need to &quot;Install&quot; a plugin: <em>scs-telemetry.dll</em></li>
+<li>Download the latest release and Follow the installation instructions here: <strong><a href="https://github.com/RenCloud/scs-sdk-plugin?tab=readme-ov-file#installation" target="_blank">https://github.com/RenCloud/scs-sdk-plugin?tab=readme-ov-file#installation</a></strong></li>
+</ol>
+`;export{e as default};
