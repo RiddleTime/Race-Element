eb85ab4b3bd8cfdca8143340f329caa80bb6cdfe
Reinier Klarenberg <reinierklarenberg@gmail.com>
2025-01-15 00:31:12 +0000

    Auto-generated commit
    Triggered by commit: https://github.com/RiddleTime/Race-Element/commit/65a413ab9c290981c6ff4ebf946a73a2fcf7c163

---
+++ assets/Assette-Corsa-EVO-and-Race-Element-UUptBRyv.js
@@ -0,0 +1,15 @@
+const e=`---
+title: Assetto Corsa EVO and Race Element
+slug: Assetto-Corsa-EVO-Race-Element-Future
+description: Why was Race Element prepared for a future simulator in the Assetto Corsa Series?
+date: 2025-01-15
+type: news
+---
+
+<h1 id="assetto-corsa-evo">Assetto Corsa EVO</h1>
+<p>As soon as there is a 3rd party API available, we will immediately act and try to support this as soon as possible so you can use your minimalistic and highly customizable HUDs with your new favorite Racing Simulator!</p>
+<h1 id="race-element-already-supports-7-driving-simulators">Race Element already supports 7 driving simulators!</h1>
+<p>Over the past months contributors on github have been working hard on making it possible to write code only once for multiple simulators. This has translated over to for example the DSX support which now provides active triggers for both Assetto Corsa and RaceRoom using the DualSense controller. Race Element is ready to provide a good basic HUD integration system that is ready for the new Assetto Corsa EVO!</p>
+<h1 id="low-performance-impact">Low Performance Impact</h1>
+<p>The Custom Rendering Pipeline of Race Element allows it to be highly customizable whilst certainly effecient considering the HUDs refresh up to 240 Hz! This will translate over to Assetto Corsa EVO as it will support Ray Tracing and multiple other advances over the older series. The less random loads on your Graphics Card the better, Race Element prevents this by not using the graphics card for rendering.</p>
+`;export{e as default};
