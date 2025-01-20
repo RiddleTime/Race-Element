6502755b4a1a68a04bbbc1e05ce8903da0c21483
Reinier Klarenberg <reinierklarenberg@gmail.com>
2025-01-20 17:46:31 +0000

    Auto-generated commit
    Triggered by commit: https://github.com/RiddleTime/Race-Element/commit/ee3692894c30ff50087f564263786f48148ce05e

---
+++ assets/assetto-corsa-evo-early-access-supported-Dorf_wr9.js
@@ -0,0 +1,13 @@
+const e=`---
+title: Assetto Corsa EVO Early Access is now supported by Race Element!
+slug: Assetto-Corsa-EVO-Race-Element-supported
+description: Custom HUDs & Adaptive Triggers DualSense Controller 
+date: 2025-01-20
+type: news
+---
+
+<h1 id="optimized-huds-and-high-refreshrates">Optimized HUDs and High Refreshrates.</h1>
+<p>Initially there is support for several highly optimized HUDs, they only utilize your cpu to render so your graphics card isn&#39;t being interrupted when already being heavily loaded by Assetto Corsa EVO. The Shift Bar can render up to 240Hz, so you will never miss a shift!</p>
+<h1 id="dualsense-adaptiveactive-triggers">DualSense Adaptive(Active) Triggers</h1>
+<p>Race Element is able to use the game&#39;s 3rd party telemetry data to provide active triggers for the DualSense Controller. This highly improves the playability and enjoyment when playing Assetto Corsa EVO. You will need DSX on steam for this to work, but it is worth it.</p>
+`;export{e as default};
