9e34916f5f4ad04c5b7f0cf8f3607fa48baefcac
Reinier Klarenberg <reinierklarenberg@gmail.com>
2025-04-07 15:49:55 +0000

    Auto-generated commit
    Triggered by commit: https://github.com/RiddleTime/Race-Element/commit/c41453d26b1ceb69a01dd657faf2111b17e1b3d1

--- assets/features-XfYmQ5D_.js
+++ assets/features-XfYmQ5D_.js
@@ -6,9 +6,9 @@ description: General features
 
 <img src="https://github.com/user-attachments/assets/a7686e22-3028-4c88-b93b-a797522b27ff" alt="Main Menu of Race Element" />
 
+<h2 id="download-and-installation"><strong><a href="/guide/how-to-get-started">Download and Installation</a></strong></h2>
 <h1 id="race-element">Race Element</h1>
 <p>Race Element is a lightweight application for sim racing that is designed to run with minimum system requirements. The question I asked myself when trying other HUD applications is: Why are all these apps abusing my cpu, ram and graphics card to a point where the lost performance is enough to buy another step up in graphics cards. That ends now!</p>
-<p>Get started and download the app: <strong><a href="/guide/how-to-get-started">Download</a></strong></p>
 <h1 id="supported-simulators">Supported Simulators</h1>
 <ul>
 <li>Assetto Corsa Competizione</li>
