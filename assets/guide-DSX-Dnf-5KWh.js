0c7dcf331e074cdf97e19d8bb7813a4004f3a02d
Reinier Klarenberg <reinierklarenberg@gmail.com>
2025-05-09 16:24:45 +0000

    Auto-generated commit
    Triggered by commit: https://github.com/RiddleTime/Race-Element/commit/0446c85c710a35dad97baaa850b25f9c79fb37db

--- assets/guide-DSX-Dnf-5KWh.js
+++ assets/guide-DSX-Dnf-5KWh.js
@@ -33,6 +33,6 @@ type: guide
 <li>At the bottom of the Hud Tab, Click the Pitwall Section</li>
 <li>Click DSX</li>
 <li>You can alter behaviour for Braking and Acceleration</li>
-<li>Follow the Guide on How to use the HUD tab for more information</li>
+<li>For more info read the guide for <strong><a href="/guide/how-to-use-huids">Using the HUD Tab</a></strong></li>
 </ol>
 `;export{e as default};
