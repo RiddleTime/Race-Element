fe55666404f10365bb8c37655d3a2804b3d9a946
Reinier Klarenberg <reinierklarenberg@gmail.com>
2025-05-09 14:46:59 +0000

    Auto-generated commit
    Triggered by commit: https://github.com/RiddleTime/Race-Element/commit/fc42106c3d0e4fc67c973dc7a286bff1855d10b1

--- assets/guide-DSX-SPVYDIR5.js
+++ assets/guide-DSX-SPVYDIR5.js
@@ -11,11 +11,11 @@ type: guide
 <ol>
 <li>Open steam</li>
 <li>Go to your Library</li>
-<li>Right click DSX and go to Properties.</li>
+<li>Right click DSX and go to Properties</li>
 <li>Go to Betas and select <code>dsx_v3.1_beta - v3.1 Beta</code></li>
-<li>Open DSX and at the left bottom click Settings.</li>
-<li>Open the Networking tab.</li>
-<li>Make sure the Firewall Rules are okay(else allow DSX to fix it.</li>
+<li>Open DSX and at the left bottom click Settings</li>
+<li>Open the Networking tab</li>
+<li>Make sure the Firewall Rules are okay(else allow DSX to fix it</li>
 <li>Enable <code>Incoming UDP</code></li>
 </ol>
 <h1 id="race-element">Race Element</h1>
@@ -24,8 +24,8 @@ type: guide
 <li>Make sure the game selection is set correctly</li>
 <li>Open the HUD Tab</li>
 <li>At the bottom of the Hud Tab, Click the Pitwall Section</li>
-<li>Click DSX.</li>
-<li>You can alter behaviour for Braking and acceleration.</li>
-<li>Follow the Guide on How to use the HUD tab for more information.</li>
+<li>Click DSX</li>
+<li>You can alter behaviour for Braking and acceleration</li>
+<li>Follow the Guide on How to use the HUD tab for more information</li>
 </ol>
 `;export{e as default};
