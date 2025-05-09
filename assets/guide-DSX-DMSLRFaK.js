88d8a30e80bd7b3a3da473ee2941cab4e55cba29
Reinier Klarenberg <reinierklarenberg@gmail.com>
2025-05-09 14:40:52 +0000

    Auto-generated commit
    Triggered by commit: https://github.com/RiddleTime/Race-Element/commit/29bbb97e5df899dd1b222372f4f62b92c2000e5f

---
+++ assets/guide-DSX-DMSLRFaK.js
@@ -0,0 +1,30 @@
+const e=`---
+title: "Using DSX with Race Element for Dualsense Active Triggers"
+slug: DSX
+description: Hown to set up DSX and Race Element
+type: guide
+---
+
+<h1 id="what-are-active-triggers">What are active triggers?</h1>
+<p>Active triggers behave is active Force Feedback based on the physics of the car. It allows you to feel under and oversteer during acceleration and braking. </p>
+<h1 id="dsx">DSX</h1>
+<ol>
+<li>Open steam</li>
+<li>Go to your Library</li>
+<li>Right click DSX and go to Properties.</li>
+<li>Go to Betas and select <code>dsx_v3.1_beta - v3.1 Beta</code></li>
+<li>Open DSX and at the left bottom click Settings.</li>
+<li>Open the Networking tab.</li>
+<li>Make sure the Firewall Rules are okay(else allow DSX to fix it.</li>
+<li>Enable <code>Incoming UDP</code></li>
+</ol>
+<h1 id="race-element">Race Element</h1>
+<ol>
+<li>Open Race Element</li>
+<li>Make sure the game selection is set correctly</li>
+<li>Open the HUD Tab</li>
+<li>At the bottom of the Hud Tab, Click the Pitwall Section</li>
+<li>Click DSX.</li>
+<li>Follow the Guide on How to use the HUD tab for more information.</li>
+</ol>
+`;export{e as default};
