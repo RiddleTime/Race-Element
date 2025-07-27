42039da44f5d2a9ceef8f9f6d207ea5727fa844b
Reinier Klarenberg <reinierklarenberg@gmail.com>
2025-07-27 17:15:45 +0000

    Auto-generated commit
    Triggered by commit: https://github.com/RiddleTime/Race-Element/commit/e3506908669611106e15d9813d680b1bc336ac2c

--- assets/guide-video-guides-BVWq5XbY.js
+++ assets/guide-video-guides-BVWq5XbY.js
@@ -7,6 +7,9 @@ type: guide
 
 <h1 id="video-guides">Video Guides</h1>
 <p>As my spare time is mostly involving the development of the app, I rely on the community to create video guides in for example other languages than English. If you have made a guide, please let me know so I can add it.</p>
+<h1 id="english">English</h1>
+<h2 id="race-element-hud-full-setup-guide">RACE ELEMENT HUD FULL SETUP GUIDE</h2>
+<p><a href="https://www.youtube.com/watch?v=hN0RnRmo4ZE" target="_blank"><img src="http://img.youtube.com/vi/hN0RnRmo4ZE/0.jpg" alt="IMAGE ALT TEXT"></a></p>
 <h1 id="portuguese--português">Portuguese / Português</h1>
 <h2 id="race-element--esse-app-vai-ser-o-seu-melhor-amigo-no-acc-image-alt-text">RACE ELEMENT | ESSE APP VAI SER O SEU MELHOR AMIGO NO ACC! <a href="https://www.youtube.com/watch?v=D8XTKFrADiM" target="_blank"><img src="http://img.youtube.com/vi/D8XTKFrADiM/0.jpg" alt="IMAGE ALT TEXT"></a></h2>
 <h2 id="como-utilizar-o-race-elements--assetto-corsa-competizione-image-alt-text">COMO utilizar o Race Elements | Assetto Corsa Competizione <a href="https://www.youtube.com/watch?v=dP3ELQzdgG0" target="_blank"><img src="http://img.youtube.com/vi/dP3ELQzdgG0/0.jpg" alt="IMAGE ALT TEXT"></a></h2>
