3634321f3549375e8fd846eeed5d5b93870306a7
Reinier Klarenberg <reinierklarenberg@gmail.com>
2025-08-15 18:41:31 +0200

    Update PressureHistoryConfiguration.cs
    refactor

--- Race_Element.HUD.ACC/Overlays/Driving/PressureHistory/PressureHistoryConfiguration.cs
+++ Race_Element.HUD.ACC/Overlays/Driving/PressureHistory/PressureHistoryConfiguration.cs
@@ -1,9 +1,4 @@
 ﻿using RaceElement.HUD.Overlay.Configuration;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace RaceElement.HUD.ACC.Overlays.Driving.PressureHistory;
 
