12c99351b9e3afaff15181d4834cbf192f80c38b
Reinier Klarenberg <reinierklarenberg@gmail.com>
2025-09-03 00:07:17 +0200

    add base for Forza Horizon 5

--- Race Element.Data/Games/Game.cs
+++ Race Element.Data/Games/Game.cs
@@ -14,6 +14,7 @@ public enum Game : int
     EuroTruckSimulator2 = (1 << 6),
     AmericanTruckSimulator = (1 << 7),
     AssettoCorsaEvo = (1 << 8),
+    ForzaHorizon5 = (1 << 9),
     //rFactor2,
     // LMU,
 }
@@ -30,6 +31,7 @@ public static class GameExtensions
         public const string EuroTruckSimulator2 = "Euro Truck Simulator 2";
         public const string AmericanTruckSimulator = "American Truck Simulator";
         public const string AssettoCorsaEvo = "Assetto Corsa EVO";
+        public const string ForzaHorizon5 = "Forza Horizon 5";
     }
 
     private static class ShortNames
@@ -42,11 +44,16 @@ public static class GameExtensions
         public const string EuroTruckSimulator2 = "ETS2";
         public const string AmericanTruckSimulator = "ATS";
         public const string AssettoCorsaEvo = "ACE";
+        public const string ForzaHorizon5 = "FH5";
     }
 
     private static class ExeNames
     {
-        public static readonly string[] All = [AssettoCorsa, AssettoCorsaCompetizione, AssettoCorsaEvo, IRacing, RaceRoom, RaceRoomX64, Automobilista2, EuroTruckSimulator2, AmericanTruckSimulator];
+        public static readonly string[] All =
+        [
+            AssettoCorsa, AssettoCorsaCompetizione, AssettoCorsaEvo, IRacing, RaceRoom, RaceRoomX64, Automobilista2,
+            EuroTruckSimulator2, AmericanTruckSimulator, ForzaHorizon5
+        ];
 
         public const string AssettoCorsaCompetizione = "AC2-Win64-Shipping";
         public const string AssettoCorsa = "acs";
@@ -57,6 +64,7 @@ public static class GameExtensions
         public const string EuroTruckSimulator2 = "eurotrucks2";
         public const string AmericanTruckSimulator = "amtrucks";
         public const string AssettoCorsaEvo = "AssettoCorsaEVO";
+        public const string ForzaHorizon5 = "ForzaHorizon5";
     }
 
     public static Game GameFromProcessName(string processName) => processName switch
@@ -70,6 +78,7 @@ public static class GameExtensions
         ExeNames.EuroTruckSimulator2 => Game.EuroTruckSimulator2,
         ExeNames.AmericanTruckSimulator => Game.AmericanTruckSimulator,
         ExeNames.AssettoCorsaEvo => Game.AssettoCorsaEvo,
+        ExeNames.ForzaHorizon5 => Game.ForzaHorizon5,
         _ => Game.Any,
     };
 
@@ -83,6 +92,7 @@ public static class GameExtensions
         Game.EuroTruckSimulator2 => FriendlyNames.EuroTruckSimulator2,
         Game.AmericanTruckSimulator => FriendlyNames.AmericanTruckSimulator,
         Game.AssettoCorsaEvo => FriendlyNames.AssettoCorsaEvo,
+        Game.ForzaHorizon5 => FriendlyNames.ForzaHorizon5,
         _ => string.Empty
     };
 
@@ -96,6 +106,7 @@ public static class GameExtensions
         Game.EuroTruckSimulator2 => ShortNames.EuroTruckSimulator2,
         Game.AmericanTruckSimulator => ShortNames.AmericanTruckSimulator,
         Game.AssettoCorsaEvo => ShortNames.AssettoCorsaEvo,
+        Game.ForzaHorizon5 => ShortNames.ForzaHorizon5,
         _ => string.Empty
     };
 
@@ -109,6 +120,7 @@ public static class GameExtensions
         FriendlyNames.EuroTruckSimulator2 => Game.EuroTruckSimulator2,
         FriendlyNames.AmericanTruckSimulator => Game.AmericanTruckSimulator,
         FriendlyNames.AssettoCorsaEvo => Game.AssettoCorsaEvo,
+        FriendlyNames.ForzaHorizon5 => Game.ForzaHorizon5,
         _ => Game.AssettoCorsaCompetizione,
     };
 
@@ -122,6 +134,7 @@ public static class GameExtensions
         Game.EuroTruckSimulator2 => 227300,
         Game.AmericanTruckSimulator => 270880,
         Game.AssettoCorsaEvo => 3058630,
+        Game.ForzaHorizon5 => 1551360,
         _ => -1
     };
 
