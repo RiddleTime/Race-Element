bc194876b524c95deb028736393ac3f485d3f6fe
Reinier Klarenberg <reinierklarenberg@gmail.com>
2025-09-03 00:45:33 +0200

    add base fh5 data provider

---
+++ Race Element.Data/Games/Forza/ForzaDataProvider.cs
@@ -0,0 +1,32 @@
+﻿using RaceElement.Data.Common.SimulatorData;
+using RaceElement.Data.Common.SimulatorData.LocalCar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaceElement.Data.Games.Forza
+{
+    public class ForzaDataProvider : AbstractSimDataProvider
+    {
+        public override List<string> GetCarClasses() => [];
+
+        public override bool HasTelemetry() => false;
+
+        public override void Update(ref LocalCarData localCar, ref SessionData sessionData, ref GameData gameData)
+        {
+
+        }
+
+        internal override int PollingRate() => 60;
+
+        internal override void Start()
+        {
+        }
+
+        internal override void Stop()
+        {
+        }
+    }
+}
