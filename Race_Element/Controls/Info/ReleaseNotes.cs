fb31ce034e3da8654dff6bfcdd07401a68a645ae
Reinier Klarenberg <reinierklarenberg@gmail.com>
2025-09-22 00:15:47 +0200

    add releasenote for new raceroom update to support hybrid car data

--- Race_Element/Controls/Info/ReleaseNotes.cs
+++ Race_Element/Controls/Info/ReleaseNotes.cs
@@ -6,6 +6,9 @@ public static class ReleaseNotes
 {
     internal static readonly Dictionary<string, string> Notes = new()
     {
+        {"2.3.1.0", "RaceRoom:"+
+                    "\n- Updated Data Connector to support data for hybrid cars."                     
+                    },
         {"2.3.0.2", "Race Element:"+
                     "\n- Updated .Net Packages to 9.0.9"
                     },
