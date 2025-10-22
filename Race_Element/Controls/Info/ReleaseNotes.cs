6c0f13f5d1bbe0be07edd24e2b293efaa80a9df0
RiddleTime <reinierklarenberg@gmail.com>
2025-10-22 22:47:12 +0200

    update releasenotes

--- Race_Element/Controls/Info/ReleaseNotes.cs
+++ Race_Element/Controls/Info/ReleaseNotes.cs
@@ -6,25 +6,26 @@ public static class ReleaseNotes
 {
     internal static readonly Dictionary<string, string> Notes = new()
     {
-        {"2.4.0.0", "Race Element:"+
-                    "\n- Added Le Mans Ultimate, see multi-sim guide on website how to set it up."+
-                    "\n- Added rFactor 2, see multi-sim guide on website how to set it up."+
-                    "\n- Added WRC Generations, see multi-sim guide on website how to set it up."+
+        {"2.4.0.0", "Added 3 New Driving Simulators"+
+                    "\n- Le Mans Ultimate"+
+                    "\n- rFactor 2"+
+                    "\n- WRC Generations"+
+                    "\n- Configure by reading multi-sim guide on website!!"+
+                    "\n- DSX: Added support for Le Mans Ultimate, rFactor 2 and WRC Generations."+
+
+                    "\n\nMulti-Sim HUDs"+
                     "\n- Added New Speedometer HUD for all other games than ACC."+
-                    "\n- Fixed Race Element Icon in Settings tab."+
-                    "\n\nForza Horizon 5:"+
+                    "\n- Added Oversteer Trace HUD for: Assetto Corsa, Assetto Corsa EVO, Forza Horizon 5 and RaceRoom."+
+
+                    "\n\nForza Horizon 5"+
                     "\n- Wheel Slip HUD: added wheel slip angle indicator."+
-                    "\n- Added Oversteer Trace HUD."+
-                    "\n\nRaceRoom:"+
-                    "\n- Added Oversteer Trace HUD."+
-                    "\n\nAssetto Corsa:"+
-                    "\n- Added Oversteer Trace HUD."+
-                    "\n\nAssetto Corsa EVO:"+
-                    "\n- Added Oversteer Trace HUD."+
-                    "\n\nAssetto Corsa Competizione:"+
+
+                    "\n\nAssetto Corsa Competizione"+
                     "\n- During a Race, HUDs will now become visible as soon as you hit Drive, previously visibilty started when the countdown timer for the formation lap ended."+
-                    "\n\nDSX:"+
-                    "\n- Added support for Le Mans Ultimate, rFactor 2 and WRC Generations."                 
+                    "\n- Twitch Chat HUD: +car command now also shows the max rpm if the data is available."+
+
+                    "\n\nRace Element"+
+                    "\n- Fixed Race Element Icon in Settings tab."
         },
         {"2.3.1.0", "RaceRoom:"+
                     "\n- Updated Data Connector to support data for hybrid cars."+
@@ -56,7 +57,7 @@ public static class ReleaseNotes
                     "\n\nMulti-Sim:"+
                     "\n- Input Bars HUD: Added Clutch bar."
                     },
-        {"2.2.6.6", "- Improve multi-sim experience." 
+        {"2.2.6.6", "- Improve multi-sim experience."
                     },
         {"2.2.6.4", "Race Element:"+
                     "\n- Improved logging."+
