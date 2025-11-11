2d79e64d5e33f6e85aeae834c704642e96e14dc2
RiddleTime <reinierklarenberg@gmail.com>
2025-11-11 20:53:03 +0100

    Update CachedBitmap.cs
    add extra check whether the cached bitmap isn't null and catch any exception s to still draw the image

--- Race_Element.HUD/Overlay/OverlayUtil/CachedBitmap.cs
+++ Race_Element.HUD/Overlay/OverlayUtil/CachedBitmap.cs
@@ -107,8 +107,17 @@ public sealed class CachedBitmap : IDisposable
             }
             else
             {
-                if (g.Transform.IsIdentity && width == Width && height == Height)
-                    g.DrawCachedBitmap(_internalCachedBitmap, x, y);
+                if (g.Transform.IsIdentity && width == Width && height == Height && _internalCachedBitmap != null)
+                {
+                    try
+                    {
+                        g.DrawCachedBitmap(_internalCachedBitmap, x, y);
+                    }
+                    catch
+                    {
+                        g.DrawImage(_bitmap, new Rectangle(x, y, width, height));
+                    }
+                }
                 else
                     g.DrawImage(_bitmap, new Rectangle(x, y, width, height));
             }
