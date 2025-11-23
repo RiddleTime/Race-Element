73092c19cf4d03e46c0df788c2ac3fef61767ee1
Reinier Klarenberg <reinierklarenberg@gmail.com>
2025-11-23 03:06:26 +0000

    Auto-generated commit
    Triggered by commit: https://github.com/RiddleTime/Race-Element/commit/dd5fa5a630eb5a5765e41d4025b9b3ff6ec2b36b

--- assets/2025-review-sOkxQ59Y.js
+++ assets/2025-review-sOkxQ59Y.js
@@ -1,7 +1,7 @@
 const e=`---
 title: A Review of 2025
 slug: 2025-Review
-description: Progress in 2025 and what may be next
+description: 14 supported simulators in 2025 and what may be next
 date: 2025-11-23
 type: news
 ---
