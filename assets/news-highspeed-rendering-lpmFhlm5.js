96547ca8e81a8c22e23c8716837338834552b362
Reinier Klarenberg <reinierklarenberg@gmail.com>
2025-11-23 02:44:23 +0000

    Auto-generated commit
    Triggered by commit: https://github.com/RiddleTime/Race-Element/commit/913ddb4b67cfb59392646cfe5d165c4e7711cbee

--- assets/news-highspeed-rendering-lpmFhlm5.js
+++ assets/news-highspeed-rendering-lpmFhlm5.js
@@ -12,7 +12,7 @@ It&#39;s obvious that higher refresh rates cost more computing power but thanks 
 <h1 id="efficiency-and-feedback">Efficiency and Feedback</h1>
 <p>The first 200 Hz HUD in Race Element is the newly added Shift Bar HUD. It will allow you to spot the right moment instead of losing laptime whilst waiting for a 30-60 hz shift bar to blink.
 Another HUD which demonstrates the highspeed rendering is the 3D HUD in the pitwall, which can ultimately push the limits of the current render pipeline at 500Hz and beyond.</p>
-<h1 id="what-is-next">What is next?</h1>
+<h1 id="what-is-next-1">What is next?</h1>
 <p>After experiencing the shift from 60 Hz to 200 hz it is only a matter of time for other HUDs to gain quicker refresh rates. 
 The new Shift Bar HUD serves as an example that Race Element&#39;s renderer is not only quick but that it&#39;s capable of doing so with minimal cpu usage. </p>
 `;export{e as default};
