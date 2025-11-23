5efe5681a8b0ee6e36ffd57b45ae7f9c6436395f
Reinier Klarenberg <reinierklarenberg@gmail.com>
2025-11-23 02:43:52 +0000

    Auto-generated commit
    Triggered by commit: https://github.com/RiddleTime/Race-Element/commit/17629ae57e4a155464d716d2221e11fbf2a5457c

---
+++ assets/sponsor-BX0YpJpa.js
@@ -0,0 +1,13 @@
+const o=`---
+title: Sponsor
+slug: sponsor
+description: Support Developement
+---
+
+<h1 id="more-than-a-1000-hours-have-been-spend-on-this-project-if-you-want-to-support-me-on-this-journey-leave-a-donation-or-become-a-patreon">More than a 1000 hours have been spend on this project, if you want to support me on this journey leave a donation or become a patreon</h1>
+<h2 id="sponsor">Sponsor</h2>
+<ul>
+<li><a href="https://paypal.me/CompetizioneManager" target="_blank"><strong>Donate with PayPal</strong></a></li>
+<li><a href="https://patreon.com/RaceElement" target="_blank"><strong>Become a Patreon</strong></a></li>
+</ul>
+`;export{o as default};
