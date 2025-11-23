a7bc7f5eb1f25b704712542bd2d7e7776c196209
Reinier Klarenberg <reinierklarenberg@gmail.com>
2025-11-23 03:01:56 +0000

    Auto-generated commit
    Triggered by commit: https://github.com/RiddleTime/Race-Element/commit/9395c7cc28a96b9dc3c1624090a88fd6fce66c57

---
+++ assets/2025-review-Bo43kEvk.js
@@ -0,0 +1,29 @@
+const e=`---
+title: A Review of 2025
+slug: 2025-Review
+description: Progress in 2025 and what may be next
+date: 2025-11-23
+type: news
+---
+
+<h1 id="october-2024-going-multi-sim-4-supported-simulators">October 2024: Going Multi-sim, 4 supported simulators</h1>
+<p>Back in October 2024, Race Element went Multi-Sim for the first time, adding Assetto Corsa, iRacing and RaceRoom. Since then the app has gained many features that works across many driving simulators. It not only meant that some of the existing HUDs and features for Assetto Corsa Competizione could be ported, but it also meant that some of them could be rewritten from scratch.
+<br/></p>
+<h1 id="end-of-2024-automobilista-2-euro-truck-simulator-2-and-american-truck-simulator-are-now-supported-7-supported-simulators">End of 2024: Automobilista 2, Euro Truck Simulator 2 and American Truck Simulator are now supported, 7 supported simulators</h1>
+<p>Once adding new games to Race Element became easier, Andrei Jianu added AMS2 whilst ETS2 and ATS were also added by Reinier Klarenberg (creator of Race Element). During this month a new shift bar was also added being able to render up to 200 Hz. The app was also migrated to .NET 9 during this month providing better performance and some extra techniques to write code more efficiently.
+<br /></p>
+<h1 id="january-2025-ac-evo-support-is-added-8-supported-simulators">January 2025: AC EVO Support is added, 8 supported simulators</h1>
+<p>Assetto Corsa EVO was teased and let loose at the 2024 ADAC Simracing Expo in Dortmund, in January 2025 the game was released as Early Access and supported was added to Race Element.
+<br /></p>
+<h1 id="may-2025-dsx-guides-added-to-the-website-and-auto-game-detection">May 2025: DSX Guides added to the website and auto game detection</h1>
+<p>DSX has been a main part of Race Element for the past 1.5 years. DSX provides a lot of fun with DualSense since it provides Adaptive Triggers with Race Element, this will grow further along the year! Race Element now automatically detects whether any of the supported games are running causing it to auto-switch without the user having to interact with Race Element.
+<br /></p>
+<h1 id="august-2025-forza-horizon-5-is-added-9-supported-simulators">August 2025: Forza Horizon 5 is added, 9 supported simulators</h1>
+<p>Forza Horizon 5, without a doubt, is a great game to cruise, just to have fun and chill with your friends. One easily puts in 30–50 hours just by exploring the map and enjoying a bit of multiplayer! DSX Support is also there.
+<br /></p>
+<h1 id="november-2025-14-supported-simulators">November 2025: 14 supported simulators...</h1>
+<p>What a month! Le Mans Ultimate, rFactor 2 and WRC Generations were added as well as Assetto Corsa Rally went into Early Access. Another addition is Forza Motorsport 8. All of these support DSX Adaptive Triggers with Race Element and plenty of HUDs to customize your game.
+<br /></p>
+<h1 id="what-is-next">What is next?</h1>
+<p>Additional driving simulators and HUDs will be added as long as the app is viable to exist, though more than a 1000 hours have been spent on this app so far. If you want to contribute, please do so with either code or a donation: <a href="https://race.elementfuture.com/guide/sponsor">https://race.elementfuture.com/guide/sponsor</a></p>
+`;export{e as default};
