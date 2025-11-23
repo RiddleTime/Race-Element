8d688ad78eabe25ebcf42ca25c516d8a31977bca
Reinier Klarenberg <reinierklarenberg@gmail.com>
2025-11-23 03:34:20 +0000

    Auto-generated commit
    Triggered by commit: https://github.com/RiddleTime/Race-Element/commit/da12e0605891092763a5dc6222f47502ac83c6ac

--- assets/2025-review-4LrHH49t.js
+++ assets/2025-review-4LrHH49t.js
@@ -1,5 +1,5 @@
 const e=`---
-title: A Review of 2025
+title: A Review of 2025, The Multi Sim Year
 slug: 2025-Review
 description: 14 supported simulators in 2025 and what may be next
 date: 2025-11-23
