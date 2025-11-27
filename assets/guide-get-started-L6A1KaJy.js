1debf8678d37bf7b6d7dc6d642af843c248b4f46
Reinier Klarenberg <reinierklarenberg@gmail.com>
2025-11-27 01:12:36 +0000

    Auto-generated commit
    Triggered by commit: https://github.com/RiddleTime/Race-Element/commit/0b46b8bcf78695d0e8c461c34d1bc1c59eff21c0

--- assets/guide-get-started-L6A1KaJy.js
+++ assets/guide-get-started-L6A1KaJy.js
@@ -1,4 +1,4 @@
-const e=`---
+const t=`---
 title: "Get Started"
 slug: how-to-get-started
 description: Downloading and Installing Race Element
@@ -10,9 +10,9 @@ type: guide
 <li><strong><a href="https://github.com/RiddleTime/Race-Element/releases/latest" target="_blank">Download</a></strong> the app. The link will lead you to the latest version at GitHub.  </li>
 <li>Place <strong>RaceElement.exe</strong> in any folder of My Documents. If the app is not located there you will encounter issues with the auto-updater.</li>
 <li>Then simply run the app. Do <strong>not</strong> run it as admin, since this will prevent some features from working.</li>
-<li>If you get a prompt that you need to install .NET, make sure to <code>install the .NET Desktop Runtime (x64)</code>.</li>
+<li>If you get a prompt that you need to install .NET, make sure to download and install the <code>.NET Desktop Runtime (x64)</code> from <strong><a href="https://dotnet.microsoft.com/en-us/download/dotnet/10.0" target="_blank">https://dotnet.microsoft.com/en-us/download/dotnet/10.0</a></strong>.</li>
 </ol>
 <h1 id="guides">Guides</h1>
 <p>The app includes an about tab which contains a guide section, read this carefully since it includes instructions on how to use for example the HUDs.  </p>
 <p>There are multiple guides posted on this web page which you can find <a href="/guide/">here</a>.</p>
-`;export{e as default};
+`;export{t as default};
