1f70b76dde8f6b79ae7203e1129bd667b64db3dc
Reinier Klarenberg <reinierklarenberg@gmail.com>
2025-11-27 01:08:18 +0000

    Auto-generated commit
    Triggered by commit: https://github.com/RiddleTime/Race-Element/commit/8ae1725caf45ccebafc21945d21f564b1d0878fe

--- assets/guide-get-started-DFzNQg5a.js
+++ assets/guide-get-started-DFzNQg5a.js
@@ -10,7 +10,7 @@ type: guide
 <li><strong><a href="https://github.com/RiddleTime/Race-Element/releases/latest" target="_blank">Download</a></strong> the app. The link will lead you to the latest version at GitHub.  </li>
 <li>Place <strong>RaceElement.exe</strong> in any folder of My Documents. If the app is not located there you will encounter issues with the auto-updater.</li>
 <li>Then simply run the app. Do <strong>not</strong> run it as admin, since this will prevent some features from working.</li>
-<li>If you get a prompt that you need to install .NET, make sure to <code>install the .NET Desktop Runtime</code>.</li>
+<li>If you get a prompt that you need to install .NET, make sure to <code>install the .NET Desktop Runtime (x64)</code>.</li>
 </ol>
 <h1 id="guides">Guides</h1>
 <p>The app includes an about tab which contains a guide section, read this carefully since it includes instructions on how to use for example the HUDs.  </p>
