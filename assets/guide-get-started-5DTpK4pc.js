9a3b8b762bf1e93dd06054fb4a3d7a7ac0fc1729
Reinier Klarenberg <reinierklarenberg@gmail.com>
2026-03-02 02:10:48 +0000

    Auto-generated commit
    Triggered by commit: https://github.com/RiddleTime/Race-Element/commit/c53dab8f0c15b81b703a28e7e94d6c621cfdafef
    GitHub Actions build: https://github.com/RiddleTime/Race-Element/actions/runs/22558527268

--- assets/guide-get-started-5DTpK4pc.js
+++ assets/guide-get-started-5DTpK4pc.js
@@ -11,7 +11,7 @@ type: guide
 <li>Place <strong>RaceElement.exe</strong> in any folder of My Documents. If the app is not located there you will encounter issues with the auto-updater.</li>
 <li>Then simply run the app. Do <strong>not</strong> run it as admin, since this will prevent some features from working.</li>
 <li>If you get a prompt that you need to install .NET, make sure to download and install the <code>.NET Desktop Runtime (x64)</code> from <strong><a href="https://dotnet.microsoft.com/en-us/download/dotnet/10.0" target="_blank">https://dotnet.microsoft.com/en-us/download/dotnet/10.0</a></strong>.</li>
-<li>Check the <strong><a href="https://race.elementfuture.com/guide/multi-sim" target="_blank">Multi-Sim Guide</a></strong> Some games require extra config.</li>
+<li>Check the <strong><a href="https://race.elementfuture.com/guide/multi-sim" target="_blank">Multi-Sim Guide</a></strong>, certain games require extra config.</li>
 </ol>
 <h1 id="guides">Guides</h1>
 <p>The app includes an about tab which contains a guide section, read this carefully since it includes instructions on how to use for example the HUDs.  </p>
