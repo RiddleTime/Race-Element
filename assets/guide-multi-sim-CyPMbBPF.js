f0e0fb8f0cff699eb11cf8955bd88e98f867d213
Reinier Klarenberg <reinierklarenberg@gmail.com>
2026-04-01 23:34:31 +0000

    Auto-generated commit
    Triggered by commit: https://github.com/RiddleTime/Race-Element/commit/779556eac6ba34718e91f3bffe7c603bc5bb34cf
    GitHub Actions build: https://github.com/RiddleTime/Race-Element/actions/runs/23875982406

--- assets/guide-multi-sim-CyPMbBPF.js
+++ assets/guide-multi-sim-CyPMbBPF.js
@@ -43,7 +43,7 @@ Below you can find a guide for each game that requires a setup. If you can&#39;t
 <li>Restart Forza Horizon 5.</li>
 <li>Restart Race Element.</li>
 <li>If Windows asks permission for network access allow it else race element won&#39;t be able to receive data from Forza Horizon 4.</li>
-<li>If run the game through Microsoft Store, so skip this step if you run it through Steam. Open a command prompt( CMD) as administrator and run the following command: <code>CheckNetIsolation.exe LoopbackExempt -a -n=Microsoft.SunriseBaseGame_8wekyb3d8bbwe</code> it allows the game to send the UDP packets to your local computer.</li>
+<li>If you run the gam from the Microsoft Store, so skip this step if you run it through Steam. Open a command prompt( CMD) as administrator and run the following command: <code>CheckNetIsolation.exe LoopbackExempt -a -n=Microsoft.SunriseBaseGame_8wekyb3d8bbwe</code> it allows the game to send the UDP packets to your local computer.</li>
 </ol>
 <h1 id="forza-horizon-5">Forza Horizon 5</h1>
 <ol>
