58114e7e6bf25d37953aab77fd0180b5b66c38b8
RiddleTime <reinierklarenberg@gmail.com>
2026-04-16 05:30:30 +0200

    remove mapperly from ac evo mappers

--- Race Element.Data/Games/AssettoCorsaEvo/DataMapper/GameDataMapper.cs
+++ Race Element.Data/Games/AssettoCorsaEvo/DataMapper/GameDataMapper.cs
@@ -1,11 +1,9 @@
 ﻿using RaceElement.Data.Common.SimulatorData;
-using Riok.Mapperly.Abstractions;
 using static RaceElement.Data.Games.AssettoCorsaEvo.SharedMemory.AcEvoSharedMemory;
 
 namespace RaceElement.Data.Games.AssettoCorsaEvo.DataMapper;
 
-[Mapper]
-internal static partial class GameDataMapper
+internal static class GameDataMapper
 {
     public static void WithStaticPage(SPageFileStaticEvo pageStatic, GameData gameData)
     {
