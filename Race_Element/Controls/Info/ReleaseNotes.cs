06e246a1732d31ae66d9fe96e3f1524ae0b5b6ff
RiddleTime <reinierklarenberg@gmail.com>
2026-05-09 01:40:35 +0200

    Update ReleaseNotes.cs

--- Race_Element/Controls/Info/ReleaseNotes.cs
+++ Race_Element/Controls/Info/ReleaseNotes.cs
@@ -10,9 +10,10 @@ public static class ReleaseNotes
                     "\n- Added support for Microsoft Flight Simulator 2020."+
                     "\n- Added support for Microsoft Flight Simulator 2024."+
                     "\n\nFlight HUDs"+
-                    "\n- Added Vertical Speed HUD."+
+                    "\n- Added Vertical Speed HUD. Based on positive or negative VS the background can change color."+
                     "\n- Added Indicated Air Speed HUD."+
-                    "\n- Added Altitude HUD."
+                    "\n- Added Altitude HUD."+
+                    "\n- Added Aircraft Info HUD. Shows data like Type, Model and Identifier."
                     },
         {"2.6.3.0", "Common HUDs"+
                     "\n- Input Trace HUD: Increase max refresh rate to 60 Hz with 2 step intervals."+
