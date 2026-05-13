6f89ed69ccb5548c074ab9b6f871c2109e30b3c4
RiddleTime <reinierklarenberg@gmail.com>
2026-05-13 20:29:40 +0200

    Update ReleaseNotes.cs

--- Race_Element/Controls/Info/ReleaseNotes.cs
+++ Race_Element/Controls/Info/ReleaseNotes.cs
@@ -6,6 +6,10 @@ public static class ReleaseNotes
 {
     internal static readonly Dictionary<string, string> Notes = new()
     {
+        {"2.7.0.6", "Flight HUDs"+
+                    "\n- Added new Controls HUD, shows the current state of basic control surfaces."+
+                    "\n- Added new Controls Trace HUD, shows a live graph of basic control surfaces."
+                    },
         {"2.7.0.4", "Race Element:"+
                     "\n- Updated SimConnect.NET package for improved performance."
                     },
