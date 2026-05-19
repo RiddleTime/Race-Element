2f0b59c2b2d46f6bc60637c83a73bc00dc61cab7
Reinier Klarenberg <reinierklarenberg@gmail.com>
2026-05-19 20:38:42 +0000

    Auto-generated commit
    Triggered by commit: https://github.com/RiddleTime/Race-Element/commit/f82eb51230f0ad922bee8934aad8ae21f914cc20
    GitHub Actions build: https://github.com/RiddleTime/Race-Element/actions/runs/26123769287

--- assets/guide-DSX-ubNGF3oQ.js
+++ assets/guide-DSX-ubNGF3oQ.js
@@ -29,8 +29,9 @@ type: guide
 <ol>
 <li>Open DSX and at the left bottom click Settings (Don&#39;t have DSX yet? <strong><a href="https://store.steampowered.com/app/1812620/DSX/">Get DSX on Steam</a></strong> )</li>
 <li>Open the Networking tab</li>
-<li>Make sure the Firewall Rules are okay, if it&#39;s not then allow DSX to fix it</li>
+<li>Make sure the Firewall Rules are okay, if it&#39;s not then allow DSX to fix it.</li>
 <li>Enable <code>Incoming UDP</code></li>
+<li>Make Incoming UDP port should be set to 6969.</li>
 </ol>
 <h1 id="race-element">Race Element</h1>
 <ol>
@@ -39,7 +40,7 @@ type: guide
 <li>Make sure the game selection is set correctly(left-bottem of the app), this should happen automatically unless disabled</li>
 <li>Open the HUD Tab</li>
 <li>Click DSX</li>
-<li>You can alter behaviour for Braking and Acceleration</li>
+<li>You can alter behaviour for Braking and Acceleration, the port that is used to connect to DSX should be set to 6969. This is not related to what you see in the multi-sim guide.</li>
 <li>To activate it read <strong><a href="/guide/how-to-use-huds">The guide for using the HUD Tab</a></strong></li>
 </ol>
 `;export{e as default};
